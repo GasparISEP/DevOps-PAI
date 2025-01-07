8ed9bcc05e73324cb293b47c4ba2a238091da7cc
Dianaguedes19 <1241903@isep.ipp.pt>
2025-01-07 16:25:26 +0000

    - Added the constructor and methods to check name and maxEcts.

--- src/main/java/PAI/domain/DegreeType.java
+++ src/main/java/PAI/domain/DegreeType.java
@@ -8,4 +8,32 @@ public class DegreeType {
     private int _maxEcts;
 
     //private List<Programme> _programme;
+
+    // Constructor
+    public DegreeType(String name, int maxEcts) throws Exception {
+        if (nameIsInvalid(name)) {
+            throw new IllegalArgumentException("Name cannot be empty");
+        }
+        _name=name;
+        if(maxEctsIsInvalid(maxEcts)) {
+            throw new IllegalArgumentException("Insert a valid number of ECTS");
+        }
+        _maxEcts =maxEcts;
+
+
+    }
+
+    public boolean nameIsInvalid (String name){
+        if(name==null || name.isBlank()){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean maxEctsIsInvalid (int maxEcts){
+        if(maxEcts<0 || maxEcts >30){
+            return true;
+        }
+        return false;
+    }
 }
