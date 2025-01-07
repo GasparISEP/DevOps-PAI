74b7cc0a9718b966404d734b4f46d6be0bf2ab62
RicardoMarques21 <1241923@isep.ipp.pt>
2025-01-07 16:33:42 +0000

    Added more tests do DegreeType and create DegreeTypeRepository

---
+++ src/main/java/PAI/domain/DegreeTypeRepository.java
@@ -0,0 +1,13 @@
+package PAI.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DegreeTypeRepository {
+
+    private List<DegreeType> degreeTypeList = new ArrayList<>();
+
+    // Constructor
+    public DegreeTypeRepository() {
+    }
+}
--- src/test/java/PAI/domain/DegreeTypeTest.java
+++ src/test/java/PAI/domain/DegreeTypeTest.java
@@ -26,4 +26,18 @@ class DegreeTypeTest {
         assertThrows(Exception.class, () -> new DegreeType(null, 20));
     }
 
+    @Test
+    void overLimitECTSDontCreateADegreeType() throws Exception {
+
+        // arrange + act + assert
+        assertThrows(Exception.class, () -> new DegreeType("Master", 35));
+    }
+
+    @Test
+    void negativeECTSDontCreateADegreeType() throws Exception {
+
+        // arrange + act + assert
+        assertThrows(Exception.class, () -> new DegreeType("Master", -1));
+    }
+
 }
