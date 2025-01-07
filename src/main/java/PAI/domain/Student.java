7e3994e15c19587d198c29afa2881f2fbbd60120
RicardoJacinto <1241922@isep.ipp.pt>
2025-01-07 17:53:47 +0000

    Added attributes to class Student.

--- src/main/java/PAI/domain/Student.java
+++ src/main/java/PAI/domain/Student.java
@@ -1,4 +1,13 @@
 package PAI.domain;
 
 public class Student {
+
+    private int _uniqueNumber;
+    private String _name;
+    private String _NIF;
+    private String _phone;
+    private String _email;
+    private Address _address;
+
+
 }
