3181f67851ee4b4d7536735f7498a3fff3aec180
jmariz-costa <1241912@isep.ipp.pt>
2025-01-07 17:30:45 +0000

    Add Contructor to AccessMethod created tests for Accessmethod.

--- src/main/java/PAI/domain/AccessMethod.java
+++ src/main/java/PAI/domain/AccessMethod.java
@@ -2,4 +2,12 @@ package PAI.domain;
 
 public class AccessMethod {
 
+    private String _name;
+
+    public AccessMethod(String acessMethodName) throws Exception {
+
+        if (acessMethodName == null)
+            throw new Exception("Access method name cannot be null");
+        this._name = acessMethodName;
+    }
 }
---
+++ src/test/java/PAI/domain/AccessMethodTest.java
@@ -0,0 +1,26 @@
+package PAI.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccessMethodTest {
+
+    @Test
+    void ValidNameAccessMethodCreatesAccessMethod() throws Exception {
+        //arrange
+
+        //act
+        AccessMethod am1 = new AccessMethod("Maiores 23");
+
+        //assert
+    }
+
+    @Test
+    void NullNameAccessMethodDoesNotCreateAccessMethod() throws Exception {
+        //arrange
+        //act
+        //assert
+        assertThrows(Exception.class, () -> new AccessMethod(null));
+    }
+}
