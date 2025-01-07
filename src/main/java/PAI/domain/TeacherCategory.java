910802fde87d9ae902cf855b64ff515ab3d3c17c
Ruben Rocha <rubenrocha26pt@gmail.com>
2025-01-07 17:12:32 +0000

    Created TeacherCategory and constructor tests.

---
+++ src/main/java/PAI/domain/TeacherCategory.java
@@ -0,0 +1,10 @@
+package PAI.domain;
+
+public class TeacherCategory {
+
+    private String _name;
+
+    public TeacherCategory (String categoryName){
+        this._name = categoryName;
+    }
+}
