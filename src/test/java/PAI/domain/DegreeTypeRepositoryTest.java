35f896e5b0a9a50f138bb67b5c3d1d74e07de5f2
ze <1241915@isep.ipp.pt>
2025-01-07 16:39:40 +0000

    Added all the tests to DegreeTypeRepository.

--- src/test/java/PAI/domain/DegreeTypeRepositoryTest.java
+++ src/test/java/PAI/domain/DegreeTypeRepositoryTest.java
@@ -1,7 +1,40 @@
 package PAI.domain;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DegreeTypeRepositoryTest {
+
+    // Test to add a valid object
+    @Test
+    void createDegreeTypeRepository () throws Exception {
+
+        //arrange
+
+        //act + assert
+        DegreeTypeRepository degreeType1 = new DegreeTypeRepository();
+    }
+
+    // Add DegreeType
+    @Test
+    void addDegreeTypeToRepository() throws Exception {
+
+
+        DegreeTypeRepository repository = new DegreeTypeRepository();
+
+
+        DegreeType Bachelor = new DegreeType("Bachelor",20);
+
+
+        repository.addDegreeType(Bachelor);
+
+
+        List<DegreeType> degreeTypeList = repository.getDegreeTypeList();
+        assertTrue(degreeTypeList.contains(Bachelor));
+
+    }
 
 }
