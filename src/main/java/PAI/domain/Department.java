707f38404dfe71d8974314eb439f6de2a57d3c13
inesbarbosa88 <inesbarbosa24@hotmail.com>
2025-01-08 16:20:31 +0000

    add name and acronym validations + tests + method "hasSameAcronym" and "hasSameName"

--- src/main/java/PAI/domain/Department.java
+++ src/main/java/PAI/domain/Department.java
@@ -10,14 +10,44 @@ public class Department {
 
     //constructor
     public Department(String acronym, String name) throws Exception {
-        this._acronym = acronym;
-        this._name = name;
+        validateAcronym(acronym);
+        validateName(name);
     }
 
+    //construtor
     public Department(String acronym, String name,Teacher teacherDirector) throws IllegalArgumentException {
-        this._acronym = acronym;
-        this._name = name;
+        validateAcronym(acronym);
+        validateName(name);
         this._director = teacherDirector;
     }
 
+    private void validateName(String name) throws IllegalArgumentException {
+        if (name == null || name.isBlank())
+            throw new IllegalArgumentException("Department´s name must be a non-empty String.");
+        if(!name.matches("^[A-Z].*")){
+            throw new IllegalArgumentException("Department´s name should start with a capital letter.");
+        }
+        if (name.length() <2 || name.length() > 100){
+            throw new IllegalArgumentException("Department´s name must be between 2 and 100 characters.");
+        }
+        this._name = name;
+    }
+
+    private void validateAcronym(String departmentAcronym) throws IllegalArgumentException {
+        if (departmentAcronym == null || departmentAcronym.isBlank())
+            throw new IllegalArgumentException("Department´s acronym must be a 3 letter non-empty String.");
+
+        if(!departmentAcronym.matches("^[A-Z]{3}$")){
+            throw new IllegalArgumentException("Department´s acronym must contain only three capital letters.");
+        }
+        this._acronym = departmentAcronym;
+    }
+
+    public boolean hasSameAcronym(Department department) {
+        return this._acronym.equals(department._acronym);
+    }
+
+    public boolean hasSameName(Department department) {
+        return this._name.equals(department._name);
+    }
 }
