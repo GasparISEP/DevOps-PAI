7982dceb65478113606b40acfdef156c6449f79e
jmariz-costa <1241912@isep.ipp.pt>
2025-01-23 12:05:32 +0000

    ProgrammeEdition Class Creation with EqualsMethod + tests

---
+++ src/main/java/PAI/domain/ProgrammeEdition.java
@@ -0,0 +1,30 @@
+package PAI.domain;
+
+public class ProgrammeEdition {
+
+    private Programme _programme;
+    private SchoolYear _schoolYear;
+
+    public ProgrammeEdition(Programme programme, SchoolYear schoolYear) {
+
+        if (programme == null)
+            throw new IllegalArgumentException("Programme cannot be null");
+        if (schoolYear == null)
+            throw new IllegalArgumentException("School year cannot be null");
+
+        _programme = programme;
+        _schoolYear = schoolYear;
+    }
+
+    @Override
+    public boolean equals (Object other) {
+
+        if (!(other instanceof ProgrammeEdition))
+            return false;
+
+        ProgrammeEdition otherEdition = (ProgrammeEdition) other;
+        if (otherEdition._programme.equals(_programme) && otherEdition._schoolYear.isSameSchoolYear(_schoolYear))
+            return true;
+        return false;
+    }
+}
