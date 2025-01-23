2c25ac09255e3d62a9de0085f01ca4218578f6de
ma.antunes <1241916@isep.ipp.pt>
2025-01-23 17:45:25 +0000

    Add CourseEditionRepository class and tests

---
+++ src/test/java/PAI/domain/CourseEditionRepositoryTest.java
@@ -0,0 +1,139 @@
+package PAI.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CourseEditionRepositoryTest {
+
+    @Test
+    void shouldReturnTrueIfCourseEditionHasBeenCreated() throws Exception {
+        //arrange
+        DegreeType master = new DegreeType("Master",240);
+        Department CSE = new Department("CSE", "Computer Science Engineer");
+        TeacherCategory assistantProfessor = new TeacherCategory("Assistant Professor");
+        Address addressIsep = new Address("Rua São Tomé Porto","4249-015","Porto", "Portugal");
+        Teacher teacher = new Teacher("ABC", "Joe Doe", "abc@isep.ipp.pt", "123456789", "B106", addressIsep, assistantProfessor, CSE);
+        Programme p1 = new Programme("Computer Engineering", "CE", 20,6,master,CSE,teacher);
+        Course c1 = new Course ("Informatics", "INF", 6, 1);
+        SchoolYear sY1 = new SchoolYear("Ano letivo de", "23-11-2024", "09-12-2025");
+        ProgrammeEdition pE1 = new ProgrammeEdition (p1, sY1);
+        CourseEditionRepository courseEditionRepository1 = new CourseEditionRepository();
+        //act
+        boolean result = courseEditionRepository1.createCourseEdition(c1,pE1);
+        //assert
+        assertTrue(result);
+
+    }
+
+    @Test
+    void shouldReturnTrueIfTheAddedCourseEditionHasDifferentCourseButTheSameProgrammeEdition() throws Exception {
+        //Arrange
+        DegreeType master = new DegreeType("Master",240);
+        Department CSE = new Department("CSE", "Computer Science Engineer");
+        TeacherCategory assistantProfessor = new TeacherCategory("Assistant Professor");
+        Address addressIsep = new Address("Rua São Tomé Porto","4249-015","Porto", "Portugal");
+        Teacher teacher = new Teacher("ABC", "Joe Doe", "abc@isep.ipp.pt", "123456789", "B106", addressIsep, assistantProfessor, CSE);
+        Programme p1 = new Programme("Computer Engineering", "CE", 20,6,master,CSE,teacher);
+        Course c1 = new Course ("Informatica", "INF", 6, 1);
+        SchoolYear sY1 = new SchoolYear("Ano letivo de", "23-11-2024", "09-12-2025");
+        ProgrammeEdition pE1 = new ProgrammeEdition (p1, sY1);
+
+        Course c2 = new Course ("Matemática", "MAT", 6, 1);
+        CourseEditionRepository courseEditionRepository1 = new CourseEditionRepository();
+        courseEditionRepository1.createCourseEdition(c1,pE1);
+        //act
+        boolean result = courseEditionRepository1.createCourseEdition(c2,pE1);
+        //assert
+        assertTrue(result);
+    }
+
+    @Test
+    void shouldReturnTrueIfTheAddedCourseEditionHasSameCourseButDifferentProgrammeEdition() throws Exception {
+        //Arrange
+        DegreeType master = new DegreeType("Master",240);
+        Department CSE = new Department("CSE", "Computer Science Engineer");
+        TeacherCategory assistantProfessor = new TeacherCategory("Assistant Professor");
+        Address addressIsep = new Address("Rua São Tomé Porto","4249-015","Porto", "Portugal");
+        Teacher teacher = new Teacher("ABC", "Joe Doe", "abc@isep.ipp.pt", "123456789", "B106", addressIsep, assistantProfessor, CSE);
+        Programme p1 = new Programme("Computer Engineering", "CE", 20,6,master,CSE,teacher);
+        Course c1 = new Course ("Informatics", "INF", 6, 1);
+        SchoolYear sY2 = new SchoolYear("Ano letivo de", "23-11-2024", "09-12-2025");
+        SchoolYear sY1 = new SchoolYear("Ano letivo de", "23-11-2023", "09-12-2024");
+        ProgrammeEdition pE1 = new ProgrammeEdition (p1, sY1);
+        ProgrammeEdition pE2 = new ProgrammeEdition (p1, sY2);
+
+        CourseEditionRepository courseEditionRepository1 = new CourseEditionRepository();
+        courseEditionRepository1.createCourseEdition(c1,pE1);
+        //act
+        boolean result = courseEditionRepository1.createCourseEdition(c1,pE2);
+        //assert
+        assertTrue(result);
+    }
+
+    @Test
+    void shouldReturnFalseIfTheCourseEditionIsAlreadyRegistered() throws Exception {
+        //arrange
+        DegreeType master = new DegreeType("Master",240);
+        Department CSE = new Department("CSE", "Computer Science Engineer");
+        TeacherCategory assistantProfessor = new TeacherCategory("Assistant Professor");
+        Address addressIsep = new Address("Rua São Tomé Porto","4249-015","Porto", "Portugal");
+        Teacher teacher = new Teacher("ABC", "Joe Doe", "abc@isep.ipp.pt", "123456789", "B106", addressIsep, assistantProfessor, CSE);
+        Programme p1 = new Programme("Computer Engineering", "CE", 20,6,master,CSE,teacher);
+        Course c1 = new Course ("Informatics", "INF", 6, 1);
+        SchoolYear sY1 = new SchoolYear("Ano letivo de", "23-11-2024", "09-12-2025");
+        ProgrammeEdition pE1 = new ProgrammeEdition (p1, sY1);
+        CourseEditionRepository courseEditionRepository1 = new CourseEditionRepository();
+        courseEditionRepository1.createCourseEdition(c1,pE1);
+        //act
+        boolean result = courseEditionRepository1.createCourseEdition(c1,pE1);
+        //assert
+        assertFalse(result);
+
+    }
+
+
+    @Test
+    void shouldReturnFalseIfCourseIsNull() throws Exception {
+        //Arrange
+        DegreeType master = new DegreeType("Master", 240);
+        Department CSE = new Department("CSE", "Computer Science Engineer");
+        TeacherCategory assistantProfessor = new TeacherCategory("Assistant Professor");
+        Address addressIsep = new Address("Rua São Tomé Porto", "4249-015", "Porto", "Portugal");
+        Teacher teacher = new Teacher("ABC", "Joe Doe", "abc@isep.ipp.pt", "123456789", "B106", addressIsep, assistantProfessor, CSE);
+        Programme p1 = new Programme("Computer Engineering", "CE", 20, 6, master, CSE, teacher);
+        SchoolYear sY1 = new SchoolYear("Ano letivo de", "23-11-2024", "09-12-2025");
+        ProgrammeEdition pE1 = new ProgrammeEdition(p1, sY1);
+        CourseEditionRepository courseEditionRepository1 = new CourseEditionRepository();
+        //Act
+        boolean result = courseEditionRepository1.createCourseEdition(null, pE1);
+        //assert
+        assertFalse(result);
+
+    }
+
+    @Test
+    void shouldReturnFalseIfProgrammeEditionIsNull() throws Exception {
+        //Arrange
+        Course c1 = new Course("Informatics", "INF", 6, 1);
+        CourseEditionRepository courseEditionRepository1 = new CourseEditionRepository();
+        //Act
+        boolean result = courseEditionRepository1.createCourseEdition(c1, null);
+        //assert
+        assertFalse(result);
+    }
+
+    @Test
+    void shouldReturnFalseIfCourseAndProgrammeEditionAreNull() throws Exception {
+        //Arrange
+        CourseEditionRepository courseEditionRepository1 = new CourseEditionRepository();
+        //Act
+        boolean result = courseEditionRepository1.createCourseEdition(null, null);
+        //assert
+        assertFalse(result);
+    }
+
+
+
+
+}
--- src/test/java/PAI/domain/CourseEditionTest.java
+++ src/test/java/PAI/domain/CourseEditionTest.java
@@ -53,6 +53,14 @@ class CourseEditionTest {
     }
 
     @Test
+    void shouldNotCreateCourseEditionIfProgrammeEditionAndCourseAreNull() throws Exception {
+        //Arrange
+        //Assert
+        assertThrows(Exception.class, () -> {new CourseEdition(null, null);});
+
+    }
+
+    @Test
     void shouldReturnTrueIfCourseEditionEqualsObject()throws Exception {
         //arrange
         DegreeType master = new DegreeType("Master",240);
@@ -114,7 +122,7 @@ class CourseEditionTest {
     }
 
     @Test
-    void shouldReturnFalseIfCourseAreNotEqualButProgrammeEditionsAreEqual() throws Exception{
+    void shouldReturnFalseIfCoursesAreNotEqualButProgrammeEditionsAreEqual() throws Exception{
         //arrange
         DegreeType master = new DegreeType("Master",240);
         Department CSE = new Department("CSE", "Computer Science Engineer");
