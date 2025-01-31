66ccedd0de5e26a0e1116f2f398738ac299e5135
paula <1241898@isep.ipp.pt>
2025-01-31 16:09:22 +0000

    -Altered US15 sequence diagram -Added tests to US15_UpdateTeacherWorkingPercentageControllerTest class

--- src/test/java/PAI/controller/US15_UpdateTeacherWorkingPercentageControllerTest.java
+++ src/test/java/PAI/controller/US15_UpdateTeacherWorkingPercentageControllerTest.java
@@ -2,8 +2,12 @@ package PAI.controller;
 
 import PAI.domain.*;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -65,6 +69,32 @@ class US15_UpdateTeacherWorkingPercentageControllerTest {
         assertTrue(optT1.isEmpty());
     }
 
+    public static Stream<Arguments> provideInvalidNIF() {
+        return Stream.of(
+                Arguments.of(""),
+                Arguments.of(" "),
+                Arguments.of((String) null)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideInvalidNIF")
+    void throwsExceptionWhenNIFIsNullBlankOrEmpty(String NIF) throws Exception {
+
+        //arrange
+        TeacherRepository repo = new TeacherRepository();
+        US15_UpdateTeacherWorkingPercentageController ctrl = new US15_UpdateTeacherWorkingPercentageController(repo);
+        TeacherCategory tc1 = new TeacherCategory("Professor Adjunto");
+        Department dpt1 = new Department("MAT", "Mathematics");
+        repo.registerTeacher("ABC", "Joe Doe", "abc@isep.ipp.pt", "123456789", "B106", "Doutoramento em Engenharia Informatica, 2005, ISEP", "Rua das Flores","4444-098","Porto","Portugal", "15-04-2005", tc1, 70, dpt1);
+
+        //act
+        Optional<Teacher> optT1 = ctrl.getTeacherByNIF(NIF);
+
+        //assert
+        assertTrue(optT1.isEmpty());
+    }
+
     @Test
     void successfullyUpdatesTeacherWorkingPercentage () throws Exception {
 
@@ -80,5 +110,47 @@ class US15_UpdateTeacherWorkingPercentageControllerTest {
 
         //assert
         assertTrue(result);
+    }
+
+    @Test
+    void throwsExceptionIfGivenWorkingPercentageIsTheSameAsLastRegisteredWorkingPercentage () throws Exception {
+
+        //arrange
+        TeacherRepository repo = new TeacherRepository();
+        US15_UpdateTeacherWorkingPercentageController ctrl = new US15_UpdateTeacherWorkingPercentageController(repo);
+        TeacherCategory tc1 = new TeacherCategory("Professor Adjunto");
+        Department dpt1 = new Department("MAT", "Mathematics");
+        Teacher teacher = new Teacher("ABC", "Joe Doe", "abc@isep.ipp.pt", "123456789", "B106", "Doutoramento em Engenharia Informatica, 2005, ISEP", "Rua das Flores","4444-098","Porto","Portugal", "15-04-2005", tc1, 70, dpt1);
+
+        //act
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> ctrl.updateTeacherWorkingPercentageInTeacherCareerProgression(teacher,"15-04-2008", 70));
+
+        //assert
+        assertEquals("Working percentage must be different than the last working percentage!", exception.getMessage());
+    }
+
+    public static Stream<Arguments> provideInvalidDates() {
+        return Stream.of(
+                Arguments.of("15-04-2005", "Date must be greater than the last date registered!"),
+                Arguments.of("14-04-2005", "Date must be greater than the last date registered!")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideInvalidDates")
+    void throwsExceptionIfGivenDateIsNotAfterLastRegisteredDate(String date, String expectedException) throws Exception {
+
+        //arrange
+        TeacherRepository repo = new TeacherRepository();
+        US15_UpdateTeacherWorkingPercentageController ctrl = new US15_UpdateTeacherWorkingPercentageController(repo);
+        TeacherCategory tc1 = new TeacherCategory("Professor Adjunto");
+        Department dpt1 = new Department("MAT", "Mathematics");
+        Teacher teacher = new Teacher("ABC", "Joe Doe", "abc@isep.ipp.pt", "123456789", "B106", "Doutoramento em Engenharia Informatica, 2005, ISEP", "Rua das Flores","4444-098","Porto","Portugal", "15-04-2005", tc1, 70, dpt1);
+
+        //act
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> ctrl.updateTeacherWorkingPercentageInTeacherCareerProgression(teacher,date, 50));
+
+        //assert
+        assertEquals(expectedException, exception.getMessage());
     }
 }
