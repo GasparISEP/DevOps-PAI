90a07fb68ee48b7d60b03ef78d98f773fe5775b5
pedrosoares1241921 <1241921@isep.ipp.pt>
2025-01-31 17:06:57 +0000

    update Class Diagram; update attribute in ProgrammeList

--- src/main/java/PAI/domain/ProgrammeList.java
+++ src/main/java/PAI/domain/ProgrammeList.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ProgrammeList {
-    private List<Programme> programmeList = new ArrayList();
+    private List<Programme> programmeList;
 
     public ProgrammeList() {
         programmeList = new ArrayList<>();
