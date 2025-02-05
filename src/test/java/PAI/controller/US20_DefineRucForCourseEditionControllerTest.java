d29ca500f808ef0656701bf6577f75c0a0e245eb
Claudia Valente <1241900@isep.ipp.pt>
2025-02-05 18:17:29 +0000

    US20 Update Test Controller

--- src/test/java/PAI/controller/US20_DefineRucForCourseEditionControllerTest.java
+++ src/test/java/PAI/controller/US20_DefineRucForCourseEditionControllerTest.java
@@ -40,15 +40,50 @@ class US20_DefineRucForCourseEditionControllerTest {
         //Act
         boolean result = ctrl1.defineRucForCourseEdition(cE1, t1);
 
-        //Asser
+        //Assert
         assertTrue(result, "RUC definition should succeed");
+
+
     }
+
+    @Test
+    void shouldNotDefineRucForCourseEdition() throws Exception {
+
+        //Arrange
+        CourseEditionRepository repo1 = new CourseEditionRepository();
+        TeacherRepository repo2 = new TeacherRepository();
+        US20_DefineRucForCourseEditionController ctrl1 = new US20_DefineRucForCourseEditionController(repo1, repo2);
+
+        // Arrange Teacher Ruc
+
+        //Arrange CourseEdition
+        DegreeType master = new DegreeType("Master", 240);
+        Department CSE = new Department("CSE", "Computer Science Engineer");
+        TeacherCategory assistantProfessor = new TeacherCategory("Assistant Professor");
+        Teacher teacher = new Teacher("ABC", "Joe Doe", "abc@isep.ipp.pt", "123456789", "B106", "Doutoramento em Engenharia Informatica, 2005, ISEP", "Rua São Tomé Porto", "4249-015", "Porto", "Portugal", "24-03-2010", assistantProfessor, 80, CSE);
+        CourseRepository courseRepository = new CourseRepository();
+        Programme p1 = new Programme("Computer Engineering", "CE", 20, 6, master, CSE, teacher);
+        Course c1 = new Course("Informatics", "INF", 6, 1);
+        SchoolYear sY1 = new SchoolYear("Ano letivo de", "23-11-2024", "09-12-2025");
+        ProgrammeEdition pE1 = new ProgrammeEdition(p1, sY1);
+        CourseEdition cE1 = new CourseEdition(c1, pE1);
+
+        //Act
+        boolean result = ctrl1.defineRucForCourseEdition(cE1, null);
+
+        //Assert
+        assertTrue(result, "RUC definition should not succeed");
+
+    }
+
     @Test
     void shouldReturnAllTeachersWithSameSize() throws Exception {
-        //ARRANGE
 
+        //Arrange Teacher
         TeacherCategory category = new TeacherCategory("Professor Adjunto");
         Department department = new Department("MAT", "Mathematics");
+
+        //Arrange
         CourseEditionRepository repo1 = new CourseEditionRepository();
         TeacherRepository repo2 = new TeacherRepository();
         US20_DefineRucForCourseEditionController ctrl1 = new US20_DefineRucForCourseEditionController(repo1, repo2);
@@ -63,17 +98,19 @@ class US20_DefineRucForCourseEditionControllerTest {
                 "Rua das Flores","4444-098","Porto","Portugal", "15-04-2005",
                 category, 70, department);
 
-        //ACT
+        //Act
         List<Teacher> result = ctrl1.getTeachers();
 
-        //
+        //Assert
         assertEquals(2, result.size());
 
 
     }
+
     @Test
-    void shouldReturnFalseIfRucIsNotSet() throws Exception {
-        //ARRANGE
+    void shouldReturn2CourseEditions() throws Exception {
+
+        //Arrange
 
         //TeacherCategory category = new TeacherCategory("Professor Adjunto");
 
@@ -94,13 +131,14 @@ class US20_DefineRucForCourseEditionControllerTest {
         SchoolYear sY1 = new SchoolYear("Ano letivo de", "23-11-2024", "09-12-2025");
         ProgrammeEdition pE1 = new ProgrammeEdition(p1, sY1);
 
-
+        //Arrange CourseEdition
         Course c1 = new Course ("Informatics", "INF", 6, 1);
         repo1.createCourseEdition(c1, pE1);
 
-
         Course c2 = new Course ("Cidadania", "CID", 6, 1);
         repo1.createCourseEdition(c2, pE1);
+
+        //Act
 
         List<CourseEdition> result = ctrl1.getCourseEditions();
 
