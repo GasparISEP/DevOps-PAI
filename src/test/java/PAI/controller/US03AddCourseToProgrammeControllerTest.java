d7d45edd6679b89d2224e8802f48be19af57f160
Gonçalo Silva <1241908@isep.ipp.pt>
2025-02-07 13:20:59 +0000

    Update tests os US03

--- src/test/java/PAI/controller/US03AddCourseToProgrammeControllerTest.java
+++ src/test/java/PAI/controller/US03AddCourseToProgrammeControllerTest.java
@@ -1,11 +1,11 @@
 package PAI.controller;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertThrows;
-import static org.junit.Assert.assertTrue;
-
 import PAI.domain.*;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
 
 public class US03AddCourseToProgrammeControllerTest {
     
@@ -70,7 +70,7 @@ public class US03AddCourseToProgrammeControllerTest {
     }
 
     @Test
-    void shouldThrowExceptionIfProgrammeIsNull() throws Exception {
+    void shouldThrowExceptionIfProgrammeListIsNull() throws Exception {
         // arrange
         ProgrammeList nullProgrammeList = null;
         CourseRepository courseRepository = new CourseRepository();
@@ -81,7 +81,7 @@ public class US03AddCourseToProgrammeControllerTest {
     }
 
     @Test
-    void shouldThrowExceptionIfCourseIsNull() throws Exception {
+    void shouldThrowExceptionIfCourseListIsNull() throws Exception {
         // arrange
         Department department1 = new Department("DEI", "Departamento EI");
         TeacherCategory teacherCategory1 = new TeacherCategory("categoria1");
@@ -96,6 +96,109 @@ public class US03AddCourseToProgrammeControllerTest {
         //act + assert
         assertThrows(Exception.class, () -> {
             US03AddCourseToProgrammeController.addCourseToProgramme(lei, null);
+        });
+    }
+
+    @Test
+    void shouldReturnSizeOneIfOnlyOneCourseInList() throws Exception {
+        // arrange
+        CourseRepository courseRepository = new CourseRepository();
+        courseRepository.registerCourse("matemática", "MTA", 5, 1);
+        // act
+        List<Course> allCourses = courseRepository.getAllCourses();
+        // assert
+        assertEquals(1, allCourses.size());
+    }
+
+    @Test
+    void shouldReturnCourseInList() throws Exception {
+        // arrange
+        CourseRepository courseRepository = new CourseRepository();
+        courseRepository.registerCourse("matemática", "MTA", 5, 1);
+
+        // act
+        List<Course> allCourses = courseRepository.getAllCourses();
+
+        // assert
+        Course expectedCourse = new Course("matemática", "MTA", 5, 1);
+        assertEquals(expectedCourse, allCourses.get(0));
+    }
+
+    @Test
+    void shouldReturnSizeOneIfOnlyOneProgrammeInList() throws Exception {
+        // arrange
+        Department department1 = new Department("DEI", "Departamento EI");
+        TeacherCategory teacherCategory1 = new TeacherCategory("categoria1");
+        Teacher teacher1 = new Teacher("NSS", "Nuno Silva", "NSS@isep.ipp.pt", "238310710","A123","Doutoramento em Engenharia Informatica, 2005, ISEP","Rua São Tomé Nº100", "4435-696","Gondomar","Portugal","20-12-2010", teacherCategory1, 100,department1);
+        DegreeType degree1 = new DegreeType("Licenciatura",30);
+        ProgrammeList programmeList = new ProgrammeList();
+        programmeList.registerProgramme("Engenharia Informática", "LEI", 30, 2, degree1, department1, teacher1);
+        // act
+        List<Programme> allProgrammes = programmeList.getAllProgrammes();
+        // assert
+        assertEquals(1, allProgrammes.size());
+    }
+
+    @Test
+    void shouldReturnProgrammeInList() throws Exception {
+        // arrange
+        Department department1 = new Department("DEI", "Departamento EI");
+        TeacherCategory teacherCategory1 = new TeacherCategory("categoria1");
+        Teacher teacher1 = new Teacher("NSS", "Nuno Silva", "NSS@isep.ipp.pt", "238310710","A123","Doutoramento em Engenharia Informatica, 2005, ISEP","Rua São Tomé Nº100", "4435-696","Gondomar","Portugal","20-12-2010", teacherCategory1, 100,department1);
+        DegreeType degree1 = new DegreeType("Licenciatura",30);
+        ProgrammeList programmeList = new ProgrammeList();
+        programmeList.registerProgramme("Engenharia Informática", "LEI", 30, 2, degree1, department1, teacher1);
+
+        // act
+        List<Programme> allProgrammes = programmeList.getAllProgrammes();
+
+        // assert
+        Programme expectedProgramme = new Programme("Engenharia Informática", "LEI", 30, 2, degree1, department1, teacher1);
+        assertEquals(expectedProgramme, allProgrammes.get(0));
+    }
+
+    @Test
+    void shouldReturnAllProgrammes() throws Exception {
+        // arrange
+        Department department1 = new Department("DEI", "Departamento EI");
+        TeacherCategory teacherCategory1 = new TeacherCategory("categoria1");
+        Teacher teacher1 = new Teacher("NSS", "Nuno Silva", "NSS@isep.ipp.pt", "238310710","A123","Doutoramento em Engenharia Informatica, 2005, ISEP","Rua São Tomé Nº100", "4435-696","Gondomar","Portugal","20-12-2010", teacherCategory1, 100,department1);
+        DegreeType degree1 = new DegreeType("Licenciatura",30);
+        ProgrammeList programmeList = new ProgrammeList();
+        programmeList.registerProgramme("Engenharia Informática", "LEI", 30, 2, degree1, department1, teacher1);
+
+        // act
+        US03_AddCourseToProgrammeController controller = new US03_AddCourseToProgrammeController(programmeList, new CourseRepository());
+        List<Programme> allProgrammes = controller.getAllProgrammes();
+
+        // assert
+        Programme expectedProgramme = new Programme("Engenharia Informática", "LEI", 30, 2, degree1, department1, teacher1);
+        assertEquals(expectedProgramme, allProgrammes.get(0));
+    }
+
+    @Test
+    void shouldReturnAllCourses() throws Exception {
+        // arrange
+        CourseRepository courseRepository = new CourseRepository();
+        courseRepository.registerCourse("matemática", "MTA", 5, 1);
+
+        // act
+        US03_AddCourseToProgrammeController controller = new US03_AddCourseToProgrammeController(new ProgrammeList(), courseRepository);
+        List<Course> allCourses = controller.getAllCourses();
+
+        // assert
+        Course expectedCourse = new Course("matemática", "MTA", 5, 1);
+        assertEquals(expectedCourse, allCourses.get(0));
+    }
+
+    @Test
+    void shouldThrowExceptionIfCourseRepositoryIsNull() {
+        // arrange
+        ProgrammeList programmeList = new ProgrammeList();
+
+        // act + assert
+        assertThrows(IllegalArgumentException.class, () -> {
+            new US03_AddCourseToProgrammeController(programmeList, null);
         });
     }
 }
