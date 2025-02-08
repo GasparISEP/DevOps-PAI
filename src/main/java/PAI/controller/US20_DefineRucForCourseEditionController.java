2cd7b0f63c75f6bf8c5a7fbde84150908f7f99bd
ema-marinho <1241904@isep.ipp.pt>
2025-02-08 16:34:29 +0000

    changed method getCourseEdition

--- src/main/java/PAI/controller/US20_DefineRucForCourseEditionController.java
+++ src/main/java/PAI/controller/US20_DefineRucForCourseEditionController.java
@@ -4,6 +4,9 @@ import PAI.domain.CourseEdition;
 import PAI.domain.CourseEditionRepository;
 import PAI.domain.Teacher;
 import PAI.domain.TeacherRepository;
+
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class US20_DefineRucForCourseEditionController {
