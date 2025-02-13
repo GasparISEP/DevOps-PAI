7e068611eade0b4176c0f4193a1653c8d1beb4dc
anaconde1241897 <1241897@isep.ipp.pt>
2025-02-13 17:17:34 +0000

    add US06 controller + tests

---
+++ src/main/java/PAI/controller/US06_IWantToUpdateTheDepartmentDirectorOfADepartmentController.java
@@ -0,0 +1,38 @@
+package PAI.controller;
+
+import PAI.domain.Department;
+import PAI.domain.DepartmentRepository;
+import PAI.domain.Teacher;
+
+import java.util.List;
+
+public class US06_IWantToUpdateTheDepartmentDirectorOfADepartmentController {
+
+    private DepartmentRepository _departmentRepository;
+
+    public US06_IWantToUpdateTheDepartmentDirectorOfADepartmentController(DepartmentRepository departmentRepository) {
+
+        validateDepartmentRepository (departmentRepository);
+    }
+
+    public boolean updateDepartmentDirector(Department department, Teacher teacher) {
+        if (department == null || teacher == null ) {
+            return false;
+        }
+
+        _departmentRepository.updateOfDepartmentDirector(department, teacher);
+        return true;
+    }
+
+    private void validateDepartmentRepository(DepartmentRepository departmentRepository) {
+        if (departmentRepository == null) {
+            throw new IllegalArgumentException("Department Repository cannot be null!");
+        }
+
+        this._departmentRepository = departmentRepository;
+    }
+
+    public List<Department> getAllDepartments() {
+        return _departmentRepository.getDepartmentsList();
+    }
+}
