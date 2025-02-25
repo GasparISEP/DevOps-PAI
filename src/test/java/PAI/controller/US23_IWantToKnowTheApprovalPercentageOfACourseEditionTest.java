27249370065bf444c5f8c5c7ded1c5d7e12f4710
Dianaguedes19 <1241903@isep.ipp.pt>
2025-02-25 12:22:02 +0000

    Updated tests of US23 controller

--- src/test/java/PAI/controller/US23_IWantToKnowTheApprovalPercentageOfACourseEditionTest.java
+++ src/test/java/PAI/controller/US23_IWantToKnowTheApprovalPercentageOfACourseEditionTest.java
@@ -38,32 +38,18 @@ class US23_IWantToKnowTheApprovalPercentageOfACourseEditionTest {
         //act
         US23_IWantToKnowTheApprovalPercentageOfACourseEdition controlador1 = new US23_IWantToKnowTheApprovalPercentageOfACourseEdition(gradeStudentRepository);
 
-        DegreeType master = new DegreeType("Master", 240);
-        Department CSE = new Department("CSE", "Computer Science Engineer");
-        Address address1 = new Address("Praceta do Sol, nº19", "3745-144", "Tomar", "Portugal");
-        TeacherCategory tc1 = new TeacherCategory("Professor Adjunto");
-        Department dpt1 = new Department("MAT", "Mathematics");
-        Teacher teacher = new Teacher("ABC", "Joe Doe", "abc@isep.ipp.pt", "123456789", "B106", "Doutoramento em Engenharia Informatica, 2005, ISEP", "Rua das Flores","4444-098","Porto","Portugal", "15-04-2005", tc1, 70, dpt1);
-        Course c1 = new Course("Informatics", "INF", 6, 1);
-        Programme p1 = new Programme("Computer Engineering", "CE", 20, 6, master, CSE, teacher);
-        Programme p2 = new Programme("Computer Science", "CES", 20, 6, master, CSE, teacher);
-        SchoolYear sY1 = new SchoolYear("Ano letivo de", "23-11-2024", "09-12-2025");
-        SchoolYear sY2 = new SchoolYear("Ano letivo de", "23-11-2023", "09-12-2025");
-        ProgrammeEdition pE1 = new ProgrammeEdition(p1, sY1);
-        ProgrammeEdition pE2 = new ProgrammeEdition(p2, sY2);
-        LocalDate currentDate = LocalDate.now();
+        Student student1 = mock(Student.class);
+        Student student2 = mock(Student.class);
+        CourseEdition courseEdition1 = mock(CourseEdition.class);
 
-        CourseEdition courseEdition1 = new CourseEdition(c1, pE1);
-
-        Student student1 = new Student(1, "Rita", "123456789", "963741258", "rita@gmail.com", address1);
-        Student student2 = new Student(2, "João", "123456786", "963741258", "joao@gmail.com", address1);
 
         GradeStudent gradeStudent1 = mock(GradeStudent.class);
         GradeStudent gradeStudent2 = mock(GradeStudent.class);
+        LocalDate localDate = LocalDate.now();
 
 
-        when(gradeStudentFactory.newGradeStudent(8, "10-02-2025", student1, courseEdition1)).thenReturn(gradeStudent1);
-        when(gradeStudentFactory.newGradeStudent(20, "10-02-2025", student2, courseEdition1)).thenReturn(gradeStudent2);
+        when(gradeStudentFactory.newGradeStudent(8, "10-10-2025", student1, courseEdition1)).thenReturn(gradeStudent1);
+        when(gradeStudentFactory.newGradeStudent(20, "10-10-2025", student2, courseEdition1)).thenReturn(gradeStudent2);
 
         when(gradeStudent1.knowGrade()).thenReturn(8.0);
         when(gradeStudent2.knowGrade()).thenReturn(20.0);
@@ -72,11 +58,11 @@ class US23_IWantToKnowTheApprovalPercentageOfACourseEditionTest {
         when(gradeStudent2.hasThisCourseEdition(courseEdition1)).thenReturn(true);
 
 
-        enrollmentRepository.enrollStudentInACourseEdition(student1, courseEdition1,currentDate);
-        enrollmentRepository.enrollStudentInACourseEdition(student2, courseEdition1,currentDate);
+        enrollmentRepository.enrollStudentInACourseEdition(student1, courseEdition1,localDate);
+        enrollmentRepository.enrollStudentInACourseEdition(student2, courseEdition1,localDate);
 
-        gradeStudentRepository.addGradeToStudent(8, "10-02-2025", student1, courseEdition1);
-        gradeStudentRepository.addGradeToStudent(20, "10-02-2025", student2, courseEdition1);
+        gradeStudentRepository.addGradeToStudent(8, "10-10-2025", student1, courseEdition1);
+        gradeStudentRepository.addGradeToStudent(20, "10-10-2025", student2, courseEdition1);
 
 
         double optC1 = controlador1.IWantToKnowTheApprovalPercentageOfACourseEdition(courseEdition1);
