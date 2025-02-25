dde9f18aec57159fd465e28d792f8aaec3bdd6e8
Dianaguedes19 <1241903@isep.ipp.pt>
2025-02-25 08:51:06 +0000

    Updated more tests of GradeStudentTest with the use of mocks

--- src/test/java/PAI/domain/GradeStudentTest.java
+++ src/test/java/PAI/domain/GradeStudentTest.java
@@ -186,23 +186,12 @@ class GradeStudentTest {
 
     void shouldReturnFalseWhenFromDifferentInstances() throws Exception{
         //arrange
-        Address address1 = new Address("Praceta do Sol, nº19", "3745-144", "Tomar", "Portugal");
-        Student student1 = new Student(1, "Rita", "123456789", "963741258", "rita@gmail.com", address1);
-        DegreeType master = new DegreeType("Master", 240);
-        Department CSE = new Department("CSE", "Computer Science Engineer");
-        TeacherCategory assistantProfessor = new TeacherCategory("Assistant Professor");
-        Teacher teacher = new Teacher("ABC", "Joe Doe", "abc@isep.ipp.pt", "123456789", "B106",
-                "Doutoramento em Engenharia Informatica, 2005, ISEP", "Rua São Tomé Porto", "4249-015",
-                "Porto", "Portugal", "24-03-2010", assistantProfessor, 80, CSE);
-        Programme p1 = new Programme("Computer Engineering", "CE", 20, 6, master, CSE, teacher);
-        Course c1 = new Course("Informatics", "INF", 6, 1);
-        Course c2 = new Course("Mathematics", "MATH", 6, 1);
-        SchoolYear sY1 = new SchoolYear("Ano letivo de", "23-11-2024", "09-12-2025");
-        ProgrammeEdition pE1 = new ProgrammeEdition(p1, sY1);
-        CourseEdition courseEdition1 = new CourseEdition(c1, pE1);
-        CourseEdition courseEdition2 = new CourseEdition(c2, pE1);
+        Student student1 = mock(Student.class);
+        CourseEdition courseEdition1 = mock(CourseEdition.class);
+        Teacher teacher = mock(Teacher.class);
+
         GradeStudent gradeStudent = new GradeStudent(18, "10-02-2025", student1, courseEdition1);
-        GradeStudent gradeStudent1 = gradeStudent;
+
         //act
         boolean result = gradeStudent.equals(teacher);
         //assert
@@ -212,21 +201,9 @@ class GradeStudentTest {
 
     void shouldReturnTrueWhenSameContent() throws Exception{
         //arrange
-        Address address1 = new Address("Praceta do Sol, nº19", "3745-144", "Tomar", "Portugal");
-        Student student1 = new Student(1, "Rita", "123456789", "963741258", "rita@gmail.com", address1);
-        DegreeType master = new DegreeType("Master", 240);
-        Department CSE = new Department("CSE", "Computer Science Engineer");
-        TeacherCategory assistantProfessor = new TeacherCategory("Assistant Professor");
-        Teacher teacher = new Teacher("ABC", "Joe Doe", "abc@isep.ipp.pt", "123456789", "B106",
-                "Doutoramento em Engenharia Informatica, 2005, ISEP", "Rua São Tomé Porto", "4249-015",
-                "Porto", "Portugal", "24-03-2010", assistantProfessor, 80, CSE);
-        Programme p1 = new Programme("Computer Engineering", "CE", 20, 6, master, CSE, teacher);
-        Course c1 = new Course("Informatics", "INF", 6, 1);
-        Course c2 = new Course("Mathematics", "MATH", 6, 1);
-        SchoolYear sY1 = new SchoolYear("Ano letivo de", "23-11-2024", "09-12-2025");
-        ProgrammeEdition pE1 = new ProgrammeEdition(p1, sY1);
-        CourseEdition courseEdition1 = new CourseEdition(c1, pE1);
-        CourseEdition courseEdition2 = new CourseEdition(c2, pE1);
+        Student student1 = mock(Student.class);
+        CourseEdition courseEdition1 = mock(CourseEdition.class);
+
         GradeStudent gradeStudent = new GradeStudent(18, "10-02-2025", student1, courseEdition1);
         GradeStudent gradeStudent1 = new GradeStudent(18, "10-02-2025", student1, courseEdition1);
 
@@ -235,51 +212,14 @@ class GradeStudentTest {
         //assert
         assertTrue(result);
     }
-    void shouldReturnFalseWhenDifferentCourseEdition() throws Exception{
-        //arrange
-        Address address1 = new Address("Praceta do Sol, nº19", "3745-144", "Tomar", "Portugal");
-        Student student1 = new Student(1, "Rita", "123456789", "963741258", "rita@gmail.com", address1);
-        DegreeType master = new DegreeType("Master", 240);
-        Department CSE = new Department("CSE", "Computer Science Engineer");
-        TeacherCategory assistantProfessor = new TeacherCategory("Assistant Professor");
-        Teacher teacher = new Teacher("ABC", "Joe Doe", "abc@isep.ipp.pt", "123456789", "B106",
-                "Doutoramento em Engenharia Informatica, 2005, ISEP", "Rua São Tomé Porto", "4249-015",
-                "Porto", "Portugal", "24-03-2010", assistantProfessor, 80, CSE);
-        Programme p1 = new Programme("Computer Engineering", "CE", 20, 6, master, CSE, teacher);
-        Course c1 = new Course("Informatics", "INF", 6, 1);
-        Course c2 = new Course("Mathematics", "MATH", 6, 1);
-        SchoolYear sY1 = new SchoolYear("Ano letivo de", "23-11-2024", "09-12-2025");
-        ProgrammeEdition pE1 = new ProgrammeEdition(p1, sY1);
-        CourseEdition courseEdition1 = new CourseEdition(c1, pE1);
-        CourseEdition courseEdition2 = new CourseEdition(c2, pE1);
-        GradeStudent gradeStudent = new GradeStudent(18, "10-02-2025", student1, courseEdition1);
-        GradeStudent gradeStudent1 = new GradeStudent(18, "10-02-2025", student1, courseEdition2);
 
-        //act
-        boolean result = gradeStudent.equals(gradeStudent1);
-        //assert
-        assertFalse(result);
-    }
     @Test
-
     void shouldReturnFalseWhenDifferentStudent() throws Exception{
         //arrange
-        Address address1 = new Address("Praceta do Sol, nº19", "3745-144", "Tomar", "Portugal");
-        Student student1 = new Student(1, "Rita", "123456789", "963741258", "rita@gmail.com", address1);
-        Student student2 = new Student(2, "Raquel", "223456789", "963741258", "rita@gmail.com", address1);
-        DegreeType master = new DegreeType("Master", 240);
-        Department CSE = new Department("CSE", "Computer Science Engineer");
-        TeacherCategory assistantProfessor = new TeacherCategory("Assistant Professor");
-        Teacher teacher = new Teacher("ABC", "Joe Doe", "abc@isep.ipp.pt", "123456789", "B106",
-                "Doutoramento em Engenharia Informatica, 2005, ISEP", "Rua São Tomé Porto", "4249-015",
-                "Porto", "Portugal", "24-03-2010", assistantProfessor, 80, CSE);
-        Programme p1 = new Programme("Computer Engineering", "CE", 20, 6, master, CSE, teacher);
-        Course c1 = new Course("Informatics", "INF", 6, 1);
-        Course c2 = new Course("Mathematics", "MATH", 6, 1);
-        SchoolYear sY1 = new SchoolYear("Ano letivo de", "23-11-2024", "09-12-2025");
-        ProgrammeEdition pE1 = new ProgrammeEdition(p1, sY1);
-        CourseEdition courseEdition1 = new CourseEdition(c1, pE1);
-        CourseEdition courseEdition2 = new CourseEdition(c2, pE1);
+        Student student1 = mock(Student.class);
+        Student student2 = mock(Student.class);
+        CourseEdition courseEdition1 = mock(CourseEdition.class);
+
         GradeStudent gradeStudent = new GradeStudent(18, "10-02-2025", student1, courseEdition1);
         GradeStudent gradeStudent1 = new GradeStudent(18, "10-02-2025", student2, courseEdition1);
 
@@ -291,24 +231,10 @@ class GradeStudentTest {
 @Test
     void shouldReturnFalseWhenStudentisNull() throws Exception {
     //arrange
-    Address address1 = new Address("Praceta do Sol, nº19", "3745-144", "Tomar", "Portugal");
-    Student student1 = new Student(1, "Rita", "123456789", "963741258", "rita@gmail.com", address1);
-    Student student2 = new Student(2, "Raquel", "223456789", "963741258", "rita@gmail.com", address1);
-    DegreeType master = new DegreeType("Master", 240);
-    Department CSE = new Department("CSE", "Computer Science Engineer");
-    TeacherCategory assistantProfessor = new TeacherCategory("Assistant Professor");
-    Teacher teacher = new Teacher("ABC", "Joe Doe", "abc@isep.ipp.pt", "123456789", "B106",
-            "Doutoramento em Engenharia Informatica, 2005, ISEP", "Rua São Tomé Porto", "4249-015",
-            "Porto", "Portugal", "24-03-2010", assistantProfessor, 80, CSE);
-    Programme p1 = new Programme("Computer Engineering", "CE", 20, 6, master, CSE, teacher);
-    Course c1 = new Course("Informatics", "INF", 6, 1);
-    Course c2 = new Course("Mathematics", "MATH", 6, 1);
-    SchoolYear sY1 = new SchoolYear("Ano letivo de", "23-11-2024", "09-12-2025");
-    ProgrammeEdition pE1 = new ProgrammeEdition(p1, sY1);
-    CourseEdition courseEdition1 = new CourseEdition(c1, pE1);
-    CourseEdition courseEdition2 = new CourseEdition(c2, pE1);
+    Student student1 = mock(Student.class);
+    CourseEdition courseEdition1 = mock(CourseEdition.class);
+
     GradeStudent gradeStudent = new GradeStudent(18, "10-02-2025", student1, courseEdition1);
-    GradeStudent gradeStudent1 = new GradeStudent(18, "10-02-2025", student2, courseEdition1);
 
     //act
     boolean result = gradeStudent.equals(null);
