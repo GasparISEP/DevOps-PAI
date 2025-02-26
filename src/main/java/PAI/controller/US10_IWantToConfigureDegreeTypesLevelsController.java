596292a6ac21463de236a00197ea710a1ef9c0c1
Nuno Cruz <1241919@isep.ipp.pt>
2025-02-26 11:18:16 +0000

    Updated the code for US 10 and added tests for US 10

--- src/main/java/PAI/controller/US10_IWantToConfigureDegreeTypesLevelsController.java
+++ src/main/java/PAI/controller/US10_IWantToConfigureDegreeTypesLevelsController.java
@@ -7,7 +7,11 @@ public class US10_IWantToConfigureDegreeTypesLevelsController {
 
     private DegreeTypeRepository _degreeTypeRepository;
 
-    public US10_IWantToConfigureDegreeTypesLevelsController(DegreeTypeRepository degreeTypeRepository) {
+    public US10_IWantToConfigureDegreeTypesLevelsController(DegreeTypeRepository degreeTypeRepository) throws Exception {
+
+        if (degreeTypeRepository == null) {
+            throw new Exception("DegreeType Repository cannot be null");
+        }
         _degreeTypeRepository = degreeTypeRepository;
     }
 
---
+++ src/test/java/PAI/controller/US10_IWantToConfigureDegreeTypesLevelsControllerTest.java
@@ -0,0 +1,47 @@
+package PAI.controller;
+
+import PAI.domain.DegreeTypeRepository;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class US10_IWantToConfigureDegreeTypesLevelsControllerTest {
+
+    @Test
+    void newDegreeTypeRepository() throws Exception {
+        //arrange
+        DegreeTypeRepository degreeTypeRepository = new DegreeTypeRepository();
+
+        //act
+        US10_IWantToConfigureDegreeTypesLevelsController degreeType1 = new US10_IWantToConfigureDegreeTypesLevelsController(degreeTypeRepository);
+
+        //assert
+        assertNotNull(degreeType1);
+    }
+
+    @Test
+    void nullDegreeTypeRepository() {
+        //arrange
+        DegreeTypeRepository degreeTypeRepository = null;
+
+        //act & assert
+        Exception exception = assertThrows(Exception.class, () ->
+                new US10_IWantToConfigureDegreeTypesLevelsController(degreeTypeRepository)
+        );
+        assertEquals("DegreeType Repository cannot be null", exception.getMessage());
+    }
+
+    @Test
+    void registerDegreeTypeInSystem() throws Exception {
+        //arrange
+        DegreeTypeRepository degreeTypeRepository = new DegreeTypeRepository();
+
+        //act
+        US10_IWantToConfigureDegreeTypesLevelsController controller1 = new US10_IWantToConfigureDegreeTypesLevelsController(degreeTypeRepository);
+
+        boolean optC1 = controller1.registerDegreeType("Master", 30);
+
+        assertTrue(optC1);
+    }
+
+}
