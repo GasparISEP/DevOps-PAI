be5f3a8dca16833c83257ca13ddf393bdb5857ee
Claudia Valente <1241900@isep.ipp.pt>
2025-02-26 19:17:12 +0000

    upd (courseRepository): updating tests

--- src/test/java/PAI/domain/CourseRepositoryTest.java
+++ src/test/java/PAI/domain/CourseRepositoryTest.java
@@ -2,8 +2,12 @@ package PAI.domain;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class CourseRepositoryTest {
 
@@ -12,7 +16,7 @@ class CourseRepositoryTest {
         //arrange
         CourseFactory courseFactory = mock(CourseFactory.class);
         //act
-        CourseRepository cR1 = new CourseRepository (courseFactory);
+        CourseRepository cR1 = new CourseRepository(courseFactory);
         //assert
         assertNotNull(cR1);
     }
@@ -21,22 +25,74 @@ class CourseRepositoryTest {
     void shouldReturnTrueIfCourseIsRegistered() throws Exception {
         //arrange
         CourseFactory courseFactory = mock(CourseFactory.class);
-        CourseRepository cR1 = new CourseRepository (courseFactory);
+        CourseRepository cR1 = new CourseRepository(courseFactory);
+
+        String courseName = "Matemática";
+        String acronym = "MAT";
+        double quantityCreditsEcts = 10;
+        int durationCourseInSemester = 1;
+
+        Course courseMock = mock(Course.class);
+
+        when(courseFactory.createCourse(courseName, acronym, quantityCreditsEcts, durationCourseInSemester)).thenReturn(courseMock);
+
         //act
-        boolean result = cR1.registerCourse("Matemática", "MAT", 10, 1);
+        boolean result = cR1.registerCourse(courseName, acronym, quantityCreditsEcts, durationCourseInSemester);
         //assert
         assertTrue(result);
     }
-
     @Test
-    void shouldReturnFalseIfAccessMethodIsNotRegistered() throws Exception {
+    void shouldReturnFalseIfCourseIsNotRegistered() throws Exception {
         //arrange
         CourseFactory courseFactory = mock(CourseFactory.class);
-        CourseRepository cR1 = new CourseRepository (courseFactory);
-        cR1.registerCourse("Matemática", "MAT", 10, 1);
+        CourseRepository courseRepository = new CourseRepository(courseFactory);
+
+        String courseName1 = "Matemática";
+        String acronym1 = "MAT";
+        double quantityCreditsECTS1 = 10;
+        int durationCourseInSemester1 = 1;
+        Course course1 = new Course(courseName1, acronym1, quantityCreditsECTS1, durationCourseInSemester1);
+
+        when(courseFactory.createCourse(courseName1, acronym1, quantityCreditsECTS1, durationCourseInSemester1)).thenReturn(course1);
+        courseRepository.registerCourse(courseName1, acronym1, quantityCreditsECTS1, durationCourseInSemester1);
+
         //act
-        boolean result = cR1.registerCourse("Matemática", "MAT", 10, 1);
+        boolean result = courseRepository.registerCourse(courseName1, acronym1, quantityCreditsECTS1, durationCourseInSemester1);
         //assert
         assertFalse(result);
+    }
+
+    @Test
+    void shouldReturnCategoryListWithRegisteredCourses() throws Exception {
+        //arrange
+        CourseFactory courseFactory = mock(CourseFactory.class);
+        CourseRepository courseRepository = new CourseRepository(courseFactory);
+
+        String courseName1 = "Matemática";
+        String acronym1 = "MAT";
+        double quantityCreditsECTS1 = 10;
+        int durationCourseInSemester1 = 1;
+        Course course1 = new Course(courseName1, acronym1, quantityCreditsECTS1, durationCourseInSemester1);
+
+        String courseName2 = "Português";
+        String acronym2 = "PT";
+        double quantityCreditsECTS2 = 8;
+        int durationCourseInSemester2 = 1;
+        Course course2 = new Course(courseName2, acronym2, quantityCreditsECTS2, durationCourseInSemester2);
+
+        when(courseFactory.createCourse(courseName1, acronym1, quantityCreditsECTS1, durationCourseInSemester1)).thenReturn(course1);
+        when(courseFactory.createCourse(courseName2, acronym2, quantityCreditsECTS2, durationCourseInSemester2)).thenReturn(course2);
+
+        courseRepository.registerCourse(courseName1, acronym1, quantityCreditsECTS1, durationCourseInSemester1);
+        courseRepository.registerCourse(courseName2, acronym2, quantityCreditsECTS2, durationCourseInSemester2);
+
+        //Act
+
+        ArrayList<Course> listCourses = courseRepository.getAllCourses();
+
+        // Assert
+        assertEquals(2, listCourses.size());
+        assertTrue(listCourses.contains(course1));
+        assertTrue(listCourses.contains(course2));
     }
 }
