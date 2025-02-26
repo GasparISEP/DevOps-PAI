12b350cccfc44d9bd38351aa9a71244ab61a9845
Claudia Valente <1241900@isep.ipp.pt>
2025-02-26 10:56:53 +0000

    feat(US03): create CourseFactoryTest

---
+++ src/test/java/PAI/domain/CourseFactoryTest.java
@@ -0,0 +1,20 @@
+package PAI.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CourseFactoryTest {
+
+    @Test
+    void shouldCreateCourse() {
+
+    }
+
+    @Test
+    void shouldNotCreateCourse(){
+
+    }
+
+
+}
