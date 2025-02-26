36eb0da5b64ee2c0437add6b69de043b90267c85
ma.antunes <1241916@isep.ipp.pt>
2025-02-26 12:35:10 +0000

    fix(courseEdition)remove CourseRepository in us16 test

--- src/test/java/PAI/domain/CourseEditionTest.java
+++ src/test/java/PAI/domain/CourseEditionTest.java
@@ -167,7 +167,6 @@ class CourseEditionTest {
         Department CSE = new Department("CSE", "Computer Science Engineer");
         TeacherCategory assistantProfessor = new TeacherCategory("Assistant Professor");
         Teacher teacher = new Teacher("ABC", "Joe Doe", "abc@isep.ipp.pt", "123456789", "B106", "Doutoramento em Engenharia Informatica, 2005, ISEP", "Rua São Tomé Porto", "4249-015", "Porto", "Portugal", "24-03-2010", assistantProfessor, 80, CSE);
-        CourseRepository courseRepository = new CourseRepository();
         Programme p1 = new Programme("Computer Engineering", "CE", 20, 6, master, CSE, teacher);
         Course c1 = new Course("Informatics", "INF", 6, 1);
         SchoolYear sY1 = new SchoolYear("Ano letivo de", "23-11-2024", "09-12-2025");
