d845c35a7d61e21a247a984c62fc9882979eb951
JoseGDuarte <1241914@isep.ipp.pt>
2025-02-26 16:50:49 +0000

    Isolation of the US14 Controller and tests

--- src/test/java/PAI/controller/US14UpdateTeachersCategoryControllerTest.java
+++ src/test/java/PAI/controller/US14UpdateTeachersCategoryControllerTest.java
@@ -1,47 +1,46 @@
 package PAI.controller;
-
 import PAI.domain.*;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+
 import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 
 import java.util.Optional;
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.mock;
-
 class US14UpdateTeachersCategoryControllerTest {
 
-    private TeacherCategoryRepository tcr1;
-    private TeacherRepository tr1;
+    private static TeacherCategoryRepository tcr1;
+    private static TeacherRepository tr1;
+    private static TeacherCategoryRepositoryFactory teacherCategoryRepositoryFactory;
+    private static TeacherRepositoryFactory teacherRepositoryFactory;
 
-    @BeforeEach
-    void setUp() throws Exception {
-        //Create Teacher Categories and add to Repository
-        TeacherCategoryFactory doubleTeacherCategoryFactory = mock(TeacherCategoryFactory.class);
-        tcr1 = new TeacherCategoryRepository(doubleTeacherCategoryFactory);
-        tcr1.registerTeacherCategory("Assistente");
-        tcr1.registerTeacherCategory("Efectivo");
-        Optional<TeacherCategory> optionalTeacherCategory1 = tcr1.getTeacherCategoryByName("Assistente");
-        TeacherCategory tc1 = optionalTeacherCategory1.get();
-        //Create Department
-        Department dpt1 = new Department("ENC", "Engenharia Civil");
-        //Create Teacher with tc1 and add to Repository
-        tr1 = new TeacherRepository();
-        tr1.registerTeacher("EDC", "Eugénio Cardoso", "edc@isep.ipp.pt", "213784542",
-                "B106", "Mestrado Engenharia, ISEP", "Rua da Pedra", "4300-020","Porto",
-                "Portugal", "28-01-2025", tc1, 75, dpt1);
+    @BeforeAll
+    static void setUp() throws Exception {
+
+        teacherCategoryRepositoryFactory = mock(TeacherCategoryRepositoryFactory.class);
+        teacherRepositoryFactory = mock(TeacherRepositoryFactory.class);
+
+        tcr1 = mock(TeacherCategoryRepository.class);
+        tr1 = mock(TeacherRepository.class);
+
+        when(teacherCategoryRepositoryFactory.newTeacherCategoryRepository()).thenReturn(tcr1);
+        when(teacherRepositoryFactory.newTeacherRepository()).thenReturn(tr1);
+
     }
 
     @Test
     void shouldCreateUpdateTeachersCategoryController() {
-        US14_UpdateTeachersCategoryController controller1 = new US14_UpdateTeachersCategoryController(tr1, tcr1);
+        US14_UpdateTeachersCategoryController controller1 = new US14_UpdateTeachersCategoryController(teacherRepositoryFactory, teacherCategoryRepositoryFactory);
     }
-
 
     static Stream<Arguments> testValues() {
         return Stream.of(
@@ -59,26 +58,61 @@ class US14UpdateTeachersCategoryControllerTest {
     @ParameterizedTest
     @MethodSource("testValues")
     void inputsAreNullOrBlank_UnsuccessfullyUpdatedCategory(String date, String teacherNIF, String teacherCategoryName) {
-        US14_UpdateTeachersCategoryController controller1 = new US14_UpdateTeachersCategoryController(tr1, tcr1);
+        US14_UpdateTeachersCategoryController controller1 = new US14_UpdateTeachersCategoryController(teacherRepositoryFactory, teacherCategoryRepositoryFactory);
         assertThrows(IllegalArgumentException.class, () -> controller1.updateTeacherCategory(date, teacherNIF, teacherCategoryName));
     }
 
     @Test
-    void successfullyUpdatedTeachersCategory() {
-        US14_UpdateTeachersCategoryController controller1 = new US14_UpdateTeachersCategoryController(tr1, tcr1);
+    void successfullyUpdatedTeachersCategory() throws Exception {
+
+        TeacherCategory tc1 = new TeacherCategory("Assistente");
+        TeacherCategory tc2 = new TeacherCategory("Efectivo");
+
+        when(tcr1.getTeacherCategoryByName("Assistente")).thenReturn(Optional.of(tc1));
+        when(tcr1.getTeacherCategoryByName("Efectivo")).thenReturn(Optional.of(tc2));
+
+
+        Teacher teacher = new Teacher("EDC", "Eugénio Cardoso", "edc@isep.ipp.pt", "213784542", "B106", "Mestrado Engenharia, ISEP",
+                "Rua da Pedra", "4300-020", "Porto", "Portugal", "28-01-2025", tc1, 75, new Department("ENC", "Engenharia Civil"));
+
+        when(tr1.getTeacherByNIF("213784542")).thenReturn(Optional.of(teacher));
+
+        when(teacherCategoryRepositoryFactory.newTeacherCategoryRepository()).thenReturn(tcr1);
+        when(teacherRepositoryFactory.newTeacherRepository()).thenReturn(tr1);
+
+        US14_UpdateTeachersCategoryController controller1 = new US14_UpdateTeachersCategoryController(teacherRepositoryFactory, teacherCategoryRepositoryFactory);
+
         boolean result = controller1.updateTeacherCategory("30-01-2025", "213784542", "Efectivo");
+
         assertTrue(result);
     }
 
     @Test
     void noTeacherInRepoWithInputNIF_UnsuccessfullyUpdatedTeachersCategory() {
-        US14_UpdateTeachersCategoryController controller1 = new US14_UpdateTeachersCategoryController(tr1, tcr1);
+
+        when(tr1.getTeacherByNIF("111111111")).thenReturn(Optional.empty());
+
+        when(teacherCategoryRepositoryFactory.newTeacherCategoryRepository()).thenReturn(tcr1);
+        when(teacherRepositoryFactory.newTeacherRepository()).thenReturn(tr1);
+
+        US14_UpdateTeachersCategoryController controller1 = new US14_UpdateTeachersCategoryController(teacherRepositoryFactory, teacherCategoryRepositoryFactory);
+
         assertThrows(IllegalArgumentException.class, () -> controller1.updateTeacherCategory("30-01-2025", "111111111", "Efectivo"));
     }
 
     @Test
-    void noTeacherCategoryInRepoWithInputTeacherCategoryName_UnsuccessfullyUpdateddTeachersCategory() {
-        US14_UpdateTeachersCategoryController controller1 = new US14_UpdateTeachersCategoryController(tr1, tcr1);
+    void noTeacherCategoryInRepoWithInputTeacherCategoryName_UnsuccessfullyUpdateddTeachersCategory() throws Exception {
+
+        TeacherCategory tc1 = new TeacherCategory("Assistente");
+
+        Teacher teacher = new Teacher("EDC", "Eugénio Cardoso", "edc@isep.ipp.pt", "213784542", "B106", "Mestrado Engenharia, ISEP",
+                "Rua da Pedra", "4300-020", "Porto", "Portugal", "28-01-2025", tc1, 75, new Department("ENC", "Engenharia Civil"));
+
+        when(tr1.getTeacherByNIF("213784542")).thenReturn(Optional.of(teacher));
+        when(tcr1.getTeacherCategoryByName("Doutor")).thenReturn(Optional.empty());
+
+        US14_UpdateTeachersCategoryController controller1 = new US14_UpdateTeachersCategoryController(teacherRepositoryFactory, teacherCategoryRepositoryFactory);
+
         assertThrows(IllegalArgumentException.class, () -> controller1.updateTeacherCategory("30-01-2025", "213784542", "Doutor"));
     }
 }
