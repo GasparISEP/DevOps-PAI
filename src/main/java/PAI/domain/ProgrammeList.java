e152617c47af45c555aa809228255a81dd77b929
Claudia Valente <1241900@isep.ipp.pt>
2025-02-27 17:29:22 +0000

    upd (): update ProgrammeList, ProgrammeListTest, Programme

--- src/main/java/PAI/domain/ProgrammeList.java
+++ src/main/java/PAI/domain/ProgrammeList.java
@@ -15,7 +15,7 @@ public class ProgrammeList {
 
     public boolean registerProgramme(String name, String acronym, int quantityOfEcts, int quantityOfSemesters, DegreeType degreeType, Department department, Teacher programmeDirector) throws Exception {
 
-        Programme programme = _programmeFactory.registerProgramme (name, acronym, quantityOfEcts, quantityOfSemesters, degreeType, department, programmeDirector);
+        Programme programme = _programmeFactory.registerProgramme(name, acronym, quantityOfEcts, quantityOfSemesters, degreeType, department, programmeDirector);
 
         if (programmeList.contains(programme))
             return false;
@@ -38,16 +38,25 @@ public class ProgrammeList {
         return new ArrayList<>(programmeList);
     }
 
-    public List<Course> getCourseList(Programme programme){
+    public List<Course> getCourseList(Programme programme) {
         return programme.getCourseList();
     }
 
     public Optional<Programme> getProgrammeByName(String name) {
-        for ( Programme programme : programmeList) {
-            if ( programme.hasThisProgrammeName(name)){
+        for (Programme programme : programmeList) {
+            if (programme.hasThisProgrammeName(name)) {
                 return Optional.of(programme);
             }
         }
         return Optional.empty();
+    }
+
+    public Programme getProgrammeByAcronym(String acronym) {
+        for (Programme programme : programmeList) {
+            if (programme.getAcronym().equals(acronym)){
+                return programme;
+            }
+        }
+        return null;
     }
 }
