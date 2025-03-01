65bbafa1c44dc988f1a6ac4b7b45b0df6ae72c64
Joao Coutinho <1241910@isep.ipp.pt>
2025-03-01 10:34:14 +0000

    Created StudentListFactory, updated StudentRepository to support that new factory, updated StudentRepositoryTest's tests and added incorrectly replaced integration tests

---
+++ src/main/java/PAI/domain/StudentListFactory.java
@@ -0,0 +1,10 @@
+package PAI.domain;
+
+import java.util.ArrayList;
+
+public class StudentListFactory {
+
+    public ArrayList<Student> newArrayList() {
+        return new ArrayList<>();
+    }
+}
--- src/main/java/PAI/domain/StudentRepository.java
+++ src/main/java/PAI/domain/StudentRepository.java
@@ -8,12 +8,13 @@ public class StudentRepository {
 
     private StudentFactory _studentFactory;
     // Create ArrayList to store students
-    private List<Student> _students = new ArrayList<>();
+    private List<Student> _students;
 
-    // Creates a default StudentFactory and calls the next constructor (constructor delegation)
-    public StudentRepository() { this(new StudentFactory()); }
-    // Constructor to be directly used for isolated testing
-    public StudentRepository(StudentFactory studentFactory) { _studentFactory = studentFactory; }
+
+    public StudentRepository(StudentFactory studentFactory, StudentListFactory studentListFactory) {
+        _studentFactory = studentFactory;
+        _students = studentListFactory.newArrayList();
+    }
 
     public boolean registerStudent(int uniqueNumber, String name, String NIF, String phone, String email, Address address) throws Exception {
 
@@ -35,7 +36,9 @@ public class StudentRepository {
         }
         return false;
     }
-
+    // Creates a default StudentFactory and StudentListFactory and calls the next constructor (constructor delegation)
+    public StudentRepository() { this(new StudentFactory(), new StudentListFactory()); }
+    // Constructor to be directly used for isolated testing
     public Optional<Student> getStudentByUniqueNumber(int uniqueNumber) {
         for ( Student student : _students) {
             if ( student.hasThisUniqueNumber(uniqueNumber)){
