08dc28fc5e3c3e870a1d0739d96fcffb55ef2211
jmariz-costa <1241912@isep.ipp.pt>
2025-03-03 09:28:30 +0000

    added ProgrammeEditionListFactory class

---
+++ src/main/java/PAI/domain/ProgrammeEditionListFactory.java
@@ -0,0 +1,11 @@
+package PAI.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProgrammeEditionListFactory {
+
+    public List<ProgrammeEdition> getProgrammeEditionList() {
+        return new ArrayList<>();
+    }
+}
