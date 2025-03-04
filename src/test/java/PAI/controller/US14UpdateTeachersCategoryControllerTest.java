0e6b8b7902d9c7155ff55f7d9aab6408d5ee2441
JoseGDuarte <1241914@isep.ipp.pt>
2025-03-04 12:39:15 +0000

    Updated US14Controller tests

--- src/test/java/PAI/controller/US14UpdateTeachersCategoryControllerTest.java
+++ src/test/java/PAI/controller/US14UpdateTeachersCategoryControllerTest.java
@@ -55,33 +55,21 @@ class US14UpdateTeachersCategoryControllerTest {
 
     @Test
     void successfullyUpdatedTeachersCategory() throws Exception {
-
         // Arrange
-        Department dp1 = mock(Department.class);
-        TeacherCategoryFactory tcf1 = mock(TeacherCategoryFactory.class);
+
         TeacherCategory tc1 = new TeacherCategory("Assistente");
         TeacherCategory tc2 = new TeacherCategory("Efectivo");
-        AddressFactory addressFactoryDouble = mock(AddressFactory.class);
-        TeacherCareerProgressionFactory TCPfactoryDouble = mock(TeacherCareerProgressionFactory.class);
 
-        when(tcf1.createTeacherCategory("Assistente")).thenReturn(tc1);
-        when(tcf1.createTeacherCategory("Efectivo")).thenReturn(tc2);
-
-        TeacherFactory t1 = mock(TeacherFactory.class);
         Teacher teacher = mock(Teacher.class);
-
-        when(t1.createTeacher("EDC", "Eugénio Cardoso", "edc@isep.ipp.pt", "213784542", "B106", "Mestrado Engenharia, ISEP",
-                "Rua da Pedra", "4300-020", "Porto", "Portugal", addressFactoryDouble, "28-01-2025", tc1, 75, dp1, TCPfactoryDouble))
-                .thenReturn(teacher);
 
         when(tr1.getTeacherByNIF("213784542")).thenReturn(Optional.of(teacher));
         when(tcr1.getTeacherCategoryByName("Assistente")).thenReturn(Optional.of(tc1));
         when(tcr1.getTeacherCategoryByName("Efectivo")).thenReturn(Optional.of(tc2));
 
-        US14_UpdateTeachersCategoryController controller1 = new US14_UpdateTeachersCategoryController(tr1, tcr1);
+        US14_UpdateTeachersCategoryController controller = new US14_UpdateTeachersCategoryController(tr1, tcr1);
 
         // Act
-        boolean result = controller1.updateTeacherCategory("30-01-2025", "213784542", "Efectivo");
+        boolean result = controller.updateTeacherCategory("30-01-2025", "213784542", "Efectivo");
 
         // Assert
         assertTrue(result);
@@ -89,37 +77,28 @@ class US14UpdateTeachersCategoryControllerTest {
 
     @Test
     void noTeacherInRepoWithInputNIF_UnsuccessfullyUpdatedTeachersCategory() {
+        // Arrange
 
         when(tr1.getTeacherByNIF("111111111")).thenReturn(Optional.empty());
 
-        US14_UpdateTeachersCategoryController controller1 = new US14_UpdateTeachersCategoryController(tr1, tcr1);
+        US14_UpdateTeachersCategoryController controller = new US14_UpdateTeachersCategoryController(tr1, tcr1);
 
-        assertThrows(IllegalArgumentException.class, () -> controller1.updateTeacherCategory("30-01-2025", "111111111", "Efectivo"));
+        // Act & Assert
+        assertThrows(IllegalArgumentException.class, () -> controller.updateTeacherCategory("30-01-2025", "111111111", "Efectivo"));
     }
 
     @Test
     void noTeacherCategoryInRepoWithInputTeacherCategoryName_UnsuccessfullyUpdatedTeachersCategory() throws Exception {
+        // Arrange
 
-        Department dp1 = mock(Department.class);
-        TeacherCareerProgressionFactory TCPfactoryDouble = mock(TeacherCareerProgressionFactory.class);
-        TeacherCategoryFactory tcf1 = mock(TeacherCategoryFactory.class);
-        TeacherCategory tc1 = new TeacherCategory("Assistente");
-        AddressFactory addressFactoryDouble = mock(AddressFactory.class);
-
-        when(tcf1.createTeacherCategory("Assistente")).thenReturn(tc1);
-
-
-        TeacherFactory t1 = mock(TeacherFactory.class);
         Teacher teacher = mock(Teacher.class);
-        when(t1.createTeacher("EDC", "Eugénio Cardoso", "edc@isep.ipp.pt", "213784542", "B106", "Mestrado Engenharia, ISEP",
-                "Rua da Pedra", "4300-020", "Porto", "Portugal", addressFactoryDouble, "28-01-2025", tc1, 75, dp1, TCPfactoryDouble))
-                .thenReturn(teacher);
 
         when(tr1.getTeacherByNIF("213784542")).thenReturn(Optional.of(teacher));
         when(tcr1.getTeacherCategoryByName("Doutor")).thenReturn(Optional.empty());
 
-        US14_UpdateTeachersCategoryController controller1 = new US14_UpdateTeachersCategoryController(tr1, tcr1);
+        US14_UpdateTeachersCategoryController controller = new US14_UpdateTeachersCategoryController(tr1, tcr1);
 
-        assertThrows(IllegalArgumentException.class, () -> controller1.updateTeacherCategory("30-01-2025", "213784542", "Doutor"));
+        // Act & Assert
+        assertThrows(IllegalArgumentException.class, () -> controller.updateTeacherCategory("30-01-2025", "213784542", "Doutor"));
     }
 }
