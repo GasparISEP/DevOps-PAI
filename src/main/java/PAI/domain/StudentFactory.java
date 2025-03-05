a1435d381e06aec340259c6729cf92dbe5d90540
Joao Coutinho <1241910@isep.ipp.pt>
2025-03-05 16:03:25 +0000

    Created StudentFactoryInterface and StudentListFactoryInterface and changed StudentFactory and StudentListFactory to implement the respective Interfaces

--- src/main/java/PAI/domain/StudentFactory.java
+++ src/main/java/PAI/domain/StudentFactory.java
@@ -1,8 +1,8 @@
 package PAI.domain;
 
-public class StudentFactory {
+public class StudentFactory implements StudentFactoryInterface{
 
-    public Student newStudent(int uniqueNumber, String name, String NIF, String phone, String email, Address address) throws Exception {
+    public Student newStudent(int uniqueNumber, String name, String NIF, String phone, String email, Address address) {
         return new Student(uniqueNumber, name, NIF, phone, email, address);
     }
 }
--- src/main/java/PAI/domain/StudentListFactory.java
+++ src/main/java/PAI/domain/StudentListFactory.java
@@ -2,7 +2,7 @@ package PAI.domain;
 
 import java.util.ArrayList;
 
-public class StudentListFactory {
+public class StudentListFactory implements StudentListFactoryInterface {
 
     public ArrayList<Student> newArrayList() {
         return new ArrayList<>();
