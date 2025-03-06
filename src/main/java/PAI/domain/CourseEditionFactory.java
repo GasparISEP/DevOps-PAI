44b9a790b315e6a7f658ec6c62bad687cff3f166
ma.antunes <1241916@isep.ipp.pt>
2025-03-06 14:03:48 +0000

    refactor(general): refactor directory

--- src/main/java/PAI/domain/CourseEditionFactory.java
+++
@@ -1,8 +0,0 @@
-package PAI.domain;
-
-public class CourseEditionFactory {
-
-    public CourseEdition newCourseEdition (Course course, ProgrammeEdition programmeEdition) throws Exception {
-        return new CourseEdition(course, programmeEdition);
-    }
-}
--- src/test/java/PAI/controller/US20_DefineRucForCourseEditionControllerTest.java
+++ src/test/java/PAI/controller/US20_DefineRucForCourseEditionControllerTest.java
@@ -1,6 +1,8 @@
 package PAI.controller;
 
 import PAI.domain.*;
+import PAI.factory.CourseEditionFactory;
+import PAI.factory.CourseEditionListFactory;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
