3fe55746808dc5e8e1096a08ec833f4320428cee
anaconde1241897 <1241897@isep.ipp.pt>
2025-03-06 17:30:21 +0000

    US16 - remove enrollmentDate in CourseEditionEnrollmentFactoryInterface

--- src/main/java/PAI/factory/CourseEditionEnrollmentFactoryInterface.java
+++ src/main/java/PAI/factory/CourseEditionEnrollmentFactoryInterface.java
@@ -8,5 +8,5 @@ import java.time.LocalDate;
 
 public interface CourseEditionEnrollmentFactoryInterface {
 
-    CourseEditionEnrollment createCourseEditionEnrollment(Student student, CourseEdition courseEdition, LocalDate enrollmentDate);
+    CourseEditionEnrollment createCourseEditionEnrollment(Student student, CourseEdition courseEdition);
 }
