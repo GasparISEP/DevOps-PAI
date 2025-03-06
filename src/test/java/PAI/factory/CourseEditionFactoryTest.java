f2f8c5387893bfaf68eca8dfc3509034c571f97d
ma.antunes <1241916@isep.ipp.pt>
2025-03-06 17:35:28 +0000

    refactor(test): refactor CourseEditionFactory tests

--- src/test/java/PAI/factory/CourseEditionFactoryTest.java
+++ src/test/java/PAI/factory/CourseEditionFactoryTest.java
@@ -4,27 +4,46 @@ import PAI.domain.Course;
 import PAI.domain.CourseEdition;
 import PAI.domain.ProgrammeEdition;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedConstruction;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 
 class CourseEditionFactoryTest {
 
     @Test
-    void shouldReturnCourseEditionSuccessfully() throws Exception {
+    void shouldCreateCourseEditionWhenConstructorIsCalled() throws Exception {
         //SUT = CourseEditionfactory - ProgrammeEdition and Course Isolated
         //Arrange
-        CourseEditionFactory courseEditionFactory = new CourseEditionFactory();
-
+            //Doubles' instantiation
         ProgrammeEdition programmeEditionDouble = mock(ProgrammeEdition.class);
         Course courseDouble = mock (Course.class);
 
-        //Act
-        CourseEdition result = courseEditionFactory.newCourseEdition(courseDouble, programmeEditionDouble);
+            //SUT
+        CourseEditionFactory courseEditionFactory = new CourseEditionFactory();
 
-        //Assert
-        assertNotNull(result);
 
+        try (MockedConstruction<CourseEdition> courseEditionDouble = mockConstruction(CourseEdition.class,(courseEditionMock, context) -> {
+
+            when(courseEditionMock.getCourse()).thenReturn(courseDouble);
+            when(courseEditionMock.getProgrammeEdition()).thenReturn(programmeEditionDouble);
+        })) {
+
+            // Act
+            CourseEdition result = courseEditionFactory.newCourseEdition(courseDouble, programmeEditionDouble);
+
+            // Assert
+            assertNotNull(result);
+            assertEquals(courseDouble, result.getCourse());
+            assertEquals(programmeEditionDouble, result.getProgrammeEdition());
+
+            List<CourseEdition> courseEditions = courseEditionDouble.constructed();
+            assertEquals(1, courseEditions.size());
+
+            assertEquals(result, courseEditions.get(0));
+        }
     }
 
     @Test
