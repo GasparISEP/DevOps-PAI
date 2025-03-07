6e9f360e1f87c92f27ffe4ea7be948d0e6d5dad5
FAlmeida-switch <1241906@isep.ipp.pt>
2025-03-07 16:28:13 +0000

    Created TeacherCareerProgressionListFactory and its Interface, aswell as tests.

---
+++ src/main/java/PAI/factory/TeacherCareerProgressionListFactoryInterface.java
@@ -0,0 +1,10 @@
+package PAI.factory;
+
+import PAI.domain.TeacherCareerProgression;
+
+import java.util.List;
+
+public interface TeacherCareerProgressionListFactoryInterface {
+
+    List<TeacherCareerProgression> createTeacherCareerProgressionList();
+}
