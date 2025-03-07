b0f0778e4411c23b8a03d71ea8985a2342abcfab
Fernando Moreira <moreirafernandoj@gmail.com>
2025-03-07 15:46:59 +0000

    Update method getAllProgrammes in ProgrammeList class, isolating the creation of ListOfProgrammesCopy through ProgrammeListArrayListFactory class.

--- src/main/java/PAI/factory/ProgrammeListArrayListFactory.java
+++ src/main/java/PAI/factory/ProgrammeListArrayListFactory.java
@@ -3,9 +3,16 @@ package PAI.factory;
 import PAI.domain.Programme;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ProgrammeListArrayListFactory implements ProgrammeListArrayListFactoryInterface{
     public ArrayList<Programme> newProgrammeArrayList() {
         return new ArrayList<>();
     }
+
+    @Override
+    public List<Programme> copyProgrammeArrayList(List<Programme> list) {
+        return List.of();
+    }
+
 }
