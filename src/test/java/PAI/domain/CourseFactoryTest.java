407326d41e19d44da6898c439bf05512a49ac632
Claudia Valente <1241900@isep.ipp.pt>
2025-03-07 09:04:21 +0000

    refactor(Course): CourseFactoryTest

--- src/test/java/PAI/domain/CourseFactoryTest.java
+++ src/test/java/PAI/domain/CourseFactoryTest.java
@@ -4,33 +4,51 @@ import PAI.factory.CourseFactory;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedConstruction;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.mockConstruction;
+import static org.mockito.Mockito.when;
 
 class CourseFactoryTest {
 
     @Test
-    void shouldCreateCourseWhenConstructorIsInvoked() throws Exception {
+    void shouldCreateCourseWhenConstructorIsInvoked2() throws Exception {
 
-        //Arrange
-
+        // Arrange
         String name = "Informatics";
         String acronym = "INF";
         double quantityOfEcts = 6;
         int durationCourseInSemester = 1;
 
-        // Act
-        try (MockedConstruction<Course> courseMocked = mockConstruction(Course.class)) {
+        CourseFactory courseFactory = new CourseFactory();
 
-            // SUT
-            CourseFactory courseFactory = new CourseFactory();
 
-            Course course = courseFactory.createCourse(name, acronym, quantityOfEcts, durationCourseInSemester);
+        try (MockedConstruction<Course> courseMocked = mockConstruction(Course.class, (mock, context) -> {
+            when(mock.getName()).thenReturn(name);
+            when(mock.getAcronym()).thenReturn(acronym);
+            when(mock.getQuantityCreditsEcts()).thenReturn(quantityOfEcts);
+            when(mock.getDurationInSemester()).thenReturn(durationCourseInSemester);
+        })) {
+
+            // Act
+            Course result = courseFactory.createCourse(name, acronym, quantityOfEcts, durationCourseInSemester);
 
             // Assert
-            assertNotNull(course);
-            assertEquals(1, courseMocked.constructed().size());
+            assertNotNull(result);
+
+            // O metodo cria uma lista e insere os Objectos criados
+            List<Course> courses = courseMocked.constructed();
+            assertEquals(1, courses.size());
+
+            // Verifica os valores do Objecto
+            assertEquals(name, result.getName());
+            assertEquals(acronym, result.getAcronym());
+            assertEquals(quantityOfEcts, result.getQuantityCreditsEcts());
+            assertEquals(durationCourseInSemester, result.getDurationInSemester());
         }
     }
+
+
 }
