7875a72daa16265ab3eb9e172c485c89b3c409e6
Claudia Valente <1241900@isep.ipp.pt>
2025-03-07 11:30:58 +0000

    refactor (Course): CourseFactoryTest

--- src/test/java/PAI/factory/CourseFactoryTest.java
+++ src/test/java/PAI/factory/CourseFactoryTest.java
@@ -1,6 +1,7 @@
 package PAI.factory;
 
 import PAI.domain.Course;
+import PAI.domain.Student;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedConstruction;
 
@@ -17,15 +18,20 @@ class CourseFactoryTest {
     void shouldCreateCourseWhenConstructorIsInvoked2() throws Exception {
 
         // Arrange
-        String name = "Informatics";
-        String acronym = "INF";
-        double quantityOfEcts = 6;
-        int durationCourseInSemester = 1;
+        String name1 = "Informatics";
+        String acronym1 = "INF";
+        double quantityOfEcts1 = 6;
+        int durationCourseInSemester1 = 1;
 
         CourseFactory courseFactory = new CourseFactory();
 
 
         try (MockedConstruction<Course> courseMocked = mockConstruction(Course.class, (mock, context) -> {
+            String name = (String) context.arguments().get(0);
+            String acronym = (String) context.arguments().get(1);
+            double quantityOfEcts = (Double) context.arguments().get(2);
+            int durationCourseInSemester = (int)  context.arguments().get(3);
+
             when(mock.getName()).thenReturn(name);
             when(mock.getAcronym()).thenReturn(acronym);
             when(mock.getQuantityCreditsEcts()).thenReturn(quantityOfEcts);
@@ -33,7 +39,7 @@ class CourseFactoryTest {
         })) {
 
             // Act
-            Course result = courseFactory.createCourse(name, acronym, quantityOfEcts, durationCourseInSemester);
+            Course result = courseFactory.createCourse(name1, acronym1, quantityOfEcts1, durationCourseInSemester1);
 
             // Assert
             assertNotNull(result);
@@ -43,10 +49,10 @@ class CourseFactoryTest {
             assertEquals(1, courses.size());
 
             // Verifica os valores do Objecto
-            assertEquals(name, result.getName());
-            assertEquals(acronym, result.getAcronym());
-            assertEquals(quantityOfEcts, result.getQuantityCreditsEcts());
-            assertEquals(durationCourseInSemester, result.getDurationInSemester());
+            assertEquals(name1, result.getName());
+            assertEquals(acronym1, result.getAcronym());
+            assertEquals(quantityOfEcts1, result.getQuantityCreditsEcts());
+            assertEquals(durationCourseInSemester1, result.getDurationInSemester());
         }
     }
 
