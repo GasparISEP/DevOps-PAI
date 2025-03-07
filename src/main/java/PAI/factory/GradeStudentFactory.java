2869ed49368b26935219aaa0f03b6bc21f67c41e
Dianaguedes19 <1241903@isep.ipp.pt>
2025-03-07 11:01:04 +0000

    Added new exceptions to GradeStudentFactory and the tests

--- src/main/java/PAI/factory/GradeStudentFactory.java
+++ src/main/java/PAI/factory/GradeStudentFactory.java
@@ -7,6 +7,12 @@ import PAI.domain.Student;
 public class GradeStudentFactory implements GradeStudentFactoryImpl {
 
     public GradeStudent newGradeStudent (double grade, String date, Student student, CourseEdition courseEdition) throws Exception {
+        if (grade < 0 ||  grade > 20){
+            throw new IllegalArgumentException("Grade cannot be less than 0 or higher than 20.");
+        }
+        if (date == null || date.isBlank()){
+            throw new IllegalArgumentException ("Date cannot be null or empty!");
+        }
         return new GradeStudent(grade, date, student, courseEdition);
     }
 }
