9d070434b4a7bcb73f052fe7c5e708e5fae36546
Nuno Cruz <1241919@isep.ipp.pt>
2025-03-13 12:11:29 +0000

    Updated DTLFImpl Test to get full coverage

--- src/test/java/PAI/factory/DegreeTypeListFactoryImplTest.java
+++ src/test/java/PAI/factory/DegreeTypeListFactoryImplTest.java
@@ -1,17 +1,21 @@
 package PAI.factory;
 
+import PAI.domain.DegreeType;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
-
 class DegreeTypeListFactoryImplTest {
-
     @Test
-    public void shouldCreateDegreeTypeList() {
+    void shouldCreateDegreeTypeList() {
         //Arrange
-        DegreeTypeListFactoryImpl degreeTypeListFactoryImpl = new DegreeTypeListFactoryImpl();
-        // Act + Assert
-        assertNotNull(degreeTypeListFactoryImpl);
+        DegreeTypeListFactoryImpl degreeTypeListFactory = new DegreeTypeListFactoryImpl();
+        //
+        List<DegreeType> degreeTypeList = degreeTypeListFactory.createDegreeTypeList();
+        assertNotNull(degreeTypeList);
+        assertInstanceOf(ArrayList.class, degreeTypeList);
     }
 
 }
