68b526724e3360cd5acdbac6e89452226161cfdf
paulojorgeribeiro <1241920@isep.ipp.pt>
2025-03-16 14:29:42 +0000

    upadte comments in TeacherCategoryRepositoryListFactoryImpl

--- src/main/java/PAI/factory/TeacherCategoryRepositoryListFactoryImpl.java
+++ src/main/java/PAI/factory/TeacherCategoryRepositoryListFactoryImpl.java
@@ -5,8 +5,21 @@ import PAI.domain.TeacherCategory;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Implementation of the {@link TeacherCategoryRepositoryListFactory} interface.
+ * This class is responsible for providing a list of {@link TeacherCategory} instances.
+ */
 public class TeacherCategoryRepositoryListFactoryImpl implements TeacherCategoryRepositoryListFactory {
 
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Retrieves an empty list of {@link TeacherCategory} objects.
+     * This implementation simply returns a new empty {@link ArrayList}.
+     * </p>
+     *
+     * @return an empty {@link List} of {@link TeacherCategory} objects.
+     */
     @Override
     public List <TeacherCategory> getTeacherCategoryList()  {
         return new ArrayList<>();
