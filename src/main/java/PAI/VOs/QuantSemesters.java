50a51cbb857a93fd37433f9467bfe17cd876ac61
RicardoMarques21 <1241923@isep.ipp.pt>
2025-03-26 11:09:58 +0000

    Add constraints to QuantSemesters VO and update tests. Close #95

--- src/main/java/PAI/VOs/QuantSemesters.java
+++ src/main/java/PAI/VOs/QuantSemesters.java
@@ -2,9 +2,20 @@ package PAI.VOs;
 
 public class QuantSemesters {
 
-    private int _quantSemesters;
+    private final int _quantSemesters;
 
-    public QuantSemesters(int quantityOfSemesters) {
+    public QuantSemesters(int quantityOfSemesters) throws Exception {
+        if(!isQuantityOfSemestersValid(quantityOfSemesters)) {
+            throw new Exception("Insert a valid number of Semesters");
+        }
         _quantSemesters = quantityOfSemesters;
+    }
+
+    private boolean isQuantityOfSemestersValid(int quantityOfSemesters) {
+        return quantityOfSemesters > 0;
+    }
+
+    public int getQuantityOfSemesters(){
+        return _quantSemesters;
     }
 }
