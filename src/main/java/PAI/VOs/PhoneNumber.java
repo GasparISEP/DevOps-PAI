d5868724a2aa9c067b752824bb9346a010c7325c
pedrofg890 <1210608@isep.ipp.pt>
2025-03-27 12:37:00 +0000

    task: Add Country Code verification for null & empty values. Refs #66

--- src/main/java/PAI/VOs/PhoneNumber.java
+++ src/main/java/PAI/VOs/PhoneNumber.java
@@ -7,9 +7,14 @@ public class PhoneNumber implements ValueObject {
     private final String _countryCode;
     private final String _number;
 
-    public PhoneNumber(String countryCode, String number){
+    public PhoneNumber(String countryCode, String number) throws Exception{
+
+        if(countryCode == null || countryCode.isBlank()){
+            throw new IllegalArgumentException("Country Code cannot be empty");
+        }
 
         this._countryCode = countryCode;
         this._number = number;
     }
+
 }
--- src/test/java/PAI/VOs/PhoneNumberTest.java
+++ src/test/java/PAI/VOs/PhoneNumberTest.java
@@ -7,11 +7,27 @@ import static org.junit.jupiter.api.Assertions.*;
 class PhoneNumberTest {
 
     @Test
-    void shouldCreatPhoneNumber(){
+    void shouldCreatPhoneNumber() throws Exception{
         //act
         PhoneNumber phoneNumber = new PhoneNumber("+351", "999999999");
 
         //assert
         assertNotNull(phoneNumber);
+    }
+
+    @Test
+    void nullCountryCodeThrowsException(){
+        //arrange
+
+        //act + assert
+        assertThrows(Exception.class, () -> new PhoneNumber(null, "999999999"));
+    }
+
+    @Test
+    void emptyCountryCodeThrowsException(){
+        //arrange
+
+        //act + assert
+        assertThrows(Exception.class, () -> new PhoneNumber("", "999999999"));
     }
 }
