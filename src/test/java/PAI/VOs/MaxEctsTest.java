8d7488b8f5ee9033b1e7027f260c979ae97b08b3
Zé Sousa <1241915@isep.ipp.pt>
2025-03-27 11:30:55 +0000

    update commit 7847b872c0c7e2391265da807f48ca67ec2e9fe7

--- src/test/java/PAI/VOs/MaxEctsTest.java
+++ src/test/java/PAI/VOs/MaxEctsTest.java
@@ -7,7 +7,7 @@ class MaxEctsTest {
 
     @Test
     void shouldStoreMaxEctsValueCorrectly() {
-        int expectedValue = 240;
+        int expectedValue = 240 ;
         MaxEcts maxEcts = new MaxEcts(expectedValue);
 
         assertEquals(expectedValue, maxEcts.getMaxEcts(), "O valor do maxEcts não foi armazenado corretamente.");
