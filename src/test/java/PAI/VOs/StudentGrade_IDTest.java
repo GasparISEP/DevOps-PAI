fd00940f787c3e2d7dd3576fcdf8ee584dab4b67
JoãoRuaCardoso <1241911@isep.ipp.pt>
2025-03-27 12:05:11 +0000

    Ref #55 : Created StudentGrade_ID VO and related test

---
+++ src/test/java/PAI/VOs/StudentGrade_IDTest.java
@@ -0,0 +1,18 @@
+package PAI.VOs;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentGrade_IDTest {
+
+    @Test
+    void shouldRandomNumber (){
+        //arrange
+        StudentGrade_ID studentGradeId = new StudentGrade_ID();
+        //assert
+        assertNotNull(studentGradeId);
+
+    }
+
+}
