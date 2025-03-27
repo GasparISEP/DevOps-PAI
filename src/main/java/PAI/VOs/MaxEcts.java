7847b872c0c7e2391265da807f48ca67ec2e9fe7
Zé Sousa <1241915@isep.ipp.pt>
2025-03-27 10:54:05 +0000

    Create MaxEcts as Value Object with test. Closes #126. Ref #103

--- src/main/java/PAI/VOs/MaxEcts.java
+++ src/main/java/PAI/VOs/MaxEcts.java
@@ -1,4 +1,14 @@
 package PAI.VOs;
 
 public class MaxEcts {
+
+    private final int _maxEcts;
+
+        public MaxEcts(int maxEcts) {
+            _maxEcts = maxEcts;
+        }
+
+        public int getMaxEcts() {
+            return _maxEcts;
+        }
 }
