6b6572bb5a63cdd6e4bfef18a218cb31379ca58d
ma.antunes <1241916@isep.ipp.pt>
2025-03-28 18:36:42 +0000

    feat(cEID): add more CourseEditionID methods and tests (closes #161)

--- src/main/java/PAI/VOs/CourseEditionID.java
+++ src/main/java/PAI/VOs/CourseEditionID.java
@@ -12,5 +12,19 @@ public class CourseEditionID implements DomainId {
         _courseEditionId = UUID.randomUUID();
     }
 
+    @Override
+    public String toString() {
+
+        return _courseEditionId.toString();
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass())
+            return false;
+        CourseEditionID courseEditionIDTest = (CourseEditionID) object;
+        return _courseEditionId.equals(courseEditionIDTest._courseEditionId);
+    }
 
 }
--- src/test/java/PAI/VOs/CourseEditionIDTest.java
+++ src/test/java/PAI/VOs/CourseEditionIDTest.java
@@ -1,6 +1,7 @@
 package PAI.VOs;
 
 import org.junit.jupiter.api.Test;
+import java.util.UUID;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CourseEditionIDTest {
@@ -12,4 +13,68 @@ class CourseEditionIDTest {
         //Assert
         assertNotNull(courseEditionID);
     }
+
+    @Test
+    void shouldReturnCorrectUUIDString() {
+        //Arrange
+        CourseEditionID courseEditionID = new CourseEditionID();
+        //Act
+        UUID uuid = UUID.fromString(courseEditionID.toString());
+        //Assert
+        assertEquals(courseEditionID.toString(), uuid.toString());
+    }
+
+    @Test
+    void shouldReturnsEqualsIfObjectsAreEqual() {
+        CourseEditionID courseEditionID = new CourseEditionID();
+        CourseEditionID courseEditionID2 = courseEditionID;
+
+        assertEquals(courseEditionID, courseEditionID2);
+    }
+
+    @Test
+    void shouldReturnEqualsIfDifferentObjectsHaveSameUUID() {
+        //Arrange
+        UUID uuid = UUID.randomUUID();
+        CourseEditionID courseEditionID = new CourseEditionID();
+        CourseEditionID courseEditionID2 = new CourseEditionID();
+
+        //Act
+        try {
+            var field = CourseEditionID.class.getDeclaredField("_courseEditionId");
+            field.setAccessible(true);
+            field.set(courseEditionID, uuid);
+            field.set(courseEditionID2, uuid);
+        } catch (Exception e) {
+            fail("Falha ao modificar UUID para teste");
+        }
+        //Assert
+        assertEquals(courseEditionID, courseEditionID2);
+    }
+
+    @Test
+    void shouldReturnNotEqualsIfCourseEditionIDsHaveDifferentUUIDs() {
+        //Arrange
+        CourseEditionID courseEditionID = new CourseEditionID();
+        CourseEditionID courseEditionID2 = new CourseEditionID();
+        //Act+Assert
+        assertNotEquals(courseEditionID, courseEditionID2);
+    }
+
+    @Test
+    void shouldReturnNotEqualsIfComparingWithNull() {
+        //Arrange
+        CourseEditionID courseEditionID = new CourseEditionID();
+        CourseEditionID courseEditionID2 = null;
+        //Act+Assert
+        assertNotEquals(courseEditionID2, courseEditionID);
+    }
+
+    @Test
+    void shouldReturnNotEqualsIfObjectsAreNotFromSameClass() {
+        CourseEditionID courseEditionID = new CourseEditionID();
+        CourseID courseID = new CourseID();
+        assertNotEquals(courseEditionID, courseID);
+    }
+
 }
