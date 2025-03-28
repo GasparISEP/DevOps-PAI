93a0c4b488175a394ebf3abe32255a57b6afde59
Ruben Rocha <1241924@isep.ipp.pt>
2025-03-28 15:48:32 +0000

    mockingConstructorThrowingException isolation test. Refs #69

--- src/test/java/PAI/domain/accessMethod/AccessMethodDDDFactoryImplTest.java
+++ src/test/java/PAI/domain/accessMethod/AccessMethodDDDFactoryImplTest.java
@@ -2,6 +2,7 @@ package PAI.domain.accessMethod;
 
 import PAI.VOs.AccessMethodID;
 import PAI.VOs.NameWithNumbersAndSpecialChars;
+import PAI.domain.AccessMethod;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedConstruction;
 
@@ -28,6 +29,29 @@ class AccessMethodDDDFactoryImplTest {
             AccessMethodDDD doubleAccessMethod = mockAccessMethod.constructed().get(0);
             assertNotNull(doubleAccessMethod);
             assertEquals(accessMethod,doubleAccessMethod);
+        }
+    }
+
+    @Test
+    void mockingConstructorThrowingException(){
+        //arrange
+        AccessMethodDDDFactoryImpl accessMethodFactoryImpl = new AccessMethodDDDFactoryImpl();
+        AccessMethodID accessMethodID = mock(AccessMethodID.class);
+        NameWithNumbersAndSpecialChars accessMethodName = mock(NameWithNumbersAndSpecialChars.class);
+        //Use try-with-resources to mock construction and throw an exception
+        try (MockedConstruction<AccessMethodDDD> mock = mockConstruction(AccessMethodDDD.class,(mocked, context) ->
+        {
+            //Define behavior: throwing an exception when a new instance of Location is created
+            throw new RuntimeException(new InstantiationException("AccessMethod constructor failed"));
+        })) {
+            //Act: trying to create accessMethod will throw the exception
+            try {
+                accessMethodFactoryImpl.createAccessMethod(accessMethodID, accessMethodName);
+                fail("Expect exception not thrown");
+            } catch (Exception e) {
+                //Assertion to check if the exception is thrown
+                assertTrue(e.getCause().getMessage().contains("AccessMethod constructor failed"));
+            }
         }
     }
 
