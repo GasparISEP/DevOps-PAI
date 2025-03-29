bb56828591d7252dccbfef1bad94e0d6ce4c65d5
Ruben Rocha <1241924@isep.ipp.pt>
2025-03-29 16:33:47 +0000

    Created IAccessMethodDDDListFactory. Refs #69

--- src/main/java/PAI/repository/IRepositoryAccessMethod.java
+++
@@ -1,8 +0,0 @@
-package PAI.repository;
-
-import PAI.VOs.AccessMethodID;
-import PAI.ddd.IRepository;
-import PAI.domain.accessMethodDDD.AccessMethodDDD;
-
-public interface IRepositoryAccessMethod extends IRepository<AccessMethodID, AccessMethodDDD> {
-}
---
+++ src/main/java/PAI/repository/accessMethodRepositoryDDD/IRepositoryAccessMethodDDD.java
@@ -0,0 +1,8 @@
+package PAI.repository.accessMethodRepositoryDDD;
+
+import PAI.VOs.AccessMethodID;
+import PAI.ddd.IRepository;
+import PAI.domain.accessMethodDDD.AccessMethodDDD;
+
+public interface IRepositoryAccessMethodDDD extends IRepository<AccessMethodID, AccessMethodDDD> {
+}
