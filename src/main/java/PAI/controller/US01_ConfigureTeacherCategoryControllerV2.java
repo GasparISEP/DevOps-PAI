dd1eb36a79551a65b10172c9c8962aafeba59b4d
paulojorgeribeiro <1241920@isep.ipp.pt>
2025-03-29 01:04:13 +0000

    US01 - Update Acording to DDD Principles new VO TeacherCategoryID (#68)

--- src/main/java/PAI/controller/US01_ConfigureTeacherCategoryControllerV2.java
+++ src/main/java/PAI/controller/US01_ConfigureTeacherCategoryControllerV2.java
@@ -1,9 +1,9 @@
 package PAI.controller.v2;
 
-import PAI.domain.TeacherCategoryV2;
-import PAI.factory.v2.ITeacherCategoryFactoryV2;
-import PAI.repository.v2.ITeacherCategoryRepository;
 import PAI.VOs.Name;
+import PAI.domain.TeacherCategoryV2;
+import PAI.factory.ITeacherCategoryFactoryV2;
+import PAI.repository.ITeacherCategoryRepository;
 
 /**
  * Controller for configuring TeacherCategoryV2.
