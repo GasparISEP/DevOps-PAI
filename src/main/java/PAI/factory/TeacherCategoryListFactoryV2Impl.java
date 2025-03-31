b5512b2c1ac75970523bf161c7ca292e4769e6cc
paulojorgeribeiro <1241920@isep.ipp.pt>
2025-03-31 18:16:29 +0100

    TeacherCategoryListFactoryV2Impl and Test refs #69

--- src/main/java/PAI/factory/TeacherCategoryListFactoryV2Impl.java
+++ src/main/java/PAI/factory/TeacherCategoryListFactoryV2Impl.java
@@ -5,6 +5,11 @@ import PAI.domain.TeacherCategoryV2;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Implementation of ITeacherCategoryListFactoryV2 that returns an empty list.
+ * Useful for initializations or test environments where no data is preloaded.
+ */
+
 public class TeacherCategoryListFactoryV2Impl implements ITeacherCategoryListFactoryV2 {
     @Override
     public List<TeacherCategoryV2> getTeacherCategoryList() {
