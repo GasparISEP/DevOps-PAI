2116389a5d60fcdc6d852fb6f7ebf62c37411ef0
Ruben Rocha <1241924@isep.ipp.pt>
2025-03-31 11:26:27 +0100

    Created registerAccessMethod + isolation tests. Refs #69

--- src/test/java/PAI/repository/accessMethodRepositoryDDD/AccessMethodDDDRepositoryTest.java
+++ src/test/java/PAI/repository/accessMethodRepositoryDDD/AccessMethodDDDRepositoryTest.java
@@ -1,10 +1,11 @@
 package PAI.repository.accessMethodRepositoryDDD;
-
+import PAI.VOs.NameWithNumbersAndSpecialChars;
+import PAI.domain.accessMethodDDD.AccessMethodDDD;
 import PAI.domain.accessMethodDDD.AccessMethodDDDFactoryImpl;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class AccessMethodDDDRepositoryTest {
 
@@ -17,6 +18,37 @@ class AccessMethodDDDRepositoryTest {
         AccessMethodDDDRepository accessMethodRepository = new AccessMethodDDDRepository(doubleAccessMethodFactory, doubleAccessMethodListFactory);
         //assert
         assertNotNull(accessMethodRepository);
+    }
+
+    @Test
+    void shouldNotRegisterAccessMethodIfIsAlreadyRegisteredInTheAccessMethodRepository(){
+        //arrange
+        AccessMethodDDDFactoryImpl doubleAccessMethodFactoryImpl = mock(AccessMethodDDDFactoryImpl.class);
+        AccessMethodDDDListFactoryImpl doubleAccessMethodListFactoryImpl = mock(AccessMethodDDDListFactoryImpl.class);
+        AccessMethodDDDRepository accessMethodRepository = new AccessMethodDDDRepository (doubleAccessMethodFactoryImpl, doubleAccessMethodListFactoryImpl);
+        AccessMethodDDD doubleAccessMethod = mock(AccessMethodDDD.class);
+        NameWithNumbersAndSpecialChars doubleAccessMethodName = mock(NameWithNumbersAndSpecialChars.class);
+        when(doubleAccessMethodFactoryImpl.createAccessMethod(doubleAccessMethodName)).thenReturn(doubleAccessMethod);
+        accessMethodRepository.registerAccessMethod(doubleAccessMethodName);
+        //act
+        boolean result = accessMethodRepository.registerAccessMethod(doubleAccessMethodName);
+        //assert
+        assertFalse(result);
+    }
+
+    @Test
+    void shouldRegisterAccessMethodIfNotRegisteredInAccessMethodRepository() {
+        //arrange
+        AccessMethodDDDFactoryImpl doubleAccessMethodFactoryImpl = mock(AccessMethodDDDFactoryImpl.class);
+        AccessMethodDDDListFactoryImpl doubleAccessMethodListFactoryImpl = mock(AccessMethodDDDListFactoryImpl.class);
+        AccessMethodDDDRepository accessMethodRepository = new AccessMethodDDDRepository (doubleAccessMethodFactoryImpl, doubleAccessMethodListFactoryImpl);
+        AccessMethodDDD doubleAccessMethod = mock(AccessMethodDDD.class);
+        NameWithNumbersAndSpecialChars doubleAccessMethodName = mock(NameWithNumbersAndSpecialChars.class);
+        when(doubleAccessMethodFactoryImpl.createAccessMethod(doubleAccessMethodName)).thenReturn(doubleAccessMethod);
+        //act
+        boolean result = accessMethodRepository.registerAccessMethod(doubleAccessMethodName);
+        //assert
+        assertTrue(result);
     }
 
 }
