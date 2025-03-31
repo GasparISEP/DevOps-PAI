c048e673e1a882e3a58eba6bbe47cce380605eb8
Ruben Rocha <1241924@isep.ipp.pt>
2025-03-31 17:44:55 +0100

    Created equals method for CountryVo. Refs #64

--- src/test/java/PAI/VOs/CountryTest.java
+++ src/test/java/PAI/VOs/CountryTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import java.util.stream.Stream;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 
@@ -57,5 +59,52 @@ class CountryTest {
 
         //act + assert
         Assertions.assertThrows(IllegalArgumentException.class, () -> new Country(country));
+    }
+
+    @Test
+    void shouldReturnTrueIfSameObject(){
+        //arrange
+        String country = "Portugal";
+        Country countryO = new Country(country);
+        //act
+        boolean result = countryO.equals(countryO);
+        //assert
+        assertTrue(result);
+    }
+
+    @Test
+    void shouldReturnTrueIfSameCountryName(){
+        //arrange
+        String country = "Portugal";
+        Country country1 = new Country(country);
+        Country country2 = new Country(country);
+        //act
+        boolean result = country1.equals(country2);
+        //assert
+        assertTrue(result);
+    }
+
+    @Test
+    void shouldReturnFalseIfAnotherObject(){
+        //arrange
+        String country = "Portugal";
+        Country countryO = new Country(country);
+        //act
+        boolean result = countryO.equals(country);
+        //assert
+        assertFalse(result);
+    }
+
+    @Test
+    void shouldReturnFalseIfCountriesHaveDifferentNames(){
+        //arrange
+        String countryP = "Portugal";
+        Country country1 = new Country(countryP);
+        String countryA = "Austria";
+        Country country2 = new Country(countryA);
+        //act
+        boolean result = country1.equals(country2);
+        //assert
+        assertFalse(result);
     }
 }
