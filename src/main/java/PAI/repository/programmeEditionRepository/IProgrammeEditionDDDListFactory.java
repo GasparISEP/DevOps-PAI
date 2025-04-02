d27c61c8b6c0f99b1c78fa9d23ac430a6e08ebfb
jmariz-costa <1241912@isep.ipp.pt>
2025-04-02 14:05:02 +0100

    added ProgrammeEditionDDDListFactory Interface. Refs #71

---
+++ src/main/java/PAI/repository/programmeEditionRepository/IProgrammeEditionDDDListFactory.java
@@ -0,0 +1,10 @@
+package PAI.repository.programmeEditionRepository;
+
+import PAI.domain.ProgrammeEdition;
+
+import java.util.List;
+
+public interface IProgrammeEditionDDDListFactory {
+
+    List<ProgrammeEdition> createProgrammeEditionList();
+}
