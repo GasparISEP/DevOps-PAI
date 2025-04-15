0da73fd2b4dbb5df1e959bd98cfa51a874a64049
joaopinheiro1241913 <1241913@isep.ipp.pt>
2025-04-15 21:28:29 +0100

    Refs #263, 363: Add SchoolYearDataModel and respective unit tests

---
+++ src/main/java/PAI/persistence/datamodel/SchoolYearDataModel.java
@@ -0,0 +1,25 @@
+package PAI.persistence.datamodel;
+
+import jakarta.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "School_Year")
+public class SchoolYearDataModel {
+
+    @Id
+    private String id;
+
+    private String description;
+    private String startDate;
+    private String endDate;
+
+    public SchoolYearDataModel() {}
+
+    public SchoolYearDataModel(String id, String description, String startDate, String endDate) {
+        this.id = id;
+        this.description = description;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+}
---
+++ src/test/java/PAI/persistence/datamodel/SchoolYearDataModelTest.java
@@ -0,0 +1,26 @@
+package PAI.persistence.datamodel;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SchoolYearDataModelTest {
+
+    @Test
+    void testEmptyConstructor() {
+        SchoolYearDataModel schoolYear = new SchoolYearDataModel();
+        assertNotNull(schoolYear);
+    }
+
+    @Test
+    void testConstrutorWithAllArguments() {
+        String id = "uuid-123";
+        String description = "2024/2025";
+        String startDate = "2024-09-01";
+        String endDate = "2025-06-30";
+
+        SchoolYearDataModel schoolYear = new SchoolYearDataModel(id, description, startDate, endDate);
+
+        assertNotNull(schoolYear);
+    }
+}
