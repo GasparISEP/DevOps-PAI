e2b1aa4f2535a62a0ebe6c9cd97ac4e766ce7865
Tomás Fernandes <1241926@isep.ipp.pt>
2025-04-16 15:00:08 +0100

    Created ProgrammeEnrolmentIDDataModel and respective tests

---
+++ src/main/java/PAI/persistence/datamodel/ProgrammeEnrolmentIDDataModel.java
@@ -0,0 +1,44 @@
+package PAI.persistence.datamodel;
+
+import jakarta.persistence.Embeddable;
+
+import java.util.Objects;
+
+@Embeddable
+public class ProgrammeEnrolmentIDDataModel {
+
+    private StudentIDDataModel _studentID;
+    private ProgrammeIDDataModel _programmeID;
+
+    public ProgrammeEnrolmentIDDataModel () {}
+
+    public ProgrammeEnrolmentIDDataModel (StudentIDDataModel studentID, ProgrammeIDDataModel programmeID) {
+        _studentID = studentID;
+        _programmeID = programmeID;
+    }
+
+    public StudentIDDataModel getStudentID () {
+        return _studentID;
+    }
+
+    public ProgrammeIDDataModel getProgrammeID () {
+        return _programmeID;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+
+        if (!(obj instanceof ProgrammeEnrolmentIDDataModel)) return false;
+
+        ProgrammeEnrolmentIDDataModel other = (ProgrammeEnrolmentIDDataModel) obj;
+
+        return Objects.equals(this._studentID, other._studentID) &&
+                Objects.equals(this._programmeID, other._programmeID);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(_studentID, _programmeID);
+    }
+}
