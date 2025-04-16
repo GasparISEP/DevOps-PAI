f5342da9a037329412c3c5e7811d45d25a9f5053
Gonçalo Silva <1241908@isep.ipp.pt>
2025-04-16 14:38:19 +0100

    Revert "Add new method to create AccessMethod with accessMethodID and NameWithNumbersAndSpecialChars to transform from DataModel to Domain. refs #384"
    This reverts commit 3228c441b783146036409d225d5015e072d940d1.

--- src/main/java/PAI/domain/accessMethod/IAccessMethodFactory.java
+++ src/main/java/PAI/domain/accessMethod/IAccessMethodFactory.java
@@ -4,6 +4,6 @@ import PAI.VOs.AccessMethodID;
 import PAI.VOs.NameWithNumbersAndSpecialChars;
 
 public interface IAccessMethodFactory {
+
     AccessMethod createAccessMethod (NameWithNumbersAndSpecialChars accessMethodName);
-    AccessMethod createAccessMethod (AccessMethodID accessMethodID, NameWithNumbersAndSpecialChars accessMethodName);
 }
