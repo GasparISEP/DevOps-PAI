9e1ad4393c7089092211b210013787571a29b367
ma.antunes <1241916@isep.ipp.pt>
2025-04-16 15:33:58 +0100

    feat(mapper): add CourseIDMapper and tests (refs #254)

---
+++ src/main/java/PAI/mapper/CourseIDMapperImpl.java
@@ -0,0 +1,25 @@
+package PAI.mapper;
+
+import PAI.VOs.Acronym;
+import PAI.VOs.CourseID;
+import PAI.VOs.Name;
+import PAI.persistence.datamodel.CourseIDDataModel;
+
+public class CourseIDMapperImpl implements ICourseIDMapper {
+
+    public CourseID toDomain(CourseIDDataModel courseIDDataModel) {
+        if (courseIDDataModel == null) {
+            throw new NullPointerException("courseIDDataModel cannot be null");
+        }
+        Acronym acronym = new Acronym(courseIDDataModel.getAcronym());
+        Name name = new Name(courseIDDataModel.getName());
+        return new CourseID(acronym, name);
+    }
+
+    public CourseIDDataModel toDataModel(CourseID courseID) {
+        if (courseID == null) {
+            throw new NullPointerException("courseID cannot be null");
+        }
+        return new CourseIDDataModel(courseID);
+    }
+}
