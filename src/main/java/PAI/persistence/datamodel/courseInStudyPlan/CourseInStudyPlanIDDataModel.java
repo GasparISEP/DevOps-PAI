b4e8f08ac97c65fee84985511fba405e4006fb8b
gasparfigueiras <1241907@isep.ipp.pt>
2025-04-18 14:13:28 +0100

    Create CourseInStudyPlanIDDAtaModel, CourseInStudyPlanIDMapperImpl and all the necessary tests (#280).

---
+++ src/main/java/PAI/persistence/datamodel/courseInStudyPlan/CourseInStudyPlanIDDataModel.java
@@ -0,0 +1,46 @@
+package PAI.persistence.datamodel.courseInStudyPlan;
+
+import PAI.persistence.datamodel.CourseIDDataModel;
+import PAI.persistence.datamodel.studyPlan.StudyPlanIDDataModel;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Embedded;
+
+import java.io.Serializable;
+
+@Embeddable
+public class CourseInStudyPlanIDDataModel implements Serializable {
+
+    @Embedded
+    private StudyPlanIDDataModel _studyPlanIDDataModel;
+    @Embedded
+    private CourseIDDataModel _courseID;
+
+    public CourseInStudyPlanIDDataModel() {}
+
+    public CourseInStudyPlanIDDataModel(StudyPlanIDDataModel studyPlanIDDataModel, CourseIDDataModel courseID) {
+        this._studyPlanIDDataModel = studyPlanIDDataModel;
+        this._courseID = courseID;
+    }
+
+    public CourseIDDataModel getCourseID() {
+        return _courseID;
+    }
+
+    public StudyPlanIDDataModel getStudyPlanIDDataModel() {
+        return _studyPlanIDDataModel;
+    }
+
+    @Override
+    public boolean equals(Object objectToCompare) {
+        if (this == objectToCompare) return true;
+        if (!(objectToCompare instanceof CourseInStudyPlanIDDataModel)) return false;
+        CourseInStudyPlanIDDataModel courseInStudyPlanIDDataModel = (CourseInStudyPlanIDDataModel) objectToCompare;
+        return _studyPlanIDDataModel.equals(courseInStudyPlanIDDataModel._studyPlanIDDataModel) &&
+                _courseID.equals(courseInStudyPlanIDDataModel._courseID);
+    }
+
+    @Override
+    public int hashCode() {
+        return _studyPlanIDDataModel.hashCode() + _courseID.hashCode();
+    }
+}
