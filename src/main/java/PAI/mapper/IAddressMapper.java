a0f8d7fa1587a8646ff66031bcad577473bf4723
MiguelAntuns7 <1241918@isep.ipp.pt>
2025-04-18 20:14:16 +0100

    Refs #361: Create AddressMapper + respective tests

---
+++ src/main/java/PAI/mapper/IAddressMapper.java
@@ -0,0 +1,9 @@
+package PAI.mapper;
+
+import PAI.VOs.Address;
+import PAI.persistence.datamodel.AddressDataModel;
+
+public interface IAddressMapper {
+
+    Address toDomain (AddressDataModel addressDataModel);
+}
