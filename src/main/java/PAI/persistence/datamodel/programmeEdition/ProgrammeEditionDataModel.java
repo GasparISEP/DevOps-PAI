b6164785dcb3430a0bf9c9a115b91298a46a68da
pedrofg890 <1210608@isep.ipp.pt>
2025-04-18 16:55:33 +0100

    feat: Add constructor to ProgrammeEditionDataModel + tests. Refs #256

--- src/main/java/PAI/persistence/datamodel/programmeEdition/ProgrammeEditionDataModel.java
+++ src/main/java/PAI/persistence/datamodel/programmeEdition/ProgrammeEditionDataModel.java
@@ -1,8 +1,11 @@
 package PAI.persistence.datamodel.programmeEdition;
 
+import PAI.persistence.datamodel.ProgrammeIDDataModel;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+
+import java.util.UUID;
 
 @Entity
 @Table(name = "programmeEdition")
@@ -10,10 +13,16 @@ public class ProgrammeEditionDataModel {
 
     @EmbeddedId
     private ProgrammeEditionIdDataModel _programmeEditionID;
+    @EmbeddedId
+    private ProgrammeIDDataModel _programmeID;
+    private UUID _schoolYearID;
 
-    private String _programmeID;
-    private String _schoolYearID;
+    protected ProgrammeEditionDataModel() {}
 
-    public ProgrammeEditionDataModel() {}
+    public ProgrammeEditionDataModel(ProgrammeEditionIdDataModel programmeEditionID, ProgrammeIDDataModel programmeID, UUID schoolYearID) {
 
+        this._programmeEditionID = programmeEditionID;
+        this._programmeID = programmeID;
+        this._schoolYearID = schoolYearID;
+    }
 }
--- src/test/java/PAI/persistence/datamodel/programmeEdition/ProgrammeEditionDataModelTest.java
+++ src/test/java/PAI/persistence/datamodel/programmeEdition/ProgrammeEditionDataModelTest.java
@@ -1,8 +1,12 @@
 package PAI.persistence.datamodel.programmeEdition;
 
+import PAI.persistence.datamodel.ProgrammeIDDataModel;
 import org.junit.jupiter.api.Test;
 
+import java.util.UUID;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 
 class ProgrammeEditionDataModelTest {
 
@@ -11,6 +15,18 @@ class ProgrammeEditionDataModelTest {
         // arrange
         // act
         ProgrammeEditionDataModel programmeEditionDataModel = new ProgrammeEditionDataModel();
+        // assert
+        assertNotNull(programmeEditionDataModel);
+    }
+
+    @Test
+    void shouldCreateProgrammeEditionDataModelWithParameters() {
+        // arrange
+        ProgrammeEditionIdDataModel programmeEditionId = mock(ProgrammeEditionIdDataModel.class);
+        ProgrammeIDDataModel programmeID = mock(ProgrammeIDDataModel.class);
+        UUID schoolYearID = mock(UUID.class);
+        // act
+        ProgrammeEditionDataModel programmeEditionDataModel = new ProgrammeEditionDataModel(programmeEditionId, programmeID, schoolYearID );
         // assert
         assertNotNull(programmeEditionDataModel);
     }
