6dae6b9095384c5c591c20165d24712fac6fdcee
Gonçalo Silva <1241908@isep.ipp.pt>
2025-04-19 11:59:22 +0100

    feat: Create AccessMethodIDDataModel and tests. refs #404

---
+++ src/main/java/PAI/persistence/datamodel/accessMethod/AccessMethodIDDataModel.java
@@ -0,0 +1,38 @@
+package PAI.persistence.datamodel.accessMethod;
+
+import jakarta.persistence.Embeddable;
+
+import java.util.Objects;
+import java.util.UUID;
+
+@Embeddable
+public class AccessMethodIDDataModel {
+
+    private UUID id;
+
+    protected AccessMethodIDDataModel() {}
+
+    public AccessMethodIDDataModel(UUID id) {
+        if (id == null) {
+            throw new IllegalArgumentException("id cannot be null");
+        }
+        this.id = id;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AccessMethodIDDataModel that = (AccessMethodIDDataModel) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
