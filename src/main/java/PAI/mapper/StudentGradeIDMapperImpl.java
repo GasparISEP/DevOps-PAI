44cd4b5fef2895f5f0f91af346dc315f7412ce06
JoãoRuaCardoso <1241911@isep.ipp.pt>
2025-04-21 16:16:21 +0100

    Ref #281 : Updated StudentGradeIDMapperImpl methods

--- src/main/java/PAI/mapper/StudentGradeIDMapperImpl.java
+++ src/main/java/PAI/mapper/StudentGradeIDMapperImpl.java
@@ -1,14 +1,11 @@
 package PAI.mapper;
 
 import PAI.VOs.*;
-import PAI.domain.programme.Programme;
-import PAI.domain.programmeEdition.ProgrammeEdition;
 import PAI.mapper.courseEdition.CourseEditionIDMapperImpl;
-import PAI.mapper.courseInStudyPlanID.CourseInStudyPlanIDMapperImpl;
 import PAI.persistence.datamodel.StudentGradeIDDataModel;
 import PAI.persistence.datamodel.StudentIDDataModel;
 import PAI.persistence.datamodel.courseEdition.CourseEditionIDDataModel;
-import PAI.persistence.datamodel.courseInStudyPlan.CourseInStudyPlanIDDataModel;
+
 
 public class StudentGradeIDMapperImpl {
 
@@ -20,19 +17,17 @@ public class StudentGradeIDMapperImpl {
         this.studentIDMapper = studentIDMapper;
     }
 
-
-
     public StudentGradeIDDataModel toDataModel (StudentGradeID studentGradeID) throws Exception{
 
-        return new StudentGradeIDDataModel(studentIDMapper.domainToDataModel(studentGradeID.get_studentID()),courseEditionIDMapper.toDataModel(studentGradeID.get_courseEdition()));
+        StudentID studentID = studentGradeID.get_studentID();
+        CourseEditionID courseEditionID = studentGradeID.get_courseEdition();
+        return new StudentGradeIDDataModel(studentIDMapper.domainToDataModel(studentID),courseEditionIDMapper.toDataModel(courseEditionID));
     }
 
     public StudentGradeID toDomain (StudentGradeIDDataModel studentGradeIDDataModel) throws Exception{
 
         CourseEditionIDDataModel courseEditionIDDataModel = studentGradeIDDataModel.get_courseEditionIDDataModel();
         StudentIDDataModel studentIDDataModel = studentGradeIDDataModel.get_studentIDDataModel();
-
-
         return new StudentGradeID(studentIDMapper.dataModelToDomain(studentIDDataModel),courseEditionIDMapper.toDomain(courseEditionIDDataModel));
     }
 }
