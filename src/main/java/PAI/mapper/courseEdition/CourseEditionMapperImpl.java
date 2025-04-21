068d6417d745def85adfd9d990e2ca187a3c795c
jmariz-costa <1241912@isep.ipp.pt>
2025-04-21 22:16:56 +0100

    refactor: add ICourseEditionFactory as parameter to toDomain method in ICourseEditionMapper. Refs #258

--- src/main/java/PAI/mapper/courseEdition/CourseEditionMapperImpl.java
+++ src/main/java/PAI/mapper/courseEdition/CourseEditionMapperImpl.java
@@ -1,12 +1,13 @@
 package PAI.mapper.courseEdition;
 
 import PAI.domain.CourseEdition;
+import PAI.factory.ICourseEditionFactory;
 import PAI.persistence.datamodel.courseEdition.CourseEditionDataModel;
 
 public class CourseEditionMapperImpl implements ICourseEditionMapper {
 
     @Override
-    public CourseEdition toDomain(CourseEditionDataModel courseEditionDataModel) {
+    public CourseEdition toDomain(CourseEditionDataModel courseEditionDataModel, ICourseEditionFactory courseEditionFactory) {
         return null;
     }
 
--- src/main/java/PAI/mapper/courseEdition/ICourseEditionMapper.java
+++ src/main/java/PAI/mapper/courseEdition/ICourseEditionMapper.java
@@ -1,11 +1,12 @@
 package PAI.mapper.courseEdition;
 
 import PAI.domain.CourseEdition;
+import PAI.factory.ICourseEditionFactory;
 import PAI.persistence.datamodel.courseEdition.CourseEditionDataModel;
 
 public interface ICourseEditionMapper {
 
-    CourseEdition toDomain (CourseEditionDataModel courseEditionDataModel);
+    CourseEdition toDomain (CourseEditionDataModel courseEditionDataModel, ICourseEditionFactory courseEditionFactory);
 
     CourseEditionDataModel toDataModel (CourseEdition courseEdition);
 }
