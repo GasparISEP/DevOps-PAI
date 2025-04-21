fcff98e3328ae93ccb31fb620d55477fe7ef5fa6
jmariz-costa <1241912@isep.ipp.pt>
2025-04-21 22:27:15 +0100

    refactor: inject CourseEditionIDMapper, ProgrammeEditionIdMapper and CourseInStudyPlanIDMapper via constructor in CourseEditionMapperImpl. Refs #258

--- src/main/java/PAI/mapper/courseEdition/CourseEditionMapperImpl.java
+++ src/main/java/PAI/mapper/courseEdition/CourseEditionMapperImpl.java
@@ -2,9 +2,21 @@ package PAI.mapper.courseEdition;
 
 import PAI.domain.CourseEdition;
 import PAI.factory.ICourseEditionFactory;
+import PAI.mapper.courseInStudyPlan.ICourseInStudyPlanIDMapper;
+import PAI.mapper.programmeEdition.IProgrammeEditionIdMapper;
 import PAI.persistence.datamodel.courseEdition.CourseEditionDataModel;
 
 public class CourseEditionMapperImpl implements ICourseEditionMapper {
+
+    private final ICourseEditionIDMapper _courseEditionIDMapper;
+    private final IProgrammeEditionIdMapper _programmeEditionIdMapper;
+    private final ICourseInStudyPlanIDMapper _courseInStudyPlanIDMapper;
+
+    public CourseEditionMapperImpl(ICourseEditionIDMapper courseEditionIDMapper, IProgrammeEditionIdMapper programmeEditionIdMapper, ICourseInStudyPlanIDMapper courseInStudyPlanIDMapper) {
+        _courseEditionIDMapper = courseEditionIDMapper;
+        _programmeEditionIdMapper = programmeEditionIdMapper;
+        _courseInStudyPlanIDMapper = courseInStudyPlanIDMapper;
+    }
 
     @Override
     public CourseEdition toDomain(CourseEditionDataModel courseEditionDataModel, ICourseEditionFactory courseEditionFactory) {
--- src/test/java/PAI/mapper/courseEdition/CourseEditionMapperImplTest.java
+++ src/test/java/PAI/mapper/courseEdition/CourseEditionMapperImplTest.java
@@ -1,6 +1,9 @@
 package PAI.mapper.courseEdition;
 
 import PAI.domain.CourseEdition;
+import PAI.factory.ICourseEditionFactory;
+import PAI.mapper.courseInStudyPlan.ICourseInStudyPlanIDMapper;
+import PAI.mapper.programmeEdition.IProgrammeEditionIdMapper;
 import PAI.persistence.datamodel.courseEdition.CourseEditionDataModel;
 import org.junit.jupiter.api.Test;
 
@@ -9,14 +12,36 @@ import static org.mockito.Mockito.mock;
 
 class CourseEditionMapperImplTest {
 
+    //--------Constructor Tests--------
+    @Test
+    void shouldCreateACourseEditionMapperImpl () {
+        // Arrange
+        ICourseEditionIDMapper cEIDMapper = mock(ICourseEditionIDMapper.class);
+        IProgrammeEditionIdMapper pEIDMapper = mock(IProgrammeEditionIdMapper.class);
+        ICourseInStudyPlanIDMapper cISPIDMapper = mock(ICourseInStudyPlanIDMapper.class);
+
+        // Act
+        ICourseEditionMapper mapper = new CourseEditionMapperImpl(cEIDMapper, pEIDMapper, cISPIDMapper);
+
+        // Assert
+        assertNotNull(mapper);
+    }
+
+
+
     @Test
     void shouldReturnNullWhenToDomainIsCalled() {
         // Arrange
+        ICourseEditionIDMapper cEIDMapper = mock(ICourseEditionIDMapper.class);
+        IProgrammeEditionIdMapper pEIDMapper = mock(IProgrammeEditionIdMapper.class);
+        ICourseInStudyPlanIDMapper cISPIDMapper = mock(ICourseInStudyPlanIDMapper.class);
+        ICourseEditionMapper mapper = new CourseEditionMapperImpl(cEIDMapper, pEIDMapper, cISPIDMapper);
+
         CourseEditionDataModel courseEditionDataModel = mock(CourseEditionDataModel.class);
-        CourseEditionMapperImpl courseEditionMapper = new CourseEditionMapperImpl();
+        ICourseEditionFactory courseEditionFactory = mock(ICourseEditionFactory.class);
 
         // Act
-        CourseEdition courseEdition = courseEditionMapper.toDomain(courseEditionDataModel);
+        CourseEdition courseEdition = mapper.toDomain(courseEditionDataModel, courseEditionFactory);
 
         // Assert
         assertNull(courseEdition);
@@ -25,11 +50,15 @@ class CourseEditionMapperImplTest {
     @Test
     void shouldReturnNullWhenToDataModelIsCalled() {
         // Arrange
+        ICourseEditionIDMapper cEIDMapper = mock(ICourseEditionIDMapper.class);
+        IProgrammeEditionIdMapper pEIDMapper = mock(IProgrammeEditionIdMapper.class);
+        ICourseInStudyPlanIDMapper cISPIDMapper = mock(ICourseInStudyPlanIDMapper.class);
+        ICourseEditionMapper mapper = new CourseEditionMapperImpl(cEIDMapper, pEIDMapper, cISPIDMapper);
+
         CourseEdition courseEdition = mock(CourseEdition.class);
-        CourseEditionMapperImpl courseEditionMapper = new CourseEditionMapperImpl();
 
         // Act
-        CourseEditionDataModel courseEditionDataModel = courseEditionMapper.toDataModel(courseEdition);
+        CourseEditionDataModel courseEditionDataModel = mapper.toDataModel(courseEdition);
 
         // Assert
         assertNull(courseEditionDataModel);
