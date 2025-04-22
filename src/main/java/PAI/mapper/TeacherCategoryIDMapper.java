286405b9d7d3ebbd062f45b3f0da0f94293f17c6
paulojorgeribeiro <1241920@isep.ipp.pt>
2025-04-22 01:32:48 +0100

    update TeacherCategoryMapper and IDMapper (Issue #253)

--- src/main/java/PAI/mapper/TeacherCategoryIDMapper.java
+++ src/main/java/PAI/mapper/TeacherCategoryIDMapper.java
@@ -5,20 +5,18 @@ import PAI.persistence.datamodel.TeacherCategoryIDDataModel;
 import org.springframework.stereotype.Component;
 
 @Component
-public class TeacherCategoryIDMapper {
+public class TeacherCategoryIDMapper implements ITeacherCategoryIDMapper {
 
-    //Public constructor for Spring to use in test cases
-    public TeacherCategoryIDMapper() {
-    }
-
-    public static TeacherCategoryIDDataModel toDataModel(TeacherCategoryID domainId) {
+    @Override
+    public TeacherCategoryIDDataModel toDataModel(TeacherCategoryID domainId) {
         if (domainId == null) {
             throw new IllegalArgumentException("Domain ID cannot be null");
         }
         return new TeacherCategoryIDDataModel(domainId.getValue());
     }
 
-    public static TeacherCategoryID toDomainModel(TeacherCategoryIDDataModel dataModelId) {
+    @Override
+    public TeacherCategoryID toDomainModel(TeacherCategoryIDDataModel dataModelId) {
         if (dataModelId == null || dataModelId.getValue() == null) {
             throw new IllegalArgumentException("Data Model ID or its value cannot be null");
         }
