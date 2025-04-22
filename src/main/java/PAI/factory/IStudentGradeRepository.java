8bc34be529b1ec58526183c03e02af094854a575
JoãoRuaCardoso <1241911@isep.ipp.pt>
2025-04-22 13:11:07 +0100

    Ref #281 : take getAverage and KnwowApprovalRate out of interface and changed some dependencies of these metohds to implements

--- src/main/java/PAI/factory/IStudentGradeRepository.java
+++ src/main/java/PAI/factory/IStudentGradeRepository.java
@@ -8,8 +8,8 @@ import PAI.repository.StudentGradeRepository;
 
 public interface IStudentGradeRepository extends IRepository<StudentGradeID, StudentGrade> {
     boolean addGradeToStudent (Grade grade, Date date, StudentID student, CourseEditionID courseEditionID) throws Exception;
-    Double getAverageGrade(CourseEditionID courseEditionID);
-    double knowApprovalRate(CourseEditionID courseEditionID);
+//    Double getAverageGrade(CourseEditionID courseEditionID);
+//    double knowApprovalRate(CourseEditionID courseEditionID);
 
 
 }
