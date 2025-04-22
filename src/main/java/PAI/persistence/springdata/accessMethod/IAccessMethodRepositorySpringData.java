1f262a600ee282ac48a8b94cb4bef04bf855f35d
Gonçalo Silva <1241908@isep.ipp.pt>
2025-04-22 15:08:35 +0100

    fix: correct method name in US02ConfigureAccessMethodControllerTest. refactor: change JpaRepository ID type in IAccessMethodRepositorySpringData refs #251 #307

--- src/main/java/PAI/persistence/springdata/accessMethod/IAccessMethodRepositorySpringData.java
+++ src/main/java/PAI/persistence/springdata/accessMethod/IAccessMethodRepositorySpringData.java
@@ -1,14 +1,12 @@
 package PAI.persistence.springdata.accessMethod;
 
-import PAI.VOs.AccessMethodID;
-import PAI.VOs.NameWithNumbersAndSpecialChars;
 import PAI.persistence.datamodel.accessMethod.AccessMethodDataModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 import java.util.UUID;
 
-public interface IAccessMethodRepositorySpringData extends JpaRepository<AccessMethodDataModel, AccessMethodID> {
+public interface IAccessMethodRepositorySpringData extends JpaRepository<AccessMethodDataModel, Long> {
     Optional<AccessMethodDataModel> findAccessMethodDataModelByName(String name);
     Optional<AccessMethodDataModel> findAccessMethodDataModelByAccessMethodID(UUID accessMethodID);
 }
