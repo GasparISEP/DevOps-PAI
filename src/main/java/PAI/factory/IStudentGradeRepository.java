87a0958d7759095559d0e8e66b9aba1e8d0bb46e
JoãoRuaCardoso <1241911@isep.ipp.pt>
2025-04-22 13:28:05 +0100

    Ref #281 : took addGradeToStudent out of interface and changed some dependencies of these metohds to implements

--- src/main/java/PAI/factory/IStudentGradeRepository.java
+++ src/main/java/PAI/factory/IStudentGradeRepository.java
@@ -7,7 +7,7 @@ import PAI.domain.StudentGrade;
 import PAI.repository.StudentGradeRepository;
 
 public interface IStudentGradeRepository extends IRepository<StudentGradeID, StudentGrade> {
-    boolean addGradeToStudent (Grade grade, Date date, StudentID student, CourseEditionID courseEditionID) throws Exception;
+//    boolean addGradeToStudent (Grade grade, Date date, StudentID student, CourseEditionID courseEditionID) throws Exception;
 //    Double getAverageGrade(CourseEditionID courseEditionID);
 //    double knowApprovalRate(CourseEditionID courseEditionID);
 
