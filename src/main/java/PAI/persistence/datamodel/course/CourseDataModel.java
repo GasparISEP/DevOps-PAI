8be67d3531fda7946bf60cf05bc9d972f2285a91
ma.antunes <1241916@isep.ipp.pt>
2025-04-23 14:53:20 +0100

    refactor(class): handle Course class changes and tests in CourseDataModel (refs #254)

--- src/main/java/PAI/persistence/datamodel/course/CourseDataModel.java
+++ src/main/java/PAI/persistence/datamodel/course/CourseDataModel.java
@@ -13,26 +13,19 @@ public class CourseDataModel
 
     private String _name;
     private String _acronym;
-    private double _quantityCreditsEcts;
-    private int _duration;
 
     public CourseDataModel() {}
 
-    public CourseDataModel(CourseIDDataModel courseIDDataModel, String name, String acronym, double quantityCreditsEcts, int duration) {
+    public CourseDataModel(CourseIDDataModel courseIDDataModel, String name, String acronym) {
 
         _courseID = courseIDDataModel;
         _name = name;
         _acronym = acronym;
-        _quantityCreditsEcts = quantityCreditsEcts;
-        _duration = duration;
     }
 
     public String get_name() {return _name; }
 
     public String get_acronym() {return _acronym;}
 
-    public double get_quantityCreditsEcts() {return _quantityCreditsEcts;}
-
-    public int get_duration() {return _duration; }
 
 }
