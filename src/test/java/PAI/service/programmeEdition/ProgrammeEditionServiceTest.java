eb95bd4cd6d1fbd9d10fd8334eef23f457f2f4dc
Ruben Rocha <1241924@isep.ipp.pt>
2025-04-23 16:27:31 +0100

    test: shouldThrowExceptionWhenProgrammeIdIsNullAtProgrammeEditionCreation. Refs #294

--- src/test/java/PAI/service/programmeEdition/ProgrammeEditionServiceTest.java
+++ src/test/java/PAI/service/programmeEdition/ProgrammeEditionServiceTest.java
@@ -4,6 +4,7 @@ import PAI.VOs.ProgrammeID;
 import PAI.VOs.SchoolYearID;
 import PAI.domain.programmeEdition.IProgrammeEditionFactory;
 import PAI.domain.programmeEdition.ProgrammeEdition;
+import PAI.domain.programmeEdition.ProgrammeEditionFactoryImpl;
 import PAI.repository.programmeEditionRepository.IProgrammeEditionRepository;
 
 import org.junit.jupiter.api.Test;
@@ -59,4 +60,32 @@ class ProgrammeEditionServiceTest {
         assertNotNull(programmeEdition);
     }
 
+    @Test
+    void shouldThrowExceptionWhenProgrammeIdIsNullAtProgrammeEditionCreationIsolationTest() throws Exception {
+        //arrange
+        IProgrammeEditionFactory programmeEditionFactory = mock(IProgrammeEditionFactory.class);
+        IProgrammeEditionRepository programmeEditionRepository = mock(IProgrammeEditionRepository.class);
+        ProgrammeEditionService programmeEditionService = new ProgrammeEditionService(programmeEditionFactory, programmeEditionRepository);
+
+        ProgrammeID programmeID = null;
+        SchoolYearID schoolYearID = mock(SchoolYearID.class);
+
+        when(programmeEditionFactory.createProgrammeEdition(programmeID, schoolYearID)).thenThrow(IllegalArgumentException.class);
+        //act + assert
+        assertThrows(IllegalArgumentException.class, () -> programmeEditionService.createProgrammeEdition(programmeID, schoolYearID));
+    }
+
+    @Test
+    void shouldThrowExceptionWhenProgrammeIdIsNullAtProgrammeEditionCreation() {
+        //arrange
+        IProgrammeEditionFactory programmeEditionFactory = new ProgrammeEditionFactoryImpl();
+        IProgrammeEditionRepository programmeEditionRepository = mock(IProgrammeEditionRepository.class);
+        ProgrammeEditionService programmeEditionService = new ProgrammeEditionService(programmeEditionFactory, programmeEditionRepository);
+
+        ProgrammeID programmeID = null;
+        SchoolYearID schoolYearID = mock(SchoolYearID.class);
+
+        //act + assert
+        assertThrows(IllegalArgumentException.class, () -> programmeEditionService.createProgrammeEdition(programmeID, schoolYearID));
+    }
 }
