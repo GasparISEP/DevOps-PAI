ad784ed3987250e1fffc795738fda4c4a463ca59
jmariz-costa <1241912@isep.ipp.pt>
2025-04-23 19:07:45 +0100

    feat: create ICourseEditionService interface and basic implementation with stub methods. Refs #295

--- src/main/java/PAI/controller/US20_DefineRucForCourseEditionController.java
+++ src/main/java/PAI/controller/US20_DefineRucForCourseEditionController.java
@@ -4,25 +4,25 @@ import PAI.VOs.CourseEditionID;
 import PAI.VOs.TeacherID;
 import PAI.domain.CourseEdition;
 import PAI.domain.Teacher;
-import PAI.service.ICourseEditionApplicationService;
+import PAI.service.courseEdition.ICourseEditionService;
 
 public class US20_DefineRucForCourseEditionController {
 
-    private final ICourseEditionApplicationService service;
+    private final ICourseEditionService service;
 
-    public US20_DefineRucForCourseEditionController(ICourseEditionApplicationService service) {
+    public US20_DefineRucForCourseEditionController(ICourseEditionService service) {
         this.service = service;
     }
 
-    public Iterable<Teacher> getAllTeachers() {
-        return service.getAllTeachers();
-    }
-
-    public Iterable<CourseEdition> getAllCourseEditions() {
-        return service.getAllCourseEditions();
-    }
-
-    public boolean defineRucForCourseEdition(CourseEditionID courseEditionID, TeacherID teacherID) {
-        return service.assignRucToCourseEdition(teacherID, courseEditionID);
-    }
+//    public Iterable<Teacher> getAllTeachers() {
+//        return service.getAllTeachers();
+//    }
+//
+//    public Iterable<CourseEdition> getAllCourseEditions() {
+//        return service.getAllCourseEditions();
+//    }
+//
+//    public boolean defineRucForCourseEdition(CourseEditionID courseEditionID, TeacherID teacherID) {
+//        return service.assignRucToCourseEdition(teacherID, courseEditionID);
+//    }
 }
