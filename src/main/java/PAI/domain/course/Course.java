4ecaec0a1ef17c3d66c48f41ee0444c75f800e31
ma.antunes <1241916@isep.ipp.pt>
2025-04-23 14:49:53 +0100

    refactor(class): refactor Course class attributes (refs #308)

--- src/main/java/PAI/domain/course/Course.java
+++ src/main/java/PAI/domain/course/Course.java
@@ -8,10 +8,8 @@ public class Course implements AggregateRoot<CourseID> {
     private final CourseID _courseID;
     private final Name _name;
     private final Acronym _acronym;
-    private final CourseQuantityCreditsEcts _quantityCreditsEcts;
-    private final DurationCourseInCurricularYear _duration;
 
-    public Course(CourseID courseID, Name name, Acronym acronym, CourseQuantityCreditsEcts quantityCreditsEcts, DurationCourseInCurricularYear durationCourseInCurricularYear){
+    public Course(CourseID courseID, Name name, Acronym acronym){
         if(courseID == null){
             throw new IllegalArgumentException("Course Id must be valid");
         }
@@ -21,18 +19,10 @@ public class Course implements AggregateRoot<CourseID> {
         if(acronym == null){
             throw new IllegalArgumentException("Course Acronym must be valid");
         }
-        if(quantityCreditsEcts == null){
-            throw new IllegalArgumentException("Course Quantity of Credits Ects must be valid");
-        }
-        if(durationCourseInCurricularYear == null){
-            throw new IllegalArgumentException("Course Duration must be valid");
-        }
 
         this._courseID = courseID;
         this._name = name;
         this._acronym = acronym;
-        this._quantityCreditsEcts = quantityCreditsEcts;
-        this._duration = durationCourseInCurricularYear;
     }
 
     @Override
@@ -63,9 +53,7 @@ public class Course implements AggregateRoot<CourseID> {
         }
         Course courseTest = (Course) objectToCompare;
         return _name.equals(courseTest._name) &&
-                _acronym.equals((courseTest._acronym)) &&
-                _quantityCreditsEcts.equals(courseTest._quantityCreditsEcts) &&
-                _duration.equals(courseTest._duration);
+                _acronym.equals((courseTest._acronym));
     }
 
     public Name getName() {
@@ -74,13 +62,5 @@ public class Course implements AggregateRoot<CourseID> {
 
     public Acronym getAcronym() {
         return _acronym;
-    }
-
-    public CourseQuantityCreditsEcts getCourseQuantityCreditsEcts() {
-        return _quantityCreditsEcts;
-    }
-
-    public DurationCourseInCurricularYear getDurationCourseInCurricularYear() {
-        return _duration;
     }
 }
