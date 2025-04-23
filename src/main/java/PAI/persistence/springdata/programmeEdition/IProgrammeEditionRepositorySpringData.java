aa02c133e6b1ed9c807bdad8acf37a5aaef851a8
pedrofg890 <1210608@isep.ipp.pt>
2025-04-23 11:18:40 +0100

    feat: Create ProgrammeEditionRepositorySpringData Interface. Refs #257

---
+++ src/main/java/PAI/persistence/springdata/programmeEdition/IProgrammeEditionRepositorySpringData.java
@@ -0,0 +1,16 @@
+package PAI.persistence.springdata.programmeEdition;
+
+import PAI.persistence.datamodel.ProgrammeIDDataModel;
+import PAI.persistence.datamodel.programmeEdition.ProgrammeEditionDataModel;
+import PAI.persistence.datamodel.programmeEdition.ProgrammeEditionIdDataModel;
+import PAI.persistence.datamodel.schoolYear.SchoolYearIDDataModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface IProgrammeEditionRepositorySpringData extends JpaRepository<ProgrammeEditionDataModel, ProgrammeEditionIdDataModel> {
+
+
+    Optional<ProgrammeEditionDataModel> findProgrammeEditionDataModelByProgrammeIDDataModelAndSchoolYearIDDataModel(
+            ProgrammeIDDataModel programmeIDDataModel, SchoolYearIDDataModel schoolYearIDDataModel);
+}
