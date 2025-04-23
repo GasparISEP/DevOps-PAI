71373df7fc56c454b53161ac5fc3c6dd8c7ad5e9
Dianaguedes19 <1241903@isep.ipp.pt>
2025-04-23 16:09:11 +0100

    Ref #281: After changes in courseEdition by other colleagues i had to change studentGrade Factory too + tests

--- src/test/java/PAI/factory/StudentGradeFactoryImplTest.java
+++ src/test/java/PAI/factory/StudentGradeFactoryImplTest.java
@@ -7,10 +7,12 @@ import PAI.domain.SchoolYear;
 import PAI.domain.StudentGrade;
 import PAI.domain.Student;
 import PAI.domain.programmeEdition.ProgrammeEdition;
+import PAI.persistence.mem.SchoolYearRepositoryImpl;
 import PAI.repository.CourseEditionRepositoryImpl;
 import PAI.repository.ICourseEditionRepository;
 import PAI.repository.ISchoolYearRepository;
 import PAI.repository.programmeEditionRepository.IProgrammeEditionRepository;
+import PAI.repository.programmeEditionRepository.ProgrammeEditionRepositoryImpl;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedConstruction;
 
@@ -23,76 +25,94 @@ import static org.mockito.Mockito.*;
 
 class StudentGradeFactoryImplTest {
 
-//    @Test
-//    public void shouldCreateConstructor() throws Exception {
-//        //Arrange
-//        ICourseEditionRepository courseEditionRepository = mock(CourseEditionRepositoryImpl.class);
-//        CourseEditionID courseEditionID = mock(CourseEditionID.class);
-//        CourseEdition courseEdition = mock(CourseEdition.class);
-//        ProgrammeEditionID programmeEditionID = mock(ProgrammeEditionID.class);
-//        ProgrammeEdition programmeEdition = mock(ProgrammeEdition.class);
-//        SchoolYearID schoolYearID = mock(SchoolYearID.class);
-//        SchoolYear schoolYear = mock(SchoolYear.class);
-//        when(courseEditionRepository.ofIdentity(courseEditionID)).thenReturn(Optional.of(courseEdition));
-//        when(courseEditionRepository.findWhichProgrammeEditionBelongsToACourseEdition(courseEdition)).thenReturn(programmeEditionID);
-//        Date Start = mock(Date.class);
-//        Date End = mock(Date.class);
-//        LocalDate SD = mock(LocalDate.class);
-//        LocalDate ED = mock(LocalDate.class);
-//        Date gradeDate = mock(Date.class);
-//        LocalDate GD = mock(LocalDate.class);
-//
-//        IProgrammeEditionRepository programmeEditionRepository = mock(IProgrammeEditionRepository.class);
-//        ISchoolYearRepository schoolYearRepository = mock(ISchoolYearRepository.class);
-//        when(programmeEditionRepository.ofIdentity(programmeEditionID)).thenReturn(Optional.of(programmeEdition));
-//        when(programmeEditionRepository.getSchoolYearIDByProgrammeEdition(programmeEdition)).thenReturn(schoolYearID);
-//        when(schoolYearRepository.ofIdentity(schoolYearID)).thenReturn(Optional.of(schoolYear));
-//        when(schoolYear.getStartDate()).thenReturn(Start);
-//        when(schoolYear.getEndDate()).thenReturn(End);
-//        when(Start.getLocalDate()).thenReturn(SD);
-//        when(End.getLocalDate()).thenReturn(ED);
-//        when(gradeDate.getLocalDate()).thenReturn(GD);
-//        when(GD.isBefore(SD)).thenReturn(false);
-//        when(GD.isAfter(ED)).thenReturn(false);
-//
-//
-//        StudentID student = mock(StudentID.class);
-//        Grade grade = mock(Grade.class);
-//
-//
-//        try (MockedConstruction<StudentGrade> mockConstruction = mockConstruction(StudentGrade.class, (mock, context) -> {
-//            Grade gradeAtual = (Grade) context.arguments().get(0);
-//            Date dateAtual = (Date) context.arguments().get(1);
-//            StudentID studentAtual = (StudentID) context.arguments().get(2);
-//            CourseEditionID courseEditionIDAtual = (CourseEditionID) context.arguments().get(3);
-//
-//            when(gradeAtual.knowGrade()).thenReturn(11.0);
-//            when(mock.get_grade()).thenReturn(gradeAtual);
-//            when(mock.get_date()).thenReturn(gradeDate);
-//            when(mock.get_studentID()).thenReturn(studentAtual);
-//            when(mock.get_courseEditionID()).thenReturn(courseEditionIDAtual);
-//
-//        })) {
-//
-//            // act
-//            StudentGradeFactoryImpl factory = new StudentGradeFactoryImpl(courseEditionRepository,programmeEditionRepository,schoolYearRepository);
-//            StudentGrade studentGrade = factory.newGradeStudent(grade, gradeDate, student, courseEditionID);
-//
-//            // Assert
-//            assertEquals(1, mockConstruction.constructed().size());
-//            StudentGrade createdStudent = mockConstruction.constructed().get(0);
-//
-//            assertEquals(grade, createdStudent.get_grade());
-//            assertEquals(gradeDate, createdStudent.get_date());
-//            assertEquals(student, createdStudent.get_studentID());
-//            assertEquals(courseEditionID, createdStudent.get_courseEditionID());
-//            assertNotNull(studentGrade);
-//
-//        }
-//
-//    }
+    @Test
+    public void shouldCreateConstructor() throws Exception {
+        //Arrange
+        ICourseEditionRepository courseEditionRepository = mock(CourseEditionRepositoryImpl.class);
+        CourseEditionID courseEditionID = mock(CourseEditionID.class);
+        IProgrammeEditionRepository programmeEditionRepository = mock(ProgrammeEditionRepositoryImpl.class);
+        ISchoolYearRepository schoolYearRepository = mock(SchoolYearRepositoryImpl.class);
 
 
+        CourseEdition courseEdition = mock(CourseEdition.class);
+        when(courseEditionRepository.ofIdentity(courseEditionID))
+                .thenReturn(Optional.of(courseEdition));
+
+
+        ProgrammeEditionID programmeEditionID = mock(ProgrammeEditionID.class);
+        when(courseEdition.getProgrammeEditionID())
+                .thenReturn(programmeEditionID);
+
+
+        ProgrammeEdition programmeEdition = mock(ProgrammeEdition.class);
+        when(programmeEditionRepository.ofIdentity(programmeEditionID))
+                .thenReturn(Optional.of(programmeEdition));
+
+
+        SchoolYearID schoolYearID = mock(SchoolYearID.class);
+        when(programmeEdition.findSchoolYearIDInProgrammeEdition())
+                .thenReturn(schoolYearID);
+
+
+        SchoolYear schoolYear = mock(SchoolYear.class);
+        when(schoolYearRepository.ofIdentity(schoolYearID))
+                .thenReturn(Optional.of(schoolYear));
+
+
+        Date Start = mock(Date.class);
+        Date End   = mock(Date.class);
+        LocalDate SD = mock(LocalDate.class);
+        LocalDate ED = mock(LocalDate.class);
+        when(schoolYear.getStartDate()).thenReturn(Start);
+        when(schoolYear.getEndDate()).thenReturn(End);
+        when(Start.getLocalDate()).thenReturn(SD);
+        when(End.getLocalDate()).thenReturn(ED);
+
+
+        Date gradeDate = mock(Date.class);
+        LocalDate GD = mock(LocalDate.class);
+        when(gradeDate.getLocalDate()).thenReturn(GD);
+        when(GD.isBefore(SD)).thenReturn(false);
+        when(GD.isAfter(ED)).thenReturn(false);
+
+
+        StudentID student = mock(StudentID.class);
+        Grade grade = mock(Grade.class);
+
+
+        try (MockedConstruction<StudentGrade> mockConstruction = mockConstruction(StudentGrade.class, (mock, context) -> {
+            Grade gradeAtual = (Grade) context.arguments().get(0);
+            Date dateAtual = (Date) context.arguments().get(1);
+            StudentID studentAtual = (StudentID) context.arguments().get(2);
+            CourseEditionID courseEditionIDAtual = (CourseEditionID) context.arguments().get(3);
+
+            when(gradeAtual.knowGrade()).thenReturn(11.0);
+            when(mock.get_grade()).thenReturn(gradeAtual);
+            when(mock.get_date()).thenReturn(gradeDate);
+            when(mock.get_studentID()).thenReturn(studentAtual);
+            when(mock.get_courseEditionID()).thenReturn(courseEditionIDAtual);
+
+        })) {
+
+            // act
+            StudentGradeFactoryImpl factory = new StudentGradeFactoryImpl(courseEditionRepository,programmeEditionRepository,schoolYearRepository);
+            StudentGrade studentGrade = factory.newGradeStudent(grade, gradeDate, student, courseEditionID);
+
+            // Assert
+            assertEquals(1, mockConstruction.constructed().size());
+            StudentGrade createdStudent = mockConstruction.constructed().get(0);
+
+            assertEquals(grade, createdStudent.get_grade());
+            assertEquals(gradeDate, createdStudent.get_date());
+            assertEquals(student, createdStudent.get_studentID());
+            assertEquals(courseEditionID, createdStudent.get_courseEditionID());
+            assertNotNull(studentGrade);
+
+        }
+
+    }
+
+//
 //    @Test
 //    public void shouldNotCreateConstructorWhenDateBeforeBeginningOfSchoolYear() throws Exception {
 //        //Arrange
@@ -135,7 +155,7 @@ class StudentGradeFactoryImplTest {
 //                factory.newGradeStudent(grade, gradeDate, student, courseEditionID));
 //        assertEquals("Date is out of Range", exception.getMessage());
 //    }
-
+//
 //    @Test
 //    public void shouldNotCreateConstructorWhenDateAfterEndOfSchoolYear() throws Exception {
 //        //Arrange
