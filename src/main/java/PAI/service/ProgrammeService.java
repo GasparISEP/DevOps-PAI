0d111293985defcf5f86655472e9354b615fb78a
RicardoMarques21 <1241923@isep.ipp.pt>
2025-04-25 16:17:26 +0100

    Added method registerProgramme to ProgrammeService and tests. Related #277

--- src/main/java/PAI/service/ProgrammeService.java
+++ src/main/java/PAI/service/ProgrammeService.java
@@ -1,5 +1,7 @@
 package PAI.service;
 
+import PAI.VOs.*;
+import PAI.domain.programme.Programme;
 import PAI.factory.IProgrammeFactory;
 import PAI.repository.programmeRepository.IProgrammeRepository;
 import org.springframework.stereotype.Service;
@@ -20,5 +22,10 @@ public class ProgrammeService implements IProgrammeService {
             throw new IllegalArgumentException("Programme Repository cannot be null");
         }
         _programmeRepository = programmeRepository;
+    }
+
+    public Programme registerProgramme(NameWithNumbersAndSpecialChars name, Acronym acronym, QuantEcts quantityOfEcts, QuantSemesters quantityOfSemesters, DegreeTypeID degreeTypeID, DepartmentID departmentID, TeacherID programmeDirectorID) throws Exception {
+        Programme programme = _programmeFactory.registerProgramme(name, acronym, quantityOfEcts, quantityOfSemesters, degreeTypeID, departmentID, programmeDirectorID);
+        return _programmeRepository.save(programme);
     }
 }
--- src/test/java/PAI/service/ProgrammeServiceTest.java
+++ src/test/java/PAI/service/ProgrammeServiceTest.java
@@ -1,11 +1,14 @@
 package PAI.service;
 
+import PAI.VOs.*;
+import PAI.domain.programme.Programme;
 import PAI.factory.IProgrammeFactory;
 import PAI.repository.programmeRepository.IProgrammeRepository;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class ProgrammeServiceTest {
 
@@ -40,6 +43,32 @@ class ProgrammeServiceTest {
 
         //Act+Assert
         assertThrows(Exception.class, () -> new ProgrammeService(programmeFactory, programmeRepository));
+    }
+
+    @Test
+    void shouldRegisterProgramme() throws Exception {
+        //Arrange
+        IProgrammeFactory programmeFactory = mock(IProgrammeFactory.class);
+        IProgrammeRepository programmeRepository = mock(IProgrammeRepository.class);
+        NameWithNumbersAndSpecialChars name = mock(NameWithNumbersAndSpecialChars.class);
+        Acronym acronym = mock(Acronym.class);
+        QuantEcts quantityOfEcts = mock(QuantEcts.class);
+        QuantSemesters quantityOfSemesters = mock(QuantSemesters.class);
+        DegreeTypeID degreeTypeID = mock(DegreeTypeID.class);
+        DepartmentID departmentID = mock(DepartmentID.class);
+        TeacherID programmeDirectorID = mock(TeacherID.class);
+        Programme programme =mock(Programme.class);
+
+        when(programmeFactory.registerProgramme(name, acronym, quantityOfEcts, quantityOfSemesters, degreeTypeID, departmentID, programmeDirectorID)).thenReturn(programme);
+        when(programmeRepository.save(programme)).thenReturn(programme);
+
+        ProgrammeService service = new ProgrammeService(programmeFactory, programmeRepository);
+
+        //Act
+        Programme result = service.registerProgramme(name, acronym, quantityOfEcts, quantityOfSemesters, degreeTypeID, departmentID, programmeDirectorID);
+
+        //Assert
+        assertNotNull(result);
     }
 
   
