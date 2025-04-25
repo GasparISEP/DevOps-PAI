449f4564af14cba382327ef509bb390dff928dcb
RicardoMarques21 <1241923@isep.ipp.pt>
2025-04-25 16:48:14 +0100

    Added method changeProgrammeDirector to ProgrammeService and tests, updated ChangeProgrammeDirector controller. Related #277

--- src/main/java/PAI/service/ProgrammeService.java
+++ src/main/java/PAI/service/ProgrammeService.java
@@ -1,8 +1,10 @@
 package PAI.service;
 
 import PAI.VOs.*;
+import PAI.domain.Teacher;
 import PAI.domain.programme.Programme;
 import PAI.factory.IProgrammeFactory;
+import PAI.repository.ITeacherRepository;
 import PAI.repository.programmeRepository.IProgrammeRepository;
 import org.springframework.stereotype.Service;
 
@@ -11,8 +13,9 @@ public class ProgrammeService implements IProgrammeService {
 
     private final IProgrammeFactory _programmeFactory;
     private final IProgrammeRepository _programmeRepository;
+    private final ITeacherRepository _teacherRepository;
 
-    public ProgrammeService(IProgrammeFactory programmeFactory, IProgrammeRepository programmeRepository) {
+    public ProgrammeService(IProgrammeFactory programmeFactory, IProgrammeRepository programmeRepository, ITeacherRepository teacherRepository) {
         if (programmeFactory == null) {
             throw new IllegalArgumentException("Programme Factory cannot be null");
         }
@@ -22,10 +25,29 @@ public class ProgrammeService implements IProgrammeService {
             throw new IllegalArgumentException("Programme Repository cannot be null");
         }
         _programmeRepository = programmeRepository;
+
+        if (teacherRepository == null) {
+            throw new IllegalArgumentException("Teacher Repository cannot be null");
+        }
+        _teacherRepository = teacherRepository;
     }
 
     public Programme registerProgramme(NameWithNumbersAndSpecialChars name, Acronym acronym, QuantEcts quantityOfEcts, QuantSemesters quantityOfSemesters, DegreeTypeID degreeTypeID, DepartmentID departmentID, TeacherID programmeDirectorID) throws Exception {
         Programme programme = _programmeFactory.registerProgramme(name, acronym, quantityOfEcts, quantityOfSemesters, degreeTypeID, departmentID, programmeDirectorID);
         return _programmeRepository.save(programme);
+    }
+
+    public boolean changeProgrammeDirector(Programme programme, Teacher programmeDirector) throws Exception {
+        if (programme == null) {
+            throw new IllegalArgumentException("Programme cannot be null");
+        }
+        if (programmeDirector == null) {
+            throw new IllegalArgumentException("Programme Director cannot be null");
+        }
+
+        ProgrammeID programmeID = programme.identity();
+        TeacherID programmeDirectorID = programmeDirector.identity();
+
+        return _programmeRepository.changeProgrammeDirector(programmeID,programmeDirectorID);
     }
 }
--- src/test/java/PAI/service/ProgrammeServiceTest.java
+++ src/test/java/PAI/service/ProgrammeServiceTest.java
@@ -1,8 +1,10 @@
 package PAI.service;
 
 import PAI.VOs.*;
+import PAI.domain.Teacher;
 import PAI.domain.programme.Programme;
 import PAI.factory.IProgrammeFactory;
+import PAI.repository.ITeacherRepository;
 import PAI.repository.programmeRepository.IProgrammeRepository;
 import org.junit.jupiter.api.Test;
 
@@ -17,9 +19,10 @@ class ProgrammeServiceTest {
         //Arrange
         IProgrammeFactory programmeFactory = mock(IProgrammeFactory.class);
         IProgrammeRepository programmeRepository = mock(IProgrammeRepository.class);
+        ITeacherRepository teacherRepository = mock(ITeacherRepository.class);
 
         //Act
-        ProgrammeService service = new ProgrammeService(programmeFactory, programmeRepository);
+        ProgrammeService service = new ProgrammeService(programmeFactory, programmeRepository, teacherRepository);
 
         //Assert
         assertNotNull(service);
@@ -30,9 +33,10 @@ class ProgrammeServiceTest {
         //Arrange
         IProgrammeFactory programmeFactory = mock(IProgrammeFactory.class);
         IProgrammeRepository programmeRepository = null;
+        ITeacherRepository teacherRepository = mock(ITeacherRepository.class);
 
         //Act+Assert
-        assertThrows(Exception.class, () -> new ProgrammeService(programmeFactory, programmeRepository));
+        assertThrows(Exception.class, () -> new ProgrammeService(programmeFactory, programmeRepository, teacherRepository));
     }
 
     @Test
@@ -40,9 +44,21 @@ class ProgrammeServiceTest {
         //Arrange
         IProgrammeFactory programmeFactory = null;
         IProgrammeRepository programmeRepository = mock(IProgrammeRepository.class);
+        ITeacherRepository teacherRepository = mock(ITeacherRepository.class);
 
         //Act+Assert
-        assertThrows(Exception.class, () -> new ProgrammeService(programmeFactory, programmeRepository));
+        assertThrows(Exception.class, () -> new ProgrammeService(programmeFactory, programmeRepository,teacherRepository));
+    }
+
+    @Test
+    void shouldThrowExceptionIfTeacherRepositoryIsNull() {
+        //Arrange
+        ITeacherRepository teacherRepository = null;
+        IProgrammeFactory programmeFactory = mock(IProgrammeFactory.class);
+        IProgrammeRepository programmeRepository = mock(IProgrammeRepository.class);
+
+        //Act+Assert
+        assertThrows(Exception.class, () -> new ProgrammeService(programmeFactory, programmeRepository,teacherRepository));
     }
 
     @Test
@@ -50,6 +66,7 @@ class ProgrammeServiceTest {
         //Arrange
         IProgrammeFactory programmeFactory = mock(IProgrammeFactory.class);
         IProgrammeRepository programmeRepository = mock(IProgrammeRepository.class);
+        ITeacherRepository teacherRepository = mock(ITeacherRepository.class);
         NameWithNumbersAndSpecialChars name = mock(NameWithNumbersAndSpecialChars.class);
         Acronym acronym = mock(Acronym.class);
         QuantEcts quantityOfEcts = mock(QuantEcts.class);
@@ -62,13 +79,68 @@ class ProgrammeServiceTest {
         when(programmeFactory.registerProgramme(name, acronym, quantityOfEcts, quantityOfSemesters, degreeTypeID, departmentID, programmeDirectorID)).thenReturn(programme);
         when(programmeRepository.save(programme)).thenReturn(programme);
 
-        ProgrammeService service = new ProgrammeService(programmeFactory, programmeRepository);
+        ProgrammeService service = new ProgrammeService(programmeFactory, programmeRepository, teacherRepository);
 
         //Act
         Programme result = service.registerProgramme(name, acronym, quantityOfEcts, quantityOfSemesters, degreeTypeID, departmentID, programmeDirectorID);
 
         //Assert
         assertNotNull(result);
+    }
+
+    @Test
+    void shouldChangeProgrammeDirector() throws Exception {
+        //Arrange
+        IProgrammeFactory programmeFactory = mock(IProgrammeFactory.class);
+        IProgrammeRepository programmeRepository = mock(IProgrammeRepository.class);
+        ITeacherRepository teacherRepository = mock(ITeacherRepository.class);
+        ProgrammeService service = new ProgrammeService(programmeFactory, programmeRepository, teacherRepository);
+
+        Programme programme = mock(Programme.class);
+        Teacher teacher = mock(Teacher.class);
+        ProgrammeID programmeID = mock(ProgrammeID.class);
+        TeacherID teacherID = mock(TeacherID.class);
+
+        when(programme.identity()).thenReturn(programmeID);
+        when(teacher.identity()).thenReturn(teacherID);
+        when(programmeRepository.changeProgrammeDirector(programmeID,teacherID)).thenReturn(true);
+
+        //Act
+        boolean result = service.changeProgrammeDirector(programme,teacher);
+
+        //Assert
+        assertTrue(result);
+    }
+
+    @Test
+    void shouldNotChangeProgrammeDirectorIfProgrammeIsNull() throws Exception {
+        //Arrange
+        IProgrammeFactory programmeFactory = mock(IProgrammeFactory.class);
+        IProgrammeRepository programmeRepository = mock(IProgrammeRepository.class);
+        ITeacherRepository teacherRepository = mock(ITeacherRepository.class);
+        ProgrammeService service = new ProgrammeService(programmeFactory, programmeRepository, teacherRepository);
+
+        Programme programme = null;
+        Teacher teacher = mock(Teacher.class);
+
+        //Act+Assert
+        assertThrows(Exception.class, () -> service.changeProgrammeDirector(programme, teacher));
+    }
+
+    @Test
+    void shouldNotChangeProgrammeDirectorIfTeacherIsNull() throws Exception {
+        //Arrange
+        IProgrammeFactory programmeFactory = mock(IProgrammeFactory.class);
+        IProgrammeRepository programmeRepository = mock(IProgrammeRepository.class);
+        ITeacherRepository teacherRepository = mock(ITeacherRepository.class);
+        ProgrammeService service = new ProgrammeService(programmeFactory, programmeRepository, teacherRepository);
+
+        Programme programme = mock(Programme.class);
+        Teacher teacher = null;
+
+        //Act+Assert
+        assertThrows(Exception.class, () -> service.changeProgrammeDirector(programme, teacher));
+
     }
 
   
