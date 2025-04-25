4af56deb29fa49b3f78de8f79079ffb83ff96d9f
sramos97 <1241925@isep.ipp.pt>
2025-04-25 17:44:23 +0100

    Creation of Interface from CourseEditionEnrolmentService (Ref #375, #441)

---
+++ src/main/java/PAI/service/ICourseEditionEnrolmentService.java
@@ -0,0 +1,17 @@
+package PAI.service;
+
+import PAI.VOs.CourseEditionID;
+import PAI.VOs.ProgrammeEditionID;
+import PAI.VOs.StudentID;
+
+import java.util.List;
+
+public interface ICourseEditionEnrolmentService {
+
+    List<ProgrammeEditionID> findProgrammeEditionIDsThatStudentIsEnrolled(StudentID studentId);
+
+    List<CourseEditionID> findCourseEditionIDsByProgrammeEdition(ProgrammeEditionID programmeEditionID);
+
+    boolean enrolStudentInACourseEdition(StudentID studentId, CourseEditionID courseEditionId);
+
+}
