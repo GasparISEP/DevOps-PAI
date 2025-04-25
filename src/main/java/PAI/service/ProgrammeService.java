ec337066f72d1656032fb8696f6b9351bd0873d2
RicardoMarques21 <1241923@isep.ipp.pt>
2025-04-25 15:12:17 +0100

    Created ProgrammeService constructor and added tests. Related #277

--- src/main/java/PAI/service/ProgrammeService.java
+++ src/main/java/PAI/service/ProgrammeService.java
@@ -1,4 +1,24 @@
 package PAI.service;
 
+import PAI.factory.IProgrammeFactory;
+import PAI.repository.programmeRepository.IProgrammeRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProgrammeService implements IProgrammeService {
+
+    private final IProgrammeFactory _programmeFactory;
+    private final IProgrammeRepository _programmeRepository;
+
+    public ProgrammeService(IProgrammeFactory programmeFactory, IProgrammeRepository programmeRepository) {
+        if (programmeFactory == null) {
+            throw new IllegalArgumentException("Programme Factory cannot be null");
+        }
+        _programmeFactory = programmeFactory;
+
+        if (programmeRepository == null) {
+            throw new IllegalArgumentException("Programme Repository cannot be null");
+        }
+        _programmeRepository = programmeRepository;
+    }
 }
