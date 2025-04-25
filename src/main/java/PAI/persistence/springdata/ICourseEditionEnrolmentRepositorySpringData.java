e32be2ef1bd4da365a9abe405ebaf2db6d80db10
anaconde1241897 <1241897@isep.ipp.pt>
2025-04-25 18:13:17 +0100

    refactor(spring data jpa): add a method to CourseEditionEnrolmentRepositorySpringDataJpa Interface (#374, #326)

--- src/main/java/PAI/persistence/springdata/ICourseEditionEnrolmentRepositorySpringData.java
+++ src/main/java/PAI/persistence/springdata/ICourseEditionEnrolmentRepositorySpringData.java
@@ -1,8 +1,12 @@
 package PAI.persistence.springdata;
 
+import PAI.VOs.CourseEditionID;
+import PAI.VOs.StudentID;
 import PAI.persistence.datamodel.CourseEditionEnrolmentDataModel;
 import PAI.persistence.datamodel.CourseEditionEnrolmentIDDataModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ICourseEditionEnrolmentRepositorySpringData extends JpaRepository<CourseEditionEnrolmentDataModel, CourseEditionEnrolmentIDDataModel> {
+
+    boolean existsById_StudentIDAndId_CourseEditionIDAndIsActiveTrue(StudentID studentID, CourseEditionID courseEditionID);
 }
