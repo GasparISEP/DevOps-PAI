25b21b08da5592305703b87a40b5a9c5d2a5944c
andremsalisep <1241899@isep.ipp.pt>
2025-04-29 17:06:09 +0100

    added StudyPlan Service and edited repository and respective controllers (#279)

--- src/main/java/PAI/repository/studyPlanRepository/IStudyPlanRepository.java
+++ src/main/java/PAI/repository/studyPlanRepository/IStudyPlanRepository.java
@@ -8,10 +8,4 @@ import java.util.List;
 
 public interface IStudyPlanRepository extends IRepository<StudyPlanID, StudyPlan> {
 
-    boolean createStudyPlan(ProgrammeID programmeID, Date implementationDate, DurationInYears durationInYears, MaxEcts quantityOfEcts);
-
-    List<StudyPlan> getAllStudyPlansByProgrammeId(ProgrammeID programmeID);
-    List<StudyPlan> getAllStudyPlans();
-
-    StudyPlanID getLatestStudyPlanIDByProgrammeID(ProgrammeID programmeID);
 }
