80f9dc332dab4dd9ae4ede9936a4c30c26f58be8
Ruben Rocha <1241924@isep.ipp.pt>
2025-04-29 11:42:14 +0100

    refactor: changed method signature to Optional. Refs #433

--- src/main/java/PAI/persistence/springdata/ISchoolYearRepositorySpringData.java
+++ src/main/java/PAI/persistence/springdata/ISchoolYearRepositorySpringData.java
@@ -8,6 +8,6 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.Optional;
 
 public interface ISchoolYearRepositorySpringData extends JpaRepository<SchoolYearDataModel, SchoolYearIDDataModel>{
-    @Query("SELECT schoolYear.id FROM SchoolYearDataModel schoolYear WHERE schoolYear.startDate < CURRENT_DATE AND schoolYear.endDate > CURRENT_DATE")
-    Optional<SchoolYearIDDataModel> findCurrentSchoolYear();
+    @Query("SELECT schoolYear FROM SchoolYearDataModel schoolYear WHERE schoolYear.startDate < CURRENT_DATE AND schoolYear.endDate > CURRENT_DATE")
+    Optional<SchoolYearDataModel> findCurrentSchoolYear();
 }
