81b1d0aaafef5c3624fb29a8a0328e11390bb920
Nuno Cruz <1241919@isep.ipp.pt>
2025-04-30 18:25:35 +0100

    Added new method to ProgID to then call in ProgService and tested it. ref #277

--- src/main/java/PAI/VOs/ProgrammeID.java
+++ src/main/java/PAI/VOs/ProgrammeID.java
@@ -42,4 +42,8 @@ public class ProgrammeID implements DomainId {
     public Acronym getAcronym() {
         return _acronym;
     }
+
+    public boolean hasThisName(NameWithNumbersAndSpecialChars name) {
+        return _name.equals(name);
+    }
 }
