56d661f85fee16107c80cbc48361b7966365dfc7
RicardoJacinto <1241922@isep.ipp.pt>
2025-05-02 14:53:45 +0100

    Creation of ProgrammeEnrolmentInitializer, #483

---
+++ src/main/java/PAI/initializer/ProgrammeEnrolmentInitializer.java
@@ -0,0 +1,58 @@
+package PAI.initializer;
+
+import PAI.controller.US09_EnrolStudentInProgrammeController;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.UUID;
+
+@Configuration
+@Profile("programmeEnrolment")
+public class ProgrammeEnrolmentInitializer {
+
+    @Bean
+    public CommandLineRunner loadDataRegisterStudent(US09_EnrolStudentInProgrammeController controller) {
+        return (args) -> {
+            loadStudents(controller);
+        };
+    }
+
+    private void loadStudents(US09_EnrolStudentInProgrammeController controller) {
+        String csvFile = "src/main/resources/ProgrammeEnrolment_Data.csv";
+
+        long startTime = System.currentTimeMillis();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+            String line;
+            boolean isFirstLine = true;
+            while ((line = br.readLine()) != null) {
+                if (isFirstLine) {
+                    isFirstLine = false;
+                    continue;
+                }
+
+                line = line.replace("\uFEFF", "");
+                String[] fields = line.split(";");
+
+                int studentNumber = Integer.parseInt(fields[0]);
+                UUID accessMethodIDStr = UUID.fromString(fields[1]);
+                String programmeName = fields[2];
+                String acronym = fields[3];
+                String enrolmentDate = fields[4];
+
+                controller.enrolStudentInProgramme(studentNumber, accessMethodIDStr, programmeName, acronym, enrolmentDate);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        long endTime = System.currentTimeMillis();
+        long duration = endTime - startTime;
+
+        System.out.println("\nProgrammeEnrolment data loading time: " + duration + " ms\n");
+    }
+}
