24ec8c555a32c5b9b7fd1c249854a165b91e8e78
anaconde1241897 <1241897@isep.ipp.pt>
2025-05-02 18:44:59 +0100

    refactor (spring data): remove some unnecessary imports and annotations #374, Closes #326.

--- src/main/java/PAI/domain/courseEditionEnrolment/ICourseEditionEnrolmentFactory.java
+++ src/main/java/PAI/domain/courseEditionEnrolment/ICourseEditionEnrolmentFactory.java
@@ -4,11 +4,8 @@ import PAI.VOs.CourseEditionID;
 import PAI.VOs.Date;
 import PAI.VOs.EnrolmentStatus;
 import PAI.VOs.StudentID;
-import org.springframework.stereotype.Component;
 
-import java.time.LocalDate;
 
-@Component
 public interface ICourseEditionEnrolmentFactory {
 
     CourseEditionEnrolment createCourseEditionEnrolment(StudentID studentId, CourseEditionID courseEditionId);
