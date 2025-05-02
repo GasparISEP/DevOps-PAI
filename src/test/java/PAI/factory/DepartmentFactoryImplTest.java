54acc394a8ee6637838ee734f51250c12198b18d
IBarbosa8 <1241909@isep.ipp.pt>
2025-05-02 08:41:43 +0100

    Refs #366. Add test to DepartmentFactoryImpl for full line coverage.

--- src/test/java/PAI/factory/DepartmentFactoryImplTest.java
+++ src/test/java/PAI/factory/DepartmentFactoryImplTest.java
@@ -1,6 +1,7 @@
 package PAI.factory;
 import PAI.VOs.DepartmentAcronym;
 import PAI.VOs.Name;
+import PAI.VOs.TeacherID;
 import PAI.domain.Department;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedConstruction;
@@ -35,6 +36,39 @@ class DepartmentFactoryImplTest {
         assertEquals(acronym, departmentDouble.constructed().get(0).getAcronym());
         assertEquals(acronym, department.getAcronym());
         assertEquals(name,department.getName());
+        }
+    }
+
+    @Test
+    void shouldCreateAValidDepartmentWithDirectorWhenMockedConstructorIsGiven() throws Exception {
+        //arrange
+        DepartmentAcronym acronym = mock(DepartmentAcronym.class);
+        Name name = mock(Name.class);
+        TeacherID director= mock(TeacherID.class);
+
+        try (MockedConstruction<Department> departmentDouble = mockConstruction(Department.class, (mock, context) -> {
+            DepartmentAcronym actualAcronym = (DepartmentAcronym) context.arguments().get(0);
+            Name actualName = (Name) context.arguments().get(1);
+            TeacherID actualTeacherID= (TeacherID) context.arguments().get(2);
+            when(mock.getAcronym()).thenReturn(actualAcronym);
+            when(mock.getName()).thenReturn(actualName);
+            when(mock.getDirectorID()).thenReturn(actualTeacherID);
+
+        })) {
+
+            DepartmentFactoryImpl factory = new DepartmentFactoryImpl();
+
+            //act
+            Department department = factory.newDepartment(acronym, name, director);
+
+            //assert
+            List<Department> departments = departmentDouble.constructed();
+            assertEquals(1, departments.size());
+
+            assertEquals(acronym, departmentDouble.constructed().get(0).getAcronym());
+            assertEquals(acronym, department.getAcronym());
+            assertEquals(name,department.getName());
+            assertEquals(director,department.getDirectorID());
         }
     }
 }
