243234ea017bed88d19259a612668cfeb59f73d5
MiguelAntuns7 <1241918@isep.ipp.pt>
2025-05-05 22:19:06 +0100

    Closes #271: Change registerTeacher method to throw BusinessRuleViolation Exception for when a Teacher is already registered instead of Optional.empty()

--- src/test/java/PAI/service/TeacherServiceImplTest.java
+++ src/test/java/PAI/service/TeacherServiceImplTest.java
@@ -2,6 +2,7 @@ package PAI.service;
 
 import PAI.VOs.*;
 import PAI.domain.Teacher;
+import PAI.exception.BusinessRuleViolationException;
 import PAI.factory.teacher.ITeacherFactory;
 import PAI.factory.teacher.TeacherFactoryImpl;
 import PAI.persistence.springdata.teacher.TeacherRepositorySpringDataImpl;
@@ -170,12 +171,14 @@ class TeacherServiceImplTest {
         when(teacherRepositoryDouble.existsByTeacherIdOrNif(teacherIDdouble, nifDouble)).thenReturn(true);
 
         // Act
-        Optional<TeacherID> result = teacherService.registerTeacher(
-                acronymDouble, nameDouble, emailDouble, nifDouble, phoneNumberDouble, academicBackgroundDouble,
-                streetDouble, postalCodeDouble, locationDouble, countryDouble, departmentIDDouble);
+        Exception exception = assertThrows(BusinessRuleViolationException.class, () -> {
+            teacherService.registerTeacher(
+                    acronymDouble, nameDouble, emailDouble, nifDouble, phoneNumberDouble, academicBackgroundDouble,
+                    streetDouble, postalCodeDouble, locationDouble, countryDouble, departmentIDDouble);
+        });
 
         // Assert
-        assertTrue(result.isEmpty());
+        assertEquals("Teacher with the provided Acronym or NIF is already registered.", exception.getMessage());
     }
 
     @Test
