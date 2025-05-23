25b842838f64dfe82fcb1159657f35c6fb22e14e
paulojorgeribeiro <1241920@isep.ipp.pt>
2025-05-23 03:08:45 +0100

    #500 #587 Update StudentProgrammeEditionEnrolmentAssemblerImpl.java

--- src/main/java/PAI/assembler/programmeEditionEnrolment/StudentProgrammeEditionEnrolmentAssemblerImpl.java
+++ src/main/java/PAI/assembler/programmeEditionEnrolment/StudentProgrammeEditionEnrolmentAssemblerImpl.java
@@ -1,4 +1,26 @@
 package PAI.assembler.programmeEditionEnrolment;
 
-public class StudentProgrammeEditionEnrolmentAssemblerImpl {
+import PAI.VOs.ProgrammeID;
+import PAI.VOs.SchoolYearID;
+import PAI.domain.programmeEdition.ProgrammeEdition;
+import PAI.dto.programmeEditionEnrolment.StudentProgrammeEditionEnrolmentDTO;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StudentProgrammeEditionEnrolmentAssemblerImpl implements IStudentProgrammeEditionEnrolmentAssembler {
+
+    public StudentProgrammeEditionEnrolmentDTO toDTO(ProgrammeEdition programmeEdition) {
+        if (programmeEdition == null) {
+            throw new IllegalArgumentException("ProgrammeEdition cannot be null");
+        }
+
+        ProgrammeID programmeID = programmeEdition.findProgrammeIDInProgrammeEdition();
+        SchoolYearID schoolYearID = programmeEdition.findSchoolYearIDInProgrammeEdition();
+
+        return new StudentProgrammeEditionEnrolmentDTO(
+                programmeID.getAcronym().toString(),
+                programmeID.getName().toString(),
+                schoolYearID.getSchoolYearID().toString()
+        );
+    }
 }
