710946a16d6408a8e214a3ea5570c375c8c54e52
MiguelAntuns7 <1241918@isep.ipp.pt>
2025-05-23 17:45:23 +0100

    Refs #561: Change CourseDTOCommand to be record instead of class + test

--- src/test/java/PAI/dto/course/CourseDTOCommandTest.java
+++ src/test/java/PAI/dto/course/CourseDTOCommandTest.java
@@ -1,74 +1,26 @@
 package PAI.dto.course;
-
 import PAI.VOs.Acronym;
 import PAI.VOs.CourseID;
 import PAI.VOs.Name;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 class CourseDTOCommandTest {
 
     @Test
-    void testConstructorNotNull () {
+    void shouldCreateValidCourseDTOCommandWithValidVOs() {
         // Arrange
-        CourseID courseIDDouble = mock(CourseID.class);
-        Name nameDouble = mock(Name.class);
-        Acronym acronymDouble = mock(Acronym.class);
+        Name name = new Name("Desenvolvimento de Software");
+        Acronym acronym = new Acronym("DSOFT");
+        CourseID courseId = new CourseID(acronym, name);
 
         // Act
-        CourseDTOCommand courseDTOCommand = new CourseDTOCommand(courseIDDouble, nameDouble, acronymDouble);
+        CourseDTOCommand command = new CourseDTOCommand(courseId, name, acronym);
+        CourseDTOCommand expected = new CourseDTOCommand(courseId, name, acronym);
 
         // Assert
-        assertNotNull(courseDTOCommand);
-    }
-
-    @Test
-    void testGetCourseIDShouldReturnCourseID () {
-        // Arrange
-        CourseID courseIDDouble = mock(CourseID.class);
-        Name nameDouble = mock(Name.class);
-        Acronym acronymDouble = mock(Acronym.class);
-
-        CourseDTOCommand courseDTOCommand = new CourseDTOCommand(courseIDDouble, nameDouble, acronymDouble);
-
-        // Act
-        CourseID result = courseDTOCommand.getCourseId();
-
-        // Assert
-        assertEquals(courseIDDouble, result);
-    }
-
-    @Test
-    void testGetNameShouldReturnName () {
-        // Arrange
-        CourseID courseIDDouble = mock(CourseID.class);
-        Name nameDouble = mock(Name.class);
-        Acronym acronymDouble = mock(Acronym.class);
-
-        CourseDTOCommand courseDTOCommand = new CourseDTOCommand(courseIDDouble, nameDouble, acronymDouble);
-
-        // Act
-        Name result = courseDTOCommand.getName();
-
-        // Assert
-        assertEquals(nameDouble, result);
-    }
-
-    @Test
-    void testGetAcronymShouldReturnAcronym () {
-        // Arrange
-        CourseID courseIDDouble = mock(CourseID.class);
-        Name nameDouble = mock(Name.class);
-        Acronym acronymDouble = mock(Acronym.class);
-
-        CourseDTOCommand courseDTOCommand = new CourseDTOCommand(courseIDDouble, nameDouble, acronymDouble);
-
-        // Act
-        Acronym result = courseDTOCommand.getAcronym();
-
-        // Assert
-        assertEquals(acronymDouble, result);
+        assertEquals(expected, command);
     }
 }
