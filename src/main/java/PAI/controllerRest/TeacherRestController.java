a6cba7f5a2f4c5f167edf08046fc5aa396881754
pedrofg890 <1210608@isep.ipp.pt>
2025-05-23 22:10:44 +0100

    feat: add GET Command to TeacherRestController and tests. Refs #581, #500

--- src/main/java/PAI/controllerRest/TeacherRestController.java
+++ src/main/java/PAI/controllerRest/TeacherRestController.java
@@ -2,7 +2,12 @@ package PAI.controllerRest;
 
 
 import PAI.assembler.teacher.ITeacherAssembler;
+import PAI.domain.teacher.Teacher;
+import PAI.dto.teacher.TeacherDTO;
 import PAI.service.teacher.ITeacherService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +21,20 @@ public class TeacherRestController {
     public TeacherRestController(ITeacherService teacherService, ITeacherAssembler teacherAssembler) {
         this.teacherService = teacherService;
         this.teacherAssembler = teacherAssembler;
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAllTeachers() {
+        try {
+            Iterable<Teacher> teachers = teacherService.getAllTeachers();
+            Iterable<TeacherDTO> teacherDTOs = teacherAssembler.toDTOs(teachers);
+            return ResponseEntity.ok(teacherDTOs);
+
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Unexpected error occurred");
+        }
     }
 }
