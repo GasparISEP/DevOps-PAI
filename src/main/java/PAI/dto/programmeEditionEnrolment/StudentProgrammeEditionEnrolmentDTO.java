8cd0faf30c10d2b050a46f0ef78c49ef3d521144
paulojorgeribeiro <1241920@isep.ipp.pt>
2025-05-23 03:09:40 +0100

    #500 #587 Update StudentProgrammeEditionEnrolmentDTO.java

--- src/main/java/PAI/dto/programmeEditionEnrolment/StudentProgrammeEditionEnrolmentDTO.java
+++ src/main/java/PAI/dto/programmeEditionEnrolment/StudentProgrammeEditionEnrolmentDTO.java
@@ -1,4 +1,26 @@
 package PAI.dto.programmeEditionEnrolment;
 
+
 public class StudentProgrammeEditionEnrolmentDTO {
+    private String programmeAcronym;
+    private String programmeName;
+    private String schoolYearId;
+
+    public StudentProgrammeEditionEnrolmentDTO(String programmeAcronym, String programmeName, String schoolYearId) {
+        this.programmeAcronym = programmeAcronym;
+        this.programmeName = programmeName;
+        this.schoolYearId = schoolYearId;
+    }
+
+    public String getProgrammeAcronym() {
+        return programmeAcronym;
+    }
+
+    public String getProgrammeName() {
+        return programmeName;
+    }
+
+    public String getSchoolYearId() {
+        return schoolYearId;
+    }
 }
