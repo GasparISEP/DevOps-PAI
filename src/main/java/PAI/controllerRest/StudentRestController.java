8db9b1c521e3fa3dd58012d42b4f873713a69fd4
Dianaguedes19 <1241903@isep.ipp.pt>
2025-05-23 13:40:38 +0100

    Ref #511: Added new annotation to controller for http request

--- src/main/java/PAI/controllerRest/StudentRestController.java
+++ src/main/java/PAI/controllerRest/StudentRestController.java
@@ -11,11 +11,9 @@ import PAI.dto.student.StudentResponseDTO;
 import PAI.service.student.IStudentService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/students")
 public class StudentRestController {
