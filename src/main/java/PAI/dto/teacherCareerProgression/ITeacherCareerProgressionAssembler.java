ecc395dab2262cb2bd066a37d7751ba7bfce9636
RicardoMarques21 <1241923@isep.ipp.pt>
2025-05-23 14:20:32 +0100

    Updated TeacherCareerProgressionServiceImplV2 and tests. Related to #582 #583

--- src/main/java/PAI/dto/teacherCareerProgression/ITeacherCareerProgressionAssembler.java
+++ src/main/java/PAI/dto/teacherCareerProgression/ITeacherCareerProgressionAssembler.java
@@ -5,7 +5,7 @@ import PAI.domain.teacherCareerProgression.TeacherCareerProgression;
 
 public interface ITeacherCareerProgressionAssembler {
     TeacherCareerProgression toTeacherCareerProgression(TeacherCareerProgressionDTO teacherCareerProgressionDTO);
-    TeacherWorkingPercentageUpdateDTO toTeacherCareerProgressionDTO(TeacherCareerProgression teacherCareerProgression);
+    TeacherWorkingPercentageUpdateDTO toTeacherWorkingPercentageUpdateDTO(TeacherCareerProgression teacherCareerProgression);
     TeacherCareerProgressionID toTeacherCareerProgressionID(TeacherCareerProgressionDTO teacherCareerProgressionDTO);
     Date todate (TeacherCareerProgressionDTO teacherCareerProgressionDTO);
     TeacherCategoryID toTeacherCategoryID (TeacherCareerProgressionDTO teacherCareerProgressionDTO);
