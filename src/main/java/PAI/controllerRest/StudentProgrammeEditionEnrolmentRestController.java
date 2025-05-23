afeafc5f7d29a4b2e6cd4548e1501a9edd018d26
paulojorgeribeiro <1241920@isep.ipp.pt>
2025-05-23 03:13:31 +0100

    #500 #587 Update StudentProgrammeEditionEnrolmentRestController.java

--- src/main/java/PAI/controllerRest/StudentProgrammeEditionEnrolmentRestController.java
+++ src/main/java/PAI/controllerRest/StudentProgrammeEditionEnrolmentRestController.java
@@ -1,4 +1,35 @@
 package PAI.controllerRest;
 
+
+import PAI.VOs.StudentID;
+import PAI.dto.programmeEditionEnrolment.StudentProgrammeEditionEnrolmentDTO;
+import PAI.service.programmeEditionEnrolment.IStudentProgrammeEditionEnrolmentService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
 public class StudentProgrammeEditionEnrolmentRestController {
+
+    private final IStudentProgrammeEditionEnrolmentService applicationService;
+
+    public StudentProgrammeEditionEnrolmentRestController(IStudentProgrammeEditionEnrolmentService applicationService) {
+        this.applicationService = applicationService;
+    }
+
+    @GetMapping("/{studentId}/programme-editions")
+    public ResponseEntity<List<StudentProgrammeEditionEnrolmentDTO>> getProgrammeEditionsStudentCanEnrollIn(
+            @PathVariable String studentId) {
+
+        try {
+            StudentID sid = new StudentID(Integer.parseInt(studentId));
+            List<StudentProgrammeEditionEnrolmentDTO> result = applicationService.findAvailableProgrammeEditionsForStudent(sid);
+            return ResponseEntity.ok(result);
+
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
 }
