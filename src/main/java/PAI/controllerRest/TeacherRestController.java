9b7d4013f6bf94f8614513bbe8ceafd662080b2a
pedrofg890 <1210608@isep.ipp.pt>
2025-05-23 21:28:36 +0100

    feat: add Constructor to TeacherRestController and tests. Refs #581, #500

--- src/main/java/PAI/controllerRest/TeacherRestController.java
+++ src/main/java/PAI/controllerRest/TeacherRestController.java
@@ -1,10 +1,20 @@
 package PAI.controllerRest;
 
 
+import PAI.assembler.teacher.ITeacherAssembler;
+import PAI.service.teacher.ITeacherService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/teachers")
 public class TeacherRestController {
+
+    private final ITeacherService teacherService;
+    private final ITeacherAssembler teacherAssembler;
+
+    public TeacherRestController(ITeacherService teacherService, ITeacherAssembler teacherAssembler) {
+        this.teacherService = teacherService;
+        this.teacherAssembler = teacherAssembler;
+    }
 }
--- src/test/java/PAI/controllerRest/TeacherRestControllerTest.java
+++ src/test/java/PAI/controllerRest/TeacherRestControllerTest.java
@@ -1,4 +1,21 @@
+package PAI.controllerRest;
+
 import static org.junit.jupiter.api.Assertions.*;
+import PAI.assembler.teacher.ITeacherAssembler;
+import PAI.service.teacher.ITeacherService;
+import static org.mockito.Mockito.mock;
+import org.junit.jupiter.api.Test;
+
 class TeacherRestControllerTest {
-  
+
+    @Test
+    void shouldCreateTeacherRestController() {
+        // Arrange
+        ITeacherService teacherService = mock(ITeacherService.class);
+        ITeacherAssembler teacherAssembler = mock(ITeacherAssembler.class);
+        TeacherRestController controller = new TeacherRestController(teacherService, teacherAssembler);
+        // Act + Assert
+        assertNotNull(controller);
+    }
+
 }
