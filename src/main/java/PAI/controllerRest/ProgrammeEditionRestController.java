b0953047869dc31d9a52438079722764be5e102f
MelanieAGBarbosa <1241917@isep.ipp.pt>
2025-05-25 15:21:36 +0100

    Created constructor for Web Controller of ProgrammeEdition + test - refs #535 #553

--- src/main/java/PAI/controllerRest/ProgrammeEditionRestController.java
+++ src/main/java/PAI/controllerRest/ProgrammeEditionRestController.java
@@ -1,5 +1,6 @@
 package PAI.controllerRest;
 
+import PAI.service.programmeEdition.IProgrammeEditionService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/programmeEdition")
 public class ProgrammeEditionRestController {
 
+    private final IProgrammeEditionService programmeEditionService;
 
-
+    public ProgrammeEditionRestController(IProgrammeEditionService programmeEditionService) {
+       if (programmeEditionService == null) {
+           throw new IllegalArgumentException("ProgrammeEdition service cannot be null");
+       }
+        this.programmeEditionService = programmeEditionService;
     }
+
+}
