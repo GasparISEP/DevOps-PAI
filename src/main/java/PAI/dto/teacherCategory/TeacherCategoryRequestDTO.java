a9c40ef80f549036af175cc07cd1e31fd20be19c
anaconde1241897 <1241897@isep.ipp.pt>
2025-05-25 14:59:37 +0100

    refactor (dto): add some annotations to TeacherCategoryRequestDTO and refactor respective unit tests (#533, #591)

--- src/main/java/PAI/dto/teacherCategory/TeacherCategoryRequestDTO.java
+++ src/main/java/PAI/dto/teacherCategory/TeacherCategoryRequestDTO.java
@@ -1,6 +1,14 @@
 package PAI.dto.teacherCategory;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public record TeacherCategoryRequestDTO(
+
+        @NotNull(message = "The name field is required!")
+        @NotBlank(message = "The name field cannot be blank!")
+        @Size(min = 3, max = 255, message = "The name must be between 3 and 255 characters long!")
         String name
 ) {
 }
