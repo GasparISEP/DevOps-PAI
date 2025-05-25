24f383488029d66e240b05c9e72f735fd17d8c0e
joaopinheiro1241913 <1241913@isep.ipp.pt>
2025-05-25 13:26:10 +0100

    refactor(createCourseEditionController): Update controller to inject dedicated service only and adjust tests (Refs #537, #601)

--- src/main/java/PAI/controller/US19_CreateCourseEditionController.java
+++ src/main/java/PAI/controller/US19_CreateCourseEditionController.java
@@ -5,6 +5,7 @@ import PAI.domain.degreeType.DegreeType;
 import PAI.domain.courseInStudyPlan.CourseInStudyPlan;
 import PAI.domain.programme.Programme;
 import PAI.domain.programmeEdition.ProgrammeEdition;
+import PAI.service.courseEdition.ICreateCourseEditionService;
 import PAI.service.degreeType.IDegreeTypeService;
 import PAI.service.studyPlan.IStudyPlanService;
 import PAI.service.courseEdition.ICourseEditionService;
@@ -15,71 +16,39 @@ import PAI.service.programmeEdition.IProgrammeEditionService;
 import java.util.List;
 
 public class US19_CreateCourseEditionController {
-    private final IDegreeTypeService degreeTypeService;
-    private final IProgrammeService programmeService;
-    private final IStudyPlanService studyPlanService;
-    private final ICourseInStudyPlanService courseInStudyPlanService;
-    private final IProgrammeEditionService programmeEditionService;
-    private final ICourseEditionService courseEditionService;
+    private final ICreateCourseEditionService courseEditionService;
 
-    public US19_CreateCourseEditionController(IDegreeTypeService degreeTypeService,
-                                              IProgrammeService programmeService,
-                                              IStudyPlanService studyPlanService,
-                                              ICourseInStudyPlanService courseInStudyPlanService,
-                                              IProgrammeEditionService programmeEditionService,
-                                              ICourseEditionService courseEditionService){
-        if (degreeTypeService == null) {
-            throw new IllegalArgumentException("degreeTypeService cannot be null");
-        }
-        if (programmeService == null) {
-            throw new IllegalArgumentException("programmeService cannot be null");
-        }
-        if (studyPlanService == null) {
-            throw new IllegalArgumentException("studyPlanService cannot be null");
-        }
-        if (courseInStudyPlanService == null) {
-            throw new IllegalArgumentException("courseInStudyPlanService cannot be null");
-        }
-        if (programmeEditionService == null) {
-            throw new IllegalArgumentException("programmeEditionService cannot be null");
-        }
+    public US19_CreateCourseEditionController(ICreateCourseEditionService courseEditionService) {
         if (courseEditionService == null) {
             throw new IllegalArgumentException("courseEditionService cannot be null");
         }
-
-        this.degreeTypeService = degreeTypeService;
-        this.programmeService = programmeService;
-        this.studyPlanService = studyPlanService;
-        this.courseInStudyPlanService = courseInStudyPlanService;
-        this.programmeEditionService = programmeEditionService;
         this.courseEditionService = courseEditionService;
     }
 
     public List<DegreeType> getAllDegreeTypes() {
-        return degreeTypeService.getAllDegreeTypes();
+        return courseEditionService.getAllDegreeTypes();
     }
 
     public List<Programme> getProgrammesByDegreeTypeID(DegreeTypeID degreeTypeID) throws Exception {
         if (degreeTypeID == null)
             return List.of();
-        return programmeService.getProgrammesByDegreeTypeID(degreeTypeID);
+        return courseEditionService.getProgrammesByDegreeTypeID(degreeTypeID);
     }
 
     public List<CourseInStudyPlan> getCoursesInStudyPlanByProgrammeID (ProgrammeID programmeID) throws Exception {
         if (programmeID == null)
             return List.of();
-        StudyPlanID studyPlanID = studyPlanService.getLatestStudyPlanIDByProgrammeID(programmeID);
+        StudyPlanID studyPlanID = courseEditionService.getLatestStudyPlanIDByProgrammeID(programmeID);
         if (studyPlanID == null)
             return List.of();
-        return courseInStudyPlanService.getCoursesByStudyPlanId(studyPlanID);
+        return courseEditionService.getCoursesByStudyPlanId(studyPlanID);
     }
 
     public List<ProgrammeEdition> getProgrammeEditionsByProgrammeID(ProgrammeID programmeID) throws Exception{
         if (programmeID == null)
             return List.of();
-        return programmeEditionService.getProgrammeEditionsByProgrammeID(programmeID);
+        return courseEditionService.getProgrammeEditionsByProgrammeID(programmeID);
     }
-
 
     public boolean createCourseEdition (CourseInStudyPlanID courseInStudyPlanID, ProgrammeEditionID programmeEditionID) {
         if (courseInStudyPlanID == null)
@@ -90,5 +59,4 @@ public class US19_CreateCourseEditionController {
         CourseEdition result = courseEditionService.createAndSaveCourseEdition(courseInStudyPlanID, programmeEditionID);
         return result != null;
     }
-
 }
