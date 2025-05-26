0515c1fa8f38a5171e5244c0b76a44ee6151cd41
JoãoRuaCardoso <1241911@isep.ipp.pt>
2025-05-26 19:25:44 +0100

    Ref #583 : Added  method to convert TeacherCategory list to DTOs.

--- src/test/java/PAI/assembler/teacherCategory/TeacherCategoryAssemblerImplTest.java
+++ src/test/java/PAI/assembler/teacherCategory/TeacherCategoryAssemblerImplTest.java
@@ -2,12 +2,15 @@ package PAI.assembler.teacherCategory;
 
 import PAI.VOs.Name;
 import PAI.VOs.TeacherCategoryID;
+import PAI.VOs.TeacherID;
 import PAI.controller.US16_EnrolAStudentInACourseEditionController;
 import PAI.domain.teacherCategory.TeacherCategory;
 import PAI.dto.teacherCategory.TeacherCategoryRequestDTO;
 import PAI.dto.teacherCategory.TeacherCategoryResponseDTO;
+import org.h2.command.dml.MergeUsing;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -83,5 +86,32 @@ class TeacherCategoryAssemblerImplTest {
 
         // assert
         assertEquals("Teacher Category Request DTO cannot be null", exception.getMessage());
+    }
+
+    @Test
+    void shouldConvertListOfTeachersToDTOs(){
+        //arrange
+        TeacherCategoryAssemblerImpl assembler = new TeacherCategoryAssemblerImpl();
+
+        TeacherCategory teacherCategory = mock(TeacherCategory.class);
+        Name name = mock(Name.class);
+        when(name.getName()).thenReturn("Assistant Professor");
+        when(teacherCategory.getName()).thenReturn(name);
+        TeacherCategoryID categoryID = mock(TeacherCategoryID.class);
+        when(categoryID.getValue()).thenReturn(UUID.fromString("3f7bfe9a-d0e7-4b18-9b42-4b0a3f3e0c85"));
+        when(teacherCategory.getId()).thenReturn(categoryID);
+        Iterable<TeacherCategory> teacherCategories = List.of(teacherCategory);
+        //act
+        Iterable<TeacherCategoryResponseDTO> teacherCategoryDTOs = assembler.toDTOs(teacherCategories);
+        //assert
+        assertNotNull(teacherCategoryDTOs);
+        assertTrue(teacherCategoryDTOs.iterator().hasNext());
+    }
+    @Test
+    void shouldReturnAnEmptyListWhenTeacherCategoryIsNull(){
+        TeacherCategoryAssemblerImpl assembler = new TeacherCategoryAssemblerImpl();
+        //act
+        Iterable<TeacherCategoryResponseDTO> teacherCategoryResponseDTOS = assembler.toDTOs(null);
+        assertFalse(teacherCategoryResponseDTOS.iterator().hasNext());
     }
 }
