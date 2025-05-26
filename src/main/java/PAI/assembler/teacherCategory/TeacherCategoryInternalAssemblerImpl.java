a74619edf63a65133171b37ab54eaaf3909c6440
anaconde1241897 <1241897@isep.ipp.pt>
2025-05-27 00:23:31 +0100

    feat (assembler): Create TeacherCategoryInternalAssembler interface, its implementation and respective unit tests (#533, #591)

---
+++ src/main/java/PAI/assembler/teacherCategory/TeacherCategoryInternalAssemblerImpl.java
@@ -0,0 +1,21 @@
+package PAI.assembler.teacherCategory;
+
+import PAI.domain.teacherCategory.TeacherCategory;
+import PAI.dto.teacherCategory.TeacherCategoryDTO;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TeacherCategoryInternalAssemblerImpl implements ITeacherCategoryInternalAssembler {
+
+    public TeacherCategoryDTO toDTO(TeacherCategory teacherCategory) {
+
+        if (teacherCategory == null) {
+            throw new IllegalArgumentException("Teacher Category cannot be null");
+        }
+
+        String id = teacherCategory.getId().getValue().toString();
+        String name = teacherCategory.getName().getName();
+
+        return new TeacherCategoryDTO(id, name);
+    }
+}
