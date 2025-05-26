fb372e47668b790f6be2a9fc6ac31c4f618159d0
Gonçalo Silva <1241908@isep.ipp.pt>
2025-05-26 19:06:07 +0100

    feat: Add getProgrammeEditionEnrollmentsByStudentID endpoint and corresponding tests in StudentProgrammeEditionEnrolmentRestController. Includes null checks for constructor dependencies. Refs #589 #500

--- src/test/java/PAI/controllerRest/StudentProgrammeEditionEnrolmentRestControllerTest.java
+++ src/test/java/PAI/controllerRest/StudentProgrammeEditionEnrolmentRestControllerTest.java
@@ -1,29 +1,50 @@
 package PAI.controllerRest;
 
 import PAI.VOs.*;
+import PAI.assembler.programmeEditionEnrolment.IProgrammeEditionEnrolmentAssembler;
+import PAI.dto.programmeEditionEnrolment.ProgrammeEditionEnrolmentDetailDto;
 import PAI.dto.programmeEditionEnrolment.ProgrammeEditionEnrolmentRequest;
 import PAI.dto.programmeEditionEnrolment.StudentProgrammeEditionEnrolmentDTO;
+import PAI.service.programmeEditionEnrolment.IProgrammeEditionEnrolmentService;
 import PAI.service.programmeEditionEnrolment.IStudentProgrammeEditionEnrolmentService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.List;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
+@WebMvcTest(StudentProgrammeEditionEnrolmentRestController.class)
 class StudentProgrammeEditionEnrolmentRestControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private IProgrammeEditionEnrolmentService programmeEditionEnrolmentService;
+
+    @MockitoBean
+    private IProgrammeEditionEnrolmentAssembler programmeEditionEnrolmentAssembler;
+
+    @MockitoBean   
     private IStudentProgrammeEditionEnrolmentService service;
+
     private StudentProgrammeEditionEnrolmentRestController controller;
 
     @BeforeEach
     void setUp() {
-        service = mock(IStudentProgrammeEditionEnrolmentService.class);
-        controller = new StudentProgrammeEditionEnrolmentRestController(service);
+        controller = new StudentProgrammeEditionEnrolmentRestController(service, programmeEditionEnrolmentAssembler, programmeEditionEnrolmentService);
     }
+
 
     @Test
     void getProgrammeEditionsStudentCanEnrollIn_validStudentId_returns200OK() throws Exception {
@@ -89,5 +110,58 @@ class StudentProgrammeEditionEnrolmentRestControllerTest {
         );
     }
 
+    @Test
+    void shouldReturn200OKAndListOfProgrammeEditionEnrolments_whenGetProgrammeEditionEnrollmentsByStudentID_validStudentId() throws Exception {
+        // Arrange
+        ProgrammeEditionID programmeEditionID = mock(ProgrammeEditionID.class);
+        ProgrammeEditionEnrolmentDetailDto programmeEditionEnrolmentDetailDto = new ProgrammeEditionEnrolmentDetailDto(
+            1000001,
+            "Engenharia Informática",
+            "LEI",
+            "2024-2025",
+            "some-uuid-string"
+        );
+        List<ProgrammeEditionID> listOfEditionIDs = List.of(programmeEditionID);
+        List<ProgrammeEditionEnrolmentDetailDto> listOfDetailDtos = List.of(programmeEditionEnrolmentDetailDto);
 
+        when(programmeEditionEnrolmentService.getProgrammeEditionEnrolmentsByStudentID(any(StudentID.class)))
+            .thenReturn(listOfEditionIDs);
+        when(programmeEditionEnrolmentAssembler.toDtoList(anyList(), any(StudentID.class)))
+            .thenReturn(listOfDetailDtos);
+
+        String expectedJson = "[{\"studentID\":1000001,\"programmeName\":\"Engenharia Informática\",\"programmeAcronym\":\"LEI\",\"schoolYearDescription\":\"2024-2025\",\"schoolYearID\":\"some-uuid-string\"}]";
+
+        // Act && Assert
+        mockMvc.perform(get("/students/programme-edition-enrolments/students")
+            .param("studentId", "1000001"))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.content().string(expectedJson));
+    }
+
+    @Test
+    void shouldReturn400BadRequest_whenGetProgrammeEditionEnrollmentsByStudentID_invalidStudentId() throws Exception {
+        // Arrange
+        // Act && Assert
+        mockMvc.perform(get("/students/programme-edition-enrolments/students")
+            .param("studentId", "15000011"))
+            .andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+
+    @Test
+    void shouldReturn200OKAndEmptyList_whenGetProgrammeEditionEnrollmentsByStudentID_studentNotEnrolledInAnyProgrammeEdition() throws Exception {
+    // Arrange
+    when(programmeEditionEnrolmentService.getProgrammeEditionEnrolmentsByStudentID(any(StudentID.class)))
+        .thenReturn(List.of());
+    when(programmeEditionEnrolmentAssembler.toDtoList(anyList(), any(StudentID.class)))
+        .thenReturn(List.of());
+
+    String expectedJson = "[]";
+
+
+    // Act && Assert
+    mockMvc.perform(get("/students/programme-edition-enrolments/students")
+        .param("studentId", "1000001"))
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.content().string(expectedJson));
+    }
 }
