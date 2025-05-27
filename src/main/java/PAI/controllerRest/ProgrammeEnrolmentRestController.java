f19101922db7987146b576f4bff9d0a172b0f36e
jmariz-costa <1241912@isep.ipp.pt>
2025-05-27 15:16:24 +0100

    refactor(rest): inject ITotalEnrolledStudentsInProgrammesByDepartmentAndSchoolYearService into ProgrammeEnrolmentRestController. Refs #572

--- src/main/java/PAI/controllerRest/ProgrammeEnrolmentRestController.java
+++ src/main/java/PAI/controllerRest/ProgrammeEnrolmentRestController.java
@@ -10,8 +10,10 @@ import PAI.domain.programmeEnrolment.ProgrammeEnrolment;
 import PAI.dto.programmeEnrolment.IProgrammeEnrolmentAssembler;
 import PAI.dto.programmeEnrolment.ProgrammeEnrolmentDTO;
 import PAI.dto.programmeEnrolment.ProgrammeEnrolmentResponseDTO;
+import PAI.dto.totalEnrolledStudents.TotalEnrolledStudentsCommand;
 import PAI.dto.totalEnrolledStudents.TotalEnrolledStudentsRequest;
 import PAI.service.programmeEnrolment.IProgrammeEnrolmentService;
+import PAI.service.totalEnrolledStudentsInProgrammesByDepartmentAndSchoolYear.ITotalEnrolledStudentsInProgrammesByDepartmentAndSchoolYearService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,13 +25,19 @@ public class ProgrammeEnrolmentRestController {
     private final IProgrammeEnrolmentService programmeEnrolmentService;
     private final IProgrammeEnrolmentAssembler programmeEnrolmentMapper;
     private final ITotalEnrolledStudentsAssembler totalEnrolledStudentsAssembler;
+    private final ITotalEnrolledStudentsInProgrammesByDepartmentAndSchoolYearService totalEnrolledStudentsService;
 
 
 
-    public ProgrammeEnrolmentRestController(IProgrammeEnrolmentService programmeEnrolmentService, IProgrammeEnrolmentAssembler programmeEnrolmentMapper, ITotalEnrolledStudentsAssembler totalEnrolledStudentsAssembler) {
+    public ProgrammeEnrolmentRestController(
+            IProgrammeEnrolmentService programmeEnrolmentService,
+            IProgrammeEnrolmentAssembler programmeEnrolmentMapper,
+            ITotalEnrolledStudentsAssembler totalEnrolledStudentsAssembler,
+            ITotalEnrolledStudentsInProgrammesByDepartmentAndSchoolYearService totalEnrolledStudentsService) {
         this.programmeEnrolmentService = programmeEnrolmentService;
         this.programmeEnrolmentMapper = programmeEnrolmentMapper;
         this.totalEnrolledStudentsAssembler = totalEnrolledStudentsAssembler;
+        this.totalEnrolledStudentsService = totalEnrolledStudentsService;
     }
 
     @PostMapping()
