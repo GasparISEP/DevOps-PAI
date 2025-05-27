10cc0e1ad77f21fa7ae1f20bcf45798b08d7ff36
joaopinheiro1241913 <1241913@isep.ipp.pt>
2025-05-27 23:34:04 +0100

    [feat] Add getProgrammeEditionIDsByProgrammeID to PE Service, toDTOFromIDs to PE ControllerAssembler, and refactor getProgrammeEditionsByProgrammeID endpoint to use only DTOs instead of domain (for Course Edition creation support) (Refs: #537, #599)

--- src/main/java/PAI/assembler/programmeEdition/IProgrammeEditionControllerAssembler.java
+++ src/main/java/PAI/assembler/programmeEdition/IProgrammeEditionControllerAssembler.java
@@ -1,5 +1,7 @@
 package PAI.assembler.programmeEdition;
 
+import PAI.VOs.ProgrammeID;
+import PAI.VOs.SchoolYearID;
 import PAI.domain.programmeEdition.ProgrammeEdition;
 import PAI.dto.programmeEdition.ProgrammeEditionDTO;
 import PAI.dto.programmeEdition.ProgrammeEditionRequestDTO;
@@ -8,4 +10,6 @@ import PAI.dto.programmeEdition.ProgrammeEditionResponseDTO;
 public interface IProgrammeEditionControllerAssembler {
     ProgrammeEditionDTO toDTO(ProgrammeEditionRequestDTO programmeEditionRequest);
     ProgrammeEditionResponseDTO toResponseDTO(ProgrammeEditionDTO programmeEdition);
+
+    ProgrammeEditionDTO toDTOFromIDs(ProgrammeID programmeID, SchoolYearID schoolYearID);
 }
--- src/main/java/PAI/assembler/programmeEdition/ProgrammeEditionControllerAssembler.java
+++ src/main/java/PAI/assembler/programmeEdition/ProgrammeEditionControllerAssembler.java
@@ -1,8 +1,12 @@
 package PAI.assembler.programmeEdition;
 
+import PAI.VOs.ProgrammeID;
+import PAI.VOs.SchoolYearID;
+import PAI.dto.Programme.ProgrammeIDDTO;
 import PAI.dto.programmeEdition.ProgrammeEditionDTO;
 import PAI.dto.programmeEdition.ProgrammeEditionRequestDTO;
 import PAI.dto.programmeEdition.ProgrammeEditionResponseDTO;
+import PAI.dto.schoolYear.SchoolYearIDRequestDTO;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -22,5 +26,18 @@ public class ProgrammeEditionControllerAssembler implements IProgrammeEditionCon
             throw new IllegalArgumentException("ProgrammeEditionDTO cannot be null");
         }
       return new ProgrammeEditionResponseDTO(programmeEdition.programme(),programmeEdition.schoolYear());
+    }
+
+    @Override
+    public ProgrammeEditionDTO toDTOFromIDs(ProgrammeID programmeID, SchoolYearID schoolYearID) {
+        if (programmeID == null || schoolYearID == null) {
+            throw new IllegalArgumentException("programmeID and or schoolYearID cannot be null");
+        }
+        String programmeName = programmeID.getProgrammeName();
+        String programmeAcronym = programmeID.getProgrammeAcronym();
+        String schoolYearId = schoolYearID.getSchoolYearID().toString();
+        ProgrammeIDDTO programmeIDDTO = new ProgrammeIDDTO(programmeName, programmeAcronym);
+        SchoolYearIDRequestDTO schoolYearIDRequestDTO = new SchoolYearIDRequestDTO(schoolYearId);
+        return new ProgrammeEditionDTO(programmeIDDTO, schoolYearIDRequestDTO);
     }
 }
