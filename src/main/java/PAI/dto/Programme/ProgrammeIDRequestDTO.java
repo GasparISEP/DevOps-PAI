9528c5b4a449c743c952f243318bf7ad0286e108
MelanieAGBarbosa <1241917@isep.ipp.pt>
2025-05-27 23:54:06 +0100

    Created ProgrammeIDRequestDTO for functionality CreateProgrammeEditionForCurrentSchoolYear + tests - refs #535 #606

---
+++ src/main/java/PAI/dto/Programme/ProgrammeIDRequestDTO.java
@@ -0,0 +1,14 @@
+package PAI.dto.Programme;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record ProgrammeIDRequestDTO(
+        @NotBlank(message = "Programme Name is required")
+        String name,
+
+        @NotNull(message = "Programme Acronym is required")
+        @Size(min = 3, max = 3, message = "Acronym must have 3 characters")
+        String acronym) {
+}
