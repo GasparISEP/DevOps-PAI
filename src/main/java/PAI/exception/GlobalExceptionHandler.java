03e68cc465efef1bfc04e8117d6e967c25446a2b
pedrosoares7 <soarespedro7@hotmail.com>
2025-05-27 23:35:14 +0100

    refactor(exception): Add handler for missing request parameters in GlobalExceptionHandler and corresponding tests (#618)

--- src/main/java/PAI/exception/GlobalExceptionHandler.java
+++ src/main/java/PAI/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package PAI.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -68,4 +69,17 @@ public class GlobalExceptionHandler {
         );
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
+
+    @ExceptionHandler(org.springframework.web.bind.MissingServletRequestParameterException.class)
+    public ResponseEntity<ErrorResponse> handleMissingParameter(org.springframework.web.bind.MissingServletRequestParameterException ex) {
+        String message = String.format("The required parameter '%s' is missing.", ex.getParameterName());
+
+        ErrorResponse error = new ErrorResponse(
+                "MISSING_PARAMETER",
+                message
+        );
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
 }
