ede0bcef0de1e969f11998ceed50a043abb8913b
RicardoMarques21 <1241923@isep.ipp.pt>
2025-05-27 11:41:20 +0100

    Refactor of UpdateWorkingPercentageDTOs, Assembler and tests. Related to #582

---
+++ src/main/java/PAI/dto/teacherCareerProgression/UpdateTeacherWorkingPercentageRequestDTO.java
@@ -0,0 +1,20 @@
+package PAI.dto.teacherCareerProgression;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
+public record UpdateTeacherWorkingPercentageRequestDTO(
+
+    @NotBlank(message = "Date is required.")
+    String date,
+    @NotBlank(message = "Teacher ID required.")
+    String teacherID,
+    @Min(value = 0, message = "Working percentage must be at least 0")
+    @Max(value = 100, message = "Working percentage must be at most 100")
+    int workingPercentage
+
+) {
+}
+
+
