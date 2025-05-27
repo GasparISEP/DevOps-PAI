fd65d3f19ceb32b181b131d9a09e6922fcab557b
jmariz-costa <1241912@isep.ipp.pt>
2025-05-27 11:49:33 +0100

    feat(application): implement conversion from TotalEnrolledStudentsRequest to TotalEnrolledStudentsCommand with validation and value object creation. Refs #572

--- src/main/java/PAI/assembler/totalEnrolledStudentsInProgrammesByDepartmentAndSchoolYear/TotalEnrolledStudentsAssemblerImpl.java
+++ src/main/java/PAI/assembler/totalEnrolledStudentsInProgrammesByDepartmentAndSchoolYear/TotalEnrolledStudentsAssemblerImpl.java
@@ -1,14 +1,31 @@
 package PAI.assembler.totalEnrolledStudentsInProgrammesByDepartmentAndSchoolYear;
 
+import PAI.VOs.DepartmentAcronym;
+import PAI.VOs.DepartmentID;
+import PAI.VOs.SchoolYearID;
 import PAI.dto.totalEnrolledStudents.TotalEnrolledStudentsCommand;
 import PAI.dto.totalEnrolledStudents.TotalEnrolledStudentsRequest;
 import org.springframework.stereotype.Component;
+
+import java.util.UUID;
 
 @Component
 public class TotalEnrolledStudentsAssemblerImpl implements ITotalEnrolledStudentsAssembler{
 
     @Override
     public TotalEnrolledStudentsCommand fromRequestToCommand(TotalEnrolledStudentsRequest request) {
-        return null;
+        if (request == null)
+            throw new IllegalArgumentException("TotalEnrolledStudentsRequest cannot be null");
+
+        String depID = request.departmentID();
+        String syID = request.schoolYearID();
+
+        DepartmentAcronym departmentAcronym = new DepartmentAcronym(depID);
+        DepartmentID departmentID = new DepartmentID(departmentAcronym);
+
+        UUID uuidSchoolYearID = UUID.fromString(syID);
+        SchoolYearID schoolYearID = new SchoolYearID(uuidSchoolYearID);
+
+        return new TotalEnrolledStudentsCommand(departmentID, schoolYearID);
     }
 }
--- src/test/java/PAI/assembler/totalEnrolledStudentsInProgrammesByDepartmentAndSchoolYear/TotalEnrolledStudentsAssemblerImplTest.java
+++ src/test/java/PAI/assembler/totalEnrolledStudentsInProgrammesByDepartmentAndSchoolYear/TotalEnrolledStudentsAssemblerImplTest.java
@@ -1,25 +1,93 @@
 package PAI.assembler.totalEnrolledStudentsInProgrammesByDepartmentAndSchoolYear;
 
+
 import PAI.dto.totalEnrolledStudents.TotalEnrolledStudentsCommand;
 import PAI.dto.totalEnrolledStudents.TotalEnrolledStudentsRequest;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 class TotalEnrolledStudentsAssemblerImplTest {
 
     @Test
-    void shouldReturnNullWhenConvertFromRequestToCommand() {
+    void shouldReturnAValidCommandWhenConvertFromRequest() {
         // Arrange
         ITotalEnrolledStudentsAssembler totalEnrolledStudentsAssembler = new TotalEnrolledStudentsAssemblerImpl();
         TotalEnrolledStudentsRequest request = mock(TotalEnrolledStudentsRequest.class);
+
+        String depID = "DEI";
+        String syID = "b8f8e7c6-3a2d-4d99-9f84-32f4a1e1a7ab";
+
+        when(request.departmentID()).thenReturn(depID);
+        when(request.schoolYearID()).thenReturn(syID);
 
         // Act
         TotalEnrolledStudentsCommand command = totalEnrolledStudentsAssembler.fromRequestToCommand(request);
 
         // Assert
-        assertNull(command);
+        assertNotNull(command);
+        assertEquals(depID, command.departmentID().getAcronym().getAcronym());
+        assertEquals(syID, command.schoolYearID().getSchoolYearID().toString());
+    }
+
+    @Test
+    void shouldThrowExceptionIfRequestReceivedIsNull() {
+        // Arrange
+        ITotalEnrolledStudentsAssembler totalEnrolledStudentsAssembler = new TotalEnrolledStudentsAssemblerImpl();
+        TotalEnrolledStudentsRequest request = null;
+
+        // Act
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> totalEnrolledStudentsAssembler.fromRequestToCommand(request));
+
+        // Assert
+        assertNotNull(exception);
+        assertEquals("TotalEnrolledStudentsRequest cannot be null", exception.getMessage());
+    }
+
+    @Test
+    void shouldThrowExceptionIfCommandContainsAnInvalidSchoolYearID() {
+        // Arrange
+        ITotalEnrolledStudentsAssembler totalEnrolledStudentsAssembler = new TotalEnrolledStudentsAssemblerImpl();
+        TotalEnrolledStudentsRequest request = mock(TotalEnrolledStudentsRequest.class);
+
+        String depID = "DEI";
+        String syID = "b8f8e7c6";
+
+        when(request.departmentID()).thenReturn(depID);
+        when(request.schoolYearID()).thenReturn(syID);
+
+        // Act
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> totalEnrolledStudentsAssembler.fromRequestToCommand(request));
+
+        // Assert
+        assertNotNull(exception);
+        assertTrue(exception.getMessage().contains("Invalid UUID string"));
+        verify(request, times(1)).departmentID();
+        verify(request, times(1)).schoolYearID();
+        verifyNoMoreInteractions(request);
+    }
+
+    @Test
+    void shouldThrowExceptionIfCommandContainsAnInvalidDepartmentID() {
+        // Arrange
+        ITotalEnrolledStudentsAssembler totalEnrolledStudentsAssembler = new TotalEnrolledStudentsAssemblerImpl();
+        TotalEnrolledStudentsRequest request = mock(TotalEnrolledStudentsRequest.class);
+
+        String depID = "DEItar";
+        String syID = "b8f8e7c6-3a2d-4d99-9f84-32f4a1e1a7ab";
+
+        when(request.departmentID()).thenReturn(depID);
+        when(request.schoolYearID()).thenReturn(syID);
+
+        // Act
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> totalEnrolledStudentsAssembler.fromRequestToCommand(request));
+
+        // Assert
+        assertNotNull(exception);
+        assertEquals("Acronym must contain only capital letters.", exception.getMessage());
+        verify(request, times(1)).departmentID();
+        verify(request, times(1)).schoolYearID();
+        verifyNoMoreInteractions(request);
     }
 }
