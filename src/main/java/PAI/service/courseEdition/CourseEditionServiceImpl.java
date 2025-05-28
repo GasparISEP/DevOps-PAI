47645dc0359c7888a47024383fb7d204eb092013
Gonçalo Silva <1241908@isep.ipp.pt>
2025-05-28 13:28:26 +0100

    feat: add method to find course editions by ProgrammeEditionID and CourseInStudyPlanID with validation and implement corresponding repository and service updates. Add unit tests to ensure functionality. refs #589 #500

--- src/main/java/PAI/service/courseEdition/CourseEditionServiceImpl.java
+++ src/main/java/PAI/service/courseEdition/CourseEditionServiceImpl.java
@@ -12,6 +12,9 @@ import jakarta.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class CourseEditionServiceImpl implements ICourseEditionService {
 
     private final ICourseEditionRepository courseEditionRepository;
