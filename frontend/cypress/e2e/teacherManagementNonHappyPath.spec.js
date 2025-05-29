02dc392e6c6ae9ff175194e1b053b632f6a57122
Ruben Rocha <1241924@isep.ipp.pt>
2025-05-29 18:44:22 +0100

    cypress tests update. Refs#501

--- frontend/cypress/e2e/teacherManagementNonHappyPath.spec.js
+++ frontend/cypress/e2e/teacherManagementNonHappyPath.spec.js
@@ -13,41 +13,6 @@ describe('Teachers Management - Non-Happy Paths', () => {
         cy.url().should('include', '/teachers/register');
     });
 
-    it('exibe erro ao tentar registar novamente o mesmo teacher', () => {
-        const teacher = {
-            name: 'Cypress Switch Dev',
-            acronym: 'CSD',
-            email: 'cypressswitchdev@isep.ipp.pt',
-            nif: '124192426',
-            academicBackground: 'Licenciatura Engenharia Informática',
-            street: 'Rua Dr. António Bernardino de Almeida',
-            postalCode: '4249-015',
-            location: 'Porto'
-        };
-        Object.entries(teacher).forEach(([field, value]) => {
-            cy.get(`input[name="${field}"]`)
-                .clear()
-                .type(value).wait(500);
-        });
-        // Seleciona país
-        cy.get('.teacher-form-select__control').click().wait(500);
-        cy.get('.teacher-form-select__menu')
-            .contains('Portugal')
-            .click().wait(1000);
-        // Telefone e departamento
-        cy.get('input.teacher-phone-number').clear().type('912345678', { delay: 50 }).wait(500);
-        cy.get('select[name="departmentID"]').select(1).wait(500);
-
-        // Submete e espera popup de erro
-        cy.contains('button', 'REGISTER').highlightOutline().wait(1000).click();
-        cy.get('.modal-overlay').should('be.visible');
-        cy.contains('Registration Error').should('be.visible');
-        // Fecha o modal
-        cy.contains('button', 'Close').as('closeBtn')
-        cy.get('@closeBtn').highlightOutline().wait(2000).click();
-        cy.get('.modal-overlay').should('not.exist');
-    });
-
     it('exibe erro ao tentar registar um teacher com um Acronym já existente', () => {
         const teacher = {
             name: 'Cypress Switch Dev Two',
