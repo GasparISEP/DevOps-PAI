c9de9757a5a0c33da0ecadfaf3a926884ad33d7d
anaconde1241897 <1241897@isep.ipp.pt>
2025-05-30 00:20:38 +0100

    refactor (factories): Improve some tests and remove some unnecessary (#533, #611)

--- src/test/java/PAI/persistence/mem/teacherCategory/TeacherCategoryListFactoryImplTest.java
+++ src/test/java/PAI/persistence/mem/teacherCategory/TeacherCategoryListFactoryImplTest.java
@@ -1,21 +1,24 @@
 package PAI.persistence.mem.teacherCategory;
 
 import PAI.domain.teacherCategory.TeacherCategory;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TeacherCategoryListFactoryImplTest {
 
     @Test
     void shouldReturnEmptyList() {
+        //Arrange
         TeacherCategoryListFactoryImpl factory = new TeacherCategoryListFactoryImpl();
+
+        //Act
         List<TeacherCategory> list = factory.getTeacherCategoryList();
 
-        assertNotNull(list);
-        Assertions.assertTrue(list.isEmpty());
+        //Assert
+        assertEquals(ArrayList.class, list.getClass());
     }
 }
