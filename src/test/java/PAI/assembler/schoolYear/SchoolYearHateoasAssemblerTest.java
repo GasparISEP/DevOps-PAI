a45b0654b30dc3818f41c5523c85b77bfe662d62
Nuno Cruz <1241919@isep.ipp.pt>
2025-06-05 14:47:47 +0100

    Created SchoolYearHateoasAssembler, added the method toModel and tested it. ref #512

---
+++ src/test/java/PAI/assembler/schoolYear/SchoolYearHateoasAssemblerTest.java
@@ -0,0 +1,38 @@
+package PAI.assembler.schoolYear;
+ import PAI.dto.schoolYear.CurrentSchoolYearDTO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.hateoas.EntityModel;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SchoolYearHateoasAssemblerTest {
+
+    private SchoolYearHateoasAssembler assembler;
+
+    @BeforeEach
+    void setUp() {
+        assembler = new SchoolYearHateoasAssembler();
+    }
+
+    @Test
+    void shouldCreateEntityModelWithExpectedLinks() {
+        CurrentSchoolYearDTO dto = new CurrentSchoolYearDTO("HI", "Department of Informatics", LocalDate.of(2024,12,2), LocalDate.of(2025,12,6));
+
+        EntityModel<CurrentSchoolYearDTO> model = assembler.toModel(dto);
+
+        assertEquals(dto, model.getContent());
+
+        assertTrue(model.getLink("self").isPresent());
+        assertTrue(model.getLink("all").isPresent());
+
+        String selfHref = model.getLink("self").get().getHref();
+        String allHref = model.getLink("all").get().getHref();
+
+        assertTrue(selfHref.contains("/schoolyears/HI"));
+        assertTrue(allHref.contains("/schoolyears"));
+    }
+
+}
