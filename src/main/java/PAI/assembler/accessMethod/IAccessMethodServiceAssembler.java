5bae5e09d53a6343fedfdbfc94d70f10c9a87e71
pedrosoares1241921 <1241921@isep.ipp.pt>
2025-06-06 13:07:22 +0100

    feat(AccessMethod): add toDTOList method for converting list of AccessMethods to DTOs and implement corresponding tests (#670)

--- src/main/java/PAI/assembler/accessMethod/IAccessMethodServiceAssembler.java
+++ src/main/java/PAI/assembler/accessMethod/IAccessMethodServiceAssembler.java
@@ -3,9 +3,11 @@ package PAI.assembler.accessMethod;
 import PAI.domain.accessMethod.AccessMethod;
 import PAI.dto.accessMethod.AccessMethodServiceDTO;
 
+import java.util.List;
+
 public interface IAccessMethodServiceAssembler {
 
     AccessMethodServiceDTO toDTO(AccessMethod accessMethod);
 
-
+    List<AccessMethodServiceDTO> toDTOList(List<AccessMethod> accessMethods);
 }
