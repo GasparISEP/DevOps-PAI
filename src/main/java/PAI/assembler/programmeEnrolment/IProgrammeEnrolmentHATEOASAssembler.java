50a7b8b70f62704aafc0d3d5b71e5058a885a96f
Dianaguedes19 <1241903@isep.ipp.pt>
2025-06-07 13:32:51 +0100

    Ref #698: Creation of interface for programme enrolment hateoas

--- src/main/java/PAI/assembler/programmeEnrolment/IProgrammeEnrolmentHATEOASAssembler.java
+++ src/main/java/PAI/assembler/programmeEnrolment/IProgrammeEnrolmentHATEOASAssembler.java
@@ -1,4 +1,8 @@
 package PAI.assembler.programmeEnrolment;
 
+import PAI.dto.programmeEnrolment.ProgrammeEnrolmentResponseDTO;
+import org.springframework.hateoas.EntityModel;
+
 public interface IProgrammeEnrolmentHATEOASAssembler {
+    EntityModel<ProgrammeEnrolmentResponseDTO> toModel(ProgrammeEnrolmentResponseDTO dto);
 }
