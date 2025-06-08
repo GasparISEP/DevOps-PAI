0ef27ac75da7d93caa1da6d7566ee8fcfe2339ab
Gonçalo Silva <1241908@isep.ipp.pt>
2025-06-08 11:21:58 +0100

    refactor: simplify ICourseEditionEnrolmentHateoasAssembler interface by removing RepresentationModelAssembler inheritance and update enrolStudentInCourseEdition. refs #659 #684

--- src/main/java/PAI/assembler/courseEditionEnrolment/ICourseEditionEnrolmentHateoasAssembler.java
+++ src/main/java/PAI/assembler/courseEditionEnrolment/ICourseEditionEnrolmentHateoasAssembler.java
@@ -2,11 +2,10 @@ package PAI.assembler.courseEditionEnrolment;
 
 import org.springframework.lang.NonNull;
 import org.springframework.hateoas.EntityModel;
-import org.springframework.hateoas.server.RepresentationModelAssembler;
 
 import PAI.dto.courseEditionEnrolment.CourseEditionEnrolmentDto;
 
-public interface ICourseEditionEnrolmentHateoasAssembler extends RepresentationModelAssembler<CourseEditionEnrolmentDto, EntityModel<CourseEditionEnrolmentDto>> {
+public interface ICourseEditionEnrolmentHateoasAssembler {
 
     @NonNull
     EntityModel<CourseEditionEnrolmentDto> toModel(@NonNull CourseEditionEnrolmentDto dto);
