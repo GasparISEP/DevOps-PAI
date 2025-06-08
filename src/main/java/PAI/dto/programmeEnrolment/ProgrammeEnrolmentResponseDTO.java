d528e78a8582d2a52533bf0893d3858fe6a90eed
paulojorgeribeiro <1241920@isep.ipp.pt>
2025-06-09 00:19:46 +0100

    Implement ProgrammeEnrolmentGeneratedID - update response DTO (Issue #688)

--- src/main/java/PAI/dto/programmeEnrolment/ProgrammeEnrolmentResponseDTO.java
+++ src/main/java/PAI/dto/programmeEnrolment/ProgrammeEnrolmentResponseDTO.java
@@ -1,19 +1,26 @@
 package PAI.dto.programmeEnrolment;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
 public class ProgrammeEnrolmentResponseDTO {
 
+    private UUID programmeEnrolmentGID;
     private int studentID;
     private String accessMethodID;
     private String programmeID;
     private LocalDate date;
 
-    public ProgrammeEnrolmentResponseDTO(int studentID, String accessMethodID, String programmeID, LocalDate date) {
+    public ProgrammeEnrolmentResponseDTO(UUID programmeEnrolmentGID, int studentID, String accessMethodID, String programmeID, LocalDate date) {
+        this.programmeEnrolmentGID = programmeEnrolmentGID;
         this.studentID = studentID;
         this.accessMethodID = accessMethodID;
         this.programmeID = programmeID;
         this.date = date;
+    }
+
+    public UUID getProgrammeEnrolmentGID() {
+        return programmeEnrolmentGID;
     }
 
     public int getStudentID() {
