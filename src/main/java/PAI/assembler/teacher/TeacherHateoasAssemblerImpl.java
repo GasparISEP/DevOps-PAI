e06bcba3d91cbe34e0f19775e06aeefa7cd395c1
Ruben Rocha <1241924@isep.ipp.pt>
2025-06-09 11:36:49 +0100

    feat: toModel method TeacherHateoasAssemblerImpl + tests. Refs #682

--- src/main/java/PAI/assembler/teacher/TeacherHateoasAssemblerImpl.java
+++ src/main/java/PAI/assembler/teacher/TeacherHateoasAssemblerImpl.java
@@ -1,14 +1,22 @@
 package PAI.assembler.teacher;
 
+import PAI.controllerRest.DepartmentRestController;
+import PAI.controllerRest.TeacherRestController;
 import PAI.dto.teacher.TeacherDTO;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.stereotype.Component;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @Component
 public class TeacherHateoasAssemblerImpl implements RepresentationModelAssembler<TeacherDTO, EntityModel<TeacherDTO>>, ITeacherHateoasAssembler {
     @Override
     public EntityModel<TeacherDTO> toModel (TeacherDTO dto) {
-        return null;
+        return EntityModel.of(dto,
+                linkTo(methodOn(TeacherRestController.class)
+                        .getAllTeachers())
+                        .withRel("teachers"));
     }
 }
---
+++ src/test/java/PAI/assembler/teacher/TeacherHateoasAssemblerImplTest.java
@@ -0,0 +1,24 @@
+package PAI.assembler.teacher;
+
+import PAI.dto.teacher.TeacherDTO;
+import org.junit.jupiter.api.Test;
+import org.springframework.hateoas.EntityModel;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+
+class TeacherHateoasAssemblerImplTest {
+
+    @Test
+    void shouldCreateEntityModelWithTeacherDto(){
+        //arrange
+        TeacherDTO teacherDTO = mock(TeacherDTO.class);
+        TeacherHateoasAssemblerImpl hateoasAssembler = new TeacherHateoasAssemblerImpl();
+        //act
+        EntityModel<TeacherDTO> result = hateoasAssembler.toModel(teacherDTO);
+        //assert
+        assertEquals(teacherDTO, result.getContent());
+        assertTrue(result.hasLink("teachers"));
+    }
+
+}
