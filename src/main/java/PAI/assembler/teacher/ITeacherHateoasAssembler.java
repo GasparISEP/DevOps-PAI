426fe6471192d48c52c98dac99b7b11a86c8a911
Ruben Rocha <1241924@isep.ipp.pt>
2025-06-09 11:11:24 +0100

    Created ITeacherHateoasAssembler. Refs #682

---
+++ src/main/java/PAI/assembler/teacher/ITeacherHateoasAssembler.java
@@ -0,0 +1,9 @@
+package PAI.assembler.teacher;
+
+import PAI.dto.teacher.TeacherDTO;
+import org.springframework.hateoas.EntityModel;
+
+public interface ITeacherHateoasAssembler {
+
+    EntityModel<TeacherDTO> toModel (TeacherDTO dto);
+}
