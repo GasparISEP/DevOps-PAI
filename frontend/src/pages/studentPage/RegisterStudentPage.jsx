4d86ae444c6c393b2aff16951262d098a2a0f353
gasparfigueiras <1241907@isep.ipp.pt>
2025-06-09 18:53:01 +0100

    updated StudentForm and created StudentDisplay for frontend (#511)

---
+++ frontend/src/pages/studentPage/RegisterStudentPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import StudentForm from '../../components/studentComponent/StudentForm';
+import '../../styles/RegisterStudentPage.css';
+import NavBar from "../../components/NavBar";
+import Footer from "../../components/Footer";
+
+export default function RegisterStudentPage() {
+    return (
+        <>
+            <NavBar />
+            <StudentForm />
+            <Footer/>
+        </>
+
+    );
+}
