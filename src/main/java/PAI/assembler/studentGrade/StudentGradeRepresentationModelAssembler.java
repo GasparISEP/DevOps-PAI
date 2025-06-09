a9c5ec12d14839b23e7e1bc6d804072cf449e5c4
paulojorgeribeiro <1241920@isep.ipp.pt>
2025-06-09 17:41:44 +0100

    Implement HATEOAS for Grade a Student in a Course Edition - update HATEOAS assembler (Issue #685)

--- src/main/java/PAI/assembler/studentGrade/StudentGradeRepresentationModelAssembler.java
+++ src/main/java/PAI/assembler/studentGrade/StudentGradeRepresentationModelAssembler.java
@@ -1,4 +1,45 @@
 package PAI.assembler.studentGrade;
 
-public class StudentGradeRepresentationModelAssembler {
+import PAI.controllerRest.CourseEditionRestController;
+import PAI.dto.studentGrade.GradeAStudentResponseDTO;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Component
+public class StudentGradeRepresentationModelAssembler
+        implements RepresentationModelAssembler<GradeAStudentResponseDTO, EntityModel<GradeAStudentResponseDTO>> {
+
+    @Override
+    public EntityModel<GradeAStudentResponseDTO> toModel(GradeAStudentResponseDTO dto) {
+        return EntityModel.of(dto,
+
+                // Link para obter os dados da média da CourseEdition (já sem throws)
+                linkTo(methodOn(CourseEditionRestController.class)
+                        .getCourseEditionAverageGrade(
+                                dto._programmeId(),
+                                dto._schoolYearId(),
+                                dto._courseId(),
+                                dto._studyPlanId()
+                        )
+                ).withRel("averageGrade"),
+
+                // Link para obter a taxa de aprovação da CourseEdition (já sem throws)
+                linkTo(methodOn(CourseEditionRestController.class)
+                        .getCourseEditionApprovalRate(
+                                dto._programmeId(),
+                                dto._schoolYearId(),
+                                dto._courseId(),
+                                dto._studyPlanId()
+                        )
+                ).withRel("approvalRate"),
+
+                // Link self para o POST (isto é simbólico, já que o POST não aceita GETs)
+                linkTo(CourseEditionRestController.class).slash("studentgrades/register")
+                        .withSelfRel()
+        );
+    }
 }
