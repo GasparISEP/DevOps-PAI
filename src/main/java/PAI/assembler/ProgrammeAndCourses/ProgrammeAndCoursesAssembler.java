2211f777987cd616a38168e9b5a726982ab7c02b
gasparfigueiras <1241907@isep.ipp.pt>
2025-06-11 19:40:15 +0100

    created methods in ProgrammeAndCoursesAssembler (#646)

--- src/main/java/PAI/assembler/ProgrammeAndCourses/ProgrammeAndCoursesAssembler.java
+++ src/main/java/PAI/assembler/ProgrammeAndCourses/ProgrammeAndCoursesAssembler.java
@@ -1,15 +1,20 @@
 package PAI.assembler.ProgrammeAndCourses;
 
-import PAI.VOs.US34Response;
+import PAI.VOs.*;
 import PAI.domain.courseEditionEnrolment.CourseEditionEnrolment;
 import PAI.domain.programmeEditionEnrolment.ProgrammeEditionEnrolment;
 import PAI.dto.ProgrammeAndCourses.ProgrammeEditionEnrolmentDTO;
 import PAI.dto.ProgrammeAndCourses.StudentEnrolmentResultDto;
+import PAI.dto.ProgrammeAndCourses.StudentProgrammeEnrolmentRequestDto;
+import PAI.dto.course.CourseIDDTO;
 import PAI.dto.courseEditionEnrolment.CourseEditionEnrolmentDto;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
+@Component
 public class ProgrammeAndCoursesAssembler {
 
     public StudentEnrolmentResultDto toDto(US34Response domain) {
@@ -36,4 +41,27 @@ public class ProgrammeAndCoursesAssembler {
         return new StudentEnrolmentResultDto(programmeDTO, courseDTOs);
     }
 
+    public StudentID toStudentID(StudentProgrammeEnrolmentRequestDto dto) {
+        return new StudentID(dto.studentId());
+    }
+
+    public ProgrammeEditionID toProgrammeEditionID(StudentProgrammeEnrolmentRequestDto dto) throws Exception {
+        Acronym acronym = new Acronym(dto.programmeAcronym());
+        ProgrammeID programmeID =  new ProgrammeID(acronym);
+
+        SchoolYearID schoolYearID = new SchoolYearID(UUID.fromString(dto.schoolYearId()));
+
+        return new ProgrammeEditionID(programmeID, schoolYearID);
+    }
+
+    public List<CourseID> toCourseIDs(StudentProgrammeEnrolmentRequestDto dto) {
+        List<CourseID> courseIDS = new ArrayList<>();
+        for (CourseIDDTO courseDTO : dto.courseIds()) {
+            Name name = new Name(courseDTO.name());
+            Acronym acronym = new Acronym(courseDTO.acronym());
+            CourseID courseID = new CourseID(acronym, name);
+            courseIDS.add(courseID);
+        }
+        return courseIDS;
+    }
 }
