867c9a15851b33b3c6f24b21eb01e8d8978072d3
Ruben Rocha <1241924@isep.ipp.pt>
2025-06-11 14:03:01 +0100

    test: TeacherIdDTO shouldBeCreatedWithNullId test. Refs #682

--- src/test/java/PAI/dto/teacher/TeacherIdDTOTest.java
+++ src/test/java/PAI/dto/teacher/TeacherIdDTOTest.java
@@ -1,5 +1,7 @@
 package PAI.dto.teacher;
 
+import jakarta.validation.Validation;
+import jakarta.validation.ValidatorFactory;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -10,6 +12,17 @@ class TeacherIdDTOTest {
     void shouldCreateTeacherIdDTO() {
         //arrange
         String id = "ABC";
+        //act
+        TeacherIdDTO teacherIdDTO = new TeacherIdDTO(id);
+        //assert
+        assertNotNull(teacherIdDTO);
+        assertEquals(id, teacherIdDTO.id());
+    }
+
+    @Test
+    void shouldCreateTeacherIdDtoWithNullId() {
+        //arrange
+        String id = null;
         //act
         TeacherIdDTO teacherIdDTO = new TeacherIdDTO(id);
         //assert
