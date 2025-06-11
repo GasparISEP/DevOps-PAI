13e3f6d03432c85bb1125b9c0547c0dc3e450819
Nuno Cruz <1241919@isep.ipp.pt>
2025-06-11 14:03:44 +0100

    Updated the method getAllProgrammesThatTheStudentIsEnrolledIn from ProgrammeRestController and refactored related classes to support the changes made. ref #646

--- src/test/java/PAI/dto/student/StudentIDDTOTest.java
+++ src/test/java/PAI/dto/student/StudentIDDTOTest.java
@@ -2,40 +2,15 @@ package PAI.dto.student;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
-import PAI.VOs.StudentID;
 
 class StudentIDDTOTest {
 
     @Test
-    void shouldReturnStudentID() {
-        // Arrange
-        StudentID mockStudentID =  mock(StudentID.class);
+    void testValidStudentIDConversion() {
+        StudentIDDTO dto = new StudentIDDTO("1234567");
+        String studentID = dto.studentID();
 
-        // Act
-        StudentIDDTO dto = new StudentIDDTO(mockStudentID);
-
-        // Assert
-        assertEquals(mockStudentID, dto.studentID());
-    }
-
-    @Test
-    void shouldSupportEquality() {
-        //arrange
-        int number = 1234567;
-        StudentID studentID1 = mock(StudentID.class);
-
-        StudentIDDTO dto1 = new StudentIDDTO(studentID1);
-        StudentIDDTO dto2 = new StudentIDDTO(studentID1);
-
-        //act
-        when(studentID1.getUniqueNumber()).thenReturn(number);
-
-        //assert
-        assertEquals(dto1, dto2);
-        assertEquals(dto1.hashCode(), dto2.hashCode());
+        assertEquals("1234567", studentID);
     }
 
 }
