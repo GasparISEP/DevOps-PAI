7db9d98a920bb795887f0da8e72acafd825ad822
pedrofg890 <1210608@isep.ipp.pt>
2025-06-12 11:27:59 +0100

    feat(course-assembler): add CourseHateoasAssembler implementation and interface for HATEOAS support. #686

---
+++ src/main/java/PAI/assembler/course/CourseHateoasAssemblerImpl.java
@@ -0,0 +1,15 @@
+package PAI.assembler.course;
+
+import PAI.controllerRest.TeacherRestController;
+import PAI.controllerRest.CourseRESTController;
+import PAI.dto.course.CourseResponseDTO;
+import PAI.dto.teacher.TeacherDTO;
+import org.springframework.hateoas.EntityModel;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+public class CourseHateoasAssemblerImpl {
+
+
+}
