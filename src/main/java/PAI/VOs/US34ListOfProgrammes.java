08faf52d6c4979fe15772598027d30f35065f6c0
Nuno Cruz <1241919@isep.ipp.pt>
2025-06-12 17:51:09 +0100

    Created a new object that holds a list of Programmes and a list of ProgrammeEnrolments to then update the gemethod getAllProgrammesThatTheStudentIsEnrolledIn. ref #646

--- src/main/java/PAI/VOs/US34ListOfProgrammes.java
+++ src/main/java/PAI/VOs/US34ListOfProgrammes.java
@@ -1,4 +1,12 @@
 package PAI.VOs;
 
-public class US34ListOfProgrammes {
+import PAI.domain.programme.Programme;
+import PAI.domain.programmeEnrolment.ProgrammeEnrolment;
+
+import java.util.List;
+
+public record US34ListOfProgrammes (
+        List<Programme> programme,
+        List<ProgrammeEnrolment> programmeEnrolment
+) {
 }
