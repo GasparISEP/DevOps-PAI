bb0306dd71dd424578e3b2b2b9aea7b6273d48d9
Nuno Cruz <1241919@isep.ipp.pt>
2025-06-13 11:03:40 +0100

    Added a new method to ProgEnrolService to get ProgIDs by a list of ProgEnrol and tested it. ref #646

--- src/main/java/PAI/service/programmeEnrolment/IProgrammeEnrolmentService.java
+++ src/main/java/PAI/service/programmeEnrolment/IProgrammeEnrolmentService.java
@@ -10,4 +10,5 @@ public interface IProgrammeEnrolmentService {
     ProgrammeEnrolment enrolStudentInProgramme (StudentID studentID, AccessMethodID accessMethodID, ProgrammeID programmeID, Date date) throws Exception;
     ProgrammeEnrolment findEnrolmentByStudentAndProgramme(StudentID sid, ProgrammeID pid);
     List<ProgrammeEnrolment> listOfProgrammesStudentIsEnrolledIn(StudentID studentID);
+    List<ProgrammeID> getProgrammeIDsByProgrammeEnrolment (List<ProgrammeEnrolment> list);
 }
