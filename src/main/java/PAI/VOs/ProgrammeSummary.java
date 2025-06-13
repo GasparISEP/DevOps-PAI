2ef615db0a2b8796f355a9e3a59ecf3dac3dbc57
Nuno Cruz <1241919@isep.ipp.pt>
2025-06-13 16:25:10 +0100

    Added new DTOs to support refactor to getAllProgThatStudentIsEnrolled in ProgRestController. ref 0646

--- src/main/java/PAI/VOs/ProgrammeSummary.java
+++ src/main/java/PAI/VOs/ProgrammeSummary.java
@@ -1,7 +1,5 @@
 package PAI.VOs;
 
-import PAI.domain.programme.Programme;
-
 public record ProgrammeSummary (
         ProgrammeID programmeID,
         NameWithNumbersAndSpecialChars programmeName,
