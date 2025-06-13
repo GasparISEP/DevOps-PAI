c4b350439f35269e8ae739a3f4115d263dfa658f
jmariz-costa <1241912@isep.ipp.pt>
2025-06-13 12:32:39 +0100

    docs: update Level 1 System Context in C4 documentation to reflect architectural changes. Refs #687

--- src/main/java/PAI/service/totalEnrolledStudentsInProgrammesByDepartmentAndSchoolYear/TotalEnrolledStudentsInProgrammesByDepartmentAndSchoolYearServiceImpl.java
+++ src/main/java/PAI/service/totalEnrolledStudentsInProgrammesByDepartmentAndSchoolYear/TotalEnrolledStudentsInProgrammesByDepartmentAndSchoolYearServiceImpl.java
@@ -13,9 +13,7 @@ import PAI.domain.repositoryInterfaces.schoolYear.ISchoolYearRepository;
 import PAI.dto.totalEnrolledStudents.TotalEnrolledStudentsCommand;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class TotalEnrolledStudentsInProgrammesByDepartmentAndSchoolYearServiceImpl implements  ITotalEnrolledStudentsInProgrammesByDepartmentAndSchoolYearService {
