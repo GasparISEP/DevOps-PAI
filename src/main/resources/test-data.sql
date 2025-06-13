9735c6f6f8fd8eceab994d6dbb1b2e41571291ae
paulojorgeribeiro <1241920@isep.ipp.pt>
2025-06-13 03:06:51 +0100

    Update test-data.sql to support HATEOAS test scenarios (Issue #685)

--- src/main/resources/test-data.sql
+++ src/main/resources/test-data.sql
@@ -1,6 +1,5 @@
 INSERT INTO course_edition_enrolments (
-    cee_programme_acronym,
-    cee_programme_name,
+    programme_acronym,
     edition_programme_acronym,
     edition_school_year,
     courseid_acronym,
@@ -11,18 +10,16 @@ INSERT INTO course_edition_enrolments (
     enrolment_date,
     cee_generated_id,
     version
-)
-VALUES (
-           'LEI',
-           'Engenharia Informática',
-           'LEI',
-           '550e8400-e29b-41d4-a716-446655440000',
-           'PAI',
-           'Processos de Apoio à Inovação',
-           '2023-09-01',
-           1234567,
-           TRUE,
-           CURRENT_DATE,
-           '09f55463-aed7-4821-bfb2-712151f62714',
-           0
-       );
+) VALUES (
+             'LEI',
+             'LEI',
+             '550e8400-e29b-41d4-a716-446655440000',
+             'PAI',
+             'Processos de Apoio à Inovação',
+             '2023-09-01',
+             1234567,
+             TRUE,
+             CURRENT_DATE,
+             '09f55463-aed7-4821-bfb2-712151f62714',
+             0
+         );
