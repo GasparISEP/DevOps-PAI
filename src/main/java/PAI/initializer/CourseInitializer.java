1abe54dba26114d1181b60060062eec0a40f7271
Joao Coutinho <1241910@isep.ipp.pt>
2025-06-15 18:57:12 +0100

    Removed unused imports in CourseInitializer. (Ref #765)

--- src/main/java/PAI/initializer/CourseInitializer.java
+++ src/main/java/PAI/initializer/CourseInitializer.java
@@ -3,15 +3,10 @@ package PAI.initializer;
 import PAI.VOs.Acronym;
 import PAI.VOs.Name;
 import PAI.service.course.ICourseService;
-import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 
 @Component
 public class CourseInitializer {
