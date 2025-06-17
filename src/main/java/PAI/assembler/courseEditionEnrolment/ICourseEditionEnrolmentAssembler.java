5d44ce63c98f86b79234ffdb3d1d9b1a9b7cb742
paulojorgeribeiro <1241920@isep.ipp.pt>
2025-06-17 08:52:25 +0100

    Update ICourseEditionEnrolmentAssembler interface for HATEOAS (Issue #685)

--- src/main/java/PAI/assembler/courseEditionEnrolment/ICourseEditionEnrolmentAssembler.java
+++ src/main/java/PAI/assembler/courseEditionEnrolment/ICourseEditionEnrolmentAssembler.java
@@ -4,12 +4,16 @@ import PAI.VOs.CourseEditionID;
 import PAI.VOs.StudentID;
 import PAI.domain.courseEditionEnrolment.CourseEditionEnrolment;
 import PAI.dto.courseEditionEnrolment.CourseEditionEnrolmentDto;
+import PAI.dto.courseEditionEnrolment.CourseEditionEnrolmentMinimalDTO;
 
 public interface ICourseEditionEnrolmentAssembler {
     CourseEditionID toCourseEditionID(CourseEditionEnrolmentDto courseEditionEnrolmentDto) throws Exception;
     StudentID toStudentID(int studentUniqueNumber) throws Exception;
 
     CourseEditionEnrolmentDto toDto(CourseEditionEnrolment enrolment);
+
+    CourseEditionEnrolmentMinimalDTO toMinimalDTO(CourseEditionEnrolment enrolment);
+
 
 }   
 
