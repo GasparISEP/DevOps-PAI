afbc87d6aa864f4677d06f71a8d9a324a84a2bd7
joaopinheiro1241913 <1241913@isep.ipp.pt>
2025-06-18 00:12:50 +0100

    docs(course-in-study-plan-rest-controller): add JavaDocs to clarify the purpose and behavior of the create() method (refs: #702)

--- src/main/java/PAI/controllerRest/CourseInStudyPlanRestController.java
+++ src/main/java/PAI/controllerRest/CourseInStudyPlanRestController.java
@@ -1,18 +1,12 @@
 package PAI.controllerRest;
 
 import PAI.VOs.*;
-import PAI.assembler.courseInStudyPlan.CourseInStudyPlanAssemblerImpl;
 import PAI.assembler.courseInStudyPlan.ICourseInStudyPlanAssembler;
-import PAI.assembler.courseInStudyPlan.ICourseInStudyPlanBusinessAssembler;
 import PAI.assembler.courseInStudyPlan.ICourseInStudyPlanHateoasAssembler;
-import PAI.domain.courseInStudyPlan.CourseInStudyPlan;
 import PAI.dto.courseInStudyPlan.CourseInStudyPlanCommand;
 import PAI.dto.courseInStudyPlan.CourseInStudyPlanRequestDTO;
 import PAI.dto.courseInStudyPlan.CourseInStudyPlanResponseDTO;
 import PAI.dto.courseInStudyPlan.CourseInStudyPlanServiceDTO;
-import PAI.exception.BusinessRuleViolationException;
-import PAI.service.courseEdition.ICreateCourseEditionService;
-import PAI.service.courseInStudyPlan.CourseInStudyPlanServiceImpl;
 import PAI.service.courseInStudyPlan.IAddCourseToAProgrammeService;
 import PAI.service.courseInStudyPlan.ICourseInStudyPlanService;
 import PAI.service.studyPlan.IStudyPlanService;
@@ -49,6 +43,13 @@ public class CourseInStudyPlanRestController {
         this.courseInStudyPlanHateoasAssembler = validateNotNull(courseInStudyPlanHateoasAssembler, "CourseInStudyPlanHateoasAssembler");
     }
 
+    /**
+     * Handles the creation of a new Course in a Study Plan.
+     *
+     * @param dtoRequest The request DTO containing course and programme data.
+     * @return created HATEOAS-wrapped response with 201 status
+     * @throws Exception If an unexpected error occurs during processing.
+     */
     @PostMapping
     public ResponseEntity<EntityModel<CourseInStudyPlanResponseDTO>> create(
             @Valid @RequestBody CourseInStudyPlanRequestDTO dtoRequest) throws Exception {
